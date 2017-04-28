926b2b3b2addabdb6ed806146ec9b3ed916cd538
Sibyl <sibyl@wormbase.org>
2017-04-28 11:30:34 -0400

    remove manifest revision webpack plugin * remove from config and dependencies * not needed right now, as assets are linked via html webpack plugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,7 +2,6 @@ import path from 'path';
 import webpack from 'webpack';
 
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import ManifestRevisionPlugin from 'manifest-revision-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 let isProduction = process.env.NODE_ENV === 'production';
@@ -114,13 +113,7 @@ if (isProduction) {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new ExtractTextPlugin(cssFileName),
-    // Question: do we still need Manifest file, when we inject hashed assets by
-    // html-webpack-plugin?
-    new ManifestRevisionPlugin(path.join(buildOutputPath, 'manifest.json'), {
-      rootAssetPath: '../dist', // doesn't seem to like absolute path
-      ignorePaths: ['/styles', '/scripts']
-    })
+    new ExtractTextPlugin(cssFileName)
   ]
 }
 
