e5bcf729424706e40baa79ad578d1b6808889570
Bryon Czoch <bryon.czoch@gmail.com>
2017-05-02 13:37:11 -0400

    working deconstructor on static data

--- src/components/disease/index.js
+++ src/components/disease/index.js
@@ -4,37 +4,101 @@ import FlybaseDataGrid from 'react-flybase-datagrid';
 function getHeaders(){
 
   var columns = [
-   {id:'name', name:'Disease Name'},
-   {id:'assc', name:'Association'},
-   {id:'ec', name:'Evidence Code'},
-   {id:'as', name:'Association Source'},
+   {id:'do_name', name:'Disease Name'},
+   {id:'associationType', name:'Association'},
+   {id:'evidence', name:'Evidence Code'},
+   {id:'dataProvider', name:'Association Source'},
    {id:'ref', name:'References'}
   ];
-
   return columns;
-
 }
 
-const data = [
-{ name: 'Bannayan-Riley-Ruvalcaba syndrome', assc: 'is model of', ec: 'TAS', as: 'MGI', ref: 'PMID:12345, PMID:12345'},
-{ name: 'brain glioma', assc: 'causes or contributes to condition', ec: 'TAS', as: 'FB', ref: 'PMID:12345'},
-{ name: 'Cowden disease', assc: 'is model of', ec: 'TAS', as: 'MGI', ref: 'PMID:12345'},
-{ name: 'endometrial cancer', assc: 'causes or contributes to condition', ec: 'ISS', as: 'RGD', ref: 'PMID:12345'},
-{ name: 'prostate cancer', assc: 'is model of', ec: 'IC', as: 'ZFIN', ref: 'PMID:12345'},
-{ name: 'prostate cancer', assc: 'causes or contributes to condition', ec: 'ISS', as: 'SGD', ref: 'PMID:12345'},
-{ name: 'acute lymphocytic leukemia', assc: 'is model of', ec: 'TAS', as: 'MGI', ref: 'PMID:12345'},
-{ name: 'fatty liver disease', assc: 'causes or contributes to condition', ec: 'IEP', as: 'RGD', ref: 'PMID:12345'},
-{ name: 'fatty liver disease', assc: 'is model of', ec: 'TAS', as: 'MGI', ref: 'PMID:12345'},
-{ name: 'hepatocellular carcinoma', assc: 'causes or contributes to condition', ec: 'ISS', as: 'WB', ref: 'PMID:12345'},
-{ name: 'persistent fetal circulation syndrome', assc: 'is model of', ec: 'TAS', as: 'MGI', ref: 'PMID:12345'},
-{ name: 'urinary bladder cancer', assc: 'is marker for', ec: 'ISS', as: 'RGD', ref: 'PMID:12345'},
-];
-
 class DiseaseTable extends Component {
+  constructor(props) {
+    super(props);
+
+  // const diseases = this.props.data.diseases;
+
+    var diseases = [
+      {
+        'associationType': 'contributes_to_condition',
+        'dataProvider': 'ZFIN',
+        'do_name': [
+          'cardiomyopathy'
+        ],
+        'evidence': [
+          {
+            'evidenceCode': 'TAS',
+            'pubs': [
+              {
+                'pubMedId': '27642634',
+                'publicationModId': 'ZFIN:ZDB-PUB-160920-5'
+              },
+              {
+                'pubMedId': '1233',
+                'publicationModId': 'ZFIN:ZDB-PUB-170406-10'
+              }
+            ]
+          }
+        ],
+      },
+      {
+        'associationType': 'contributes_to_condition',
+        'dataProvider': 'flybase',
+        'do_name': [
+          'cancer'
+        ],
+        'evidence': [
+          {
+            'evidenceCode': 'TAS',
+            'pubs': [
+              {
+                'pubMedId': '987654',
+                'publicationModId': 'ZFIN:ZDB-PUB-160920-5'
+              },
+              {
+                'pubMedId': '',
+                'publicationModId': 'ZFIN:ZDB-PUB-170406-10'
+              }
+            ]
+          }
+        ],
+      }
+    ];
+
+    this.data = diseases.map((disease) => {
+      const {
+        associationType: associationType,
+        dataProvider: dataProvider,
+        do_name: do_name,
+        evidence: [
+          {
+            evidenceCode: evidenceCode,
+            pubs: [ ...pubs ]
+          }
+        ],
+      } = disease;
+
+      return {
+        do_name : do_name,
+        associationType: associationType,
+        evidence: evidenceCode,
+        dataProvider: dataProvider,
+        ref: pubs.map((pub) => { return pub['pubMedId']; }).filter((pub) => { return (pub!=''); }).join()
+      };
+    });
+
+  }
+
   render() {
     return (
 
-       <FlybaseDataGrid columns={getHeaders()} data={data} showColumnFilter={true} showDownloadButton={true} />
+       <FlybaseDataGrid
+         columns={getHeaders()}
+         data={this.data}
+         showDownloadButton={false}
+         showFilter={false}
+       />
 
     );
   }
