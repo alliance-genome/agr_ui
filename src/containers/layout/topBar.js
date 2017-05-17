88caa2a9154f841ce8f9f8a5df8a8af8645be819
LucyHut <lucie.hutchins@yahoo.com>
2017-05-17 14:42:57 -0400

    Added call to socialMedia component

--- src/containers/layout/topBar.js
+++ src/containers/layout/topBar.js
@@ -2,20 +2,14 @@ import React, {Component} from 'react';
 import { Link } from 'react-router';
 
 import { SMALL_COL_CLASS, LARGE_COL_CLASS } from '../../constants';
+import SocialMedia from './socialMedia';
 
 import style from './style.css';
 
 class TopBar extends Component {
   render() {
     let email_link='mailto:info@alliancegenome.org';
-    let facebook_link='https://www.facebook.com/alliancegenome/';
-    let github_link='https://github.com/alliance-genome';
-    let twitter_link='https://twitter.com/alliancegenome';
-    
     let email_logo='info@alliancegenome.org';
-    let facebook_logo='http://www.alliancegenome.org/wp-content/uploads/2017/01/facebook.png';
-    let github_logo='http://www.alliancegenome.org/wp-content/uploads/2017/01/github.png';
-    let twitter_logo='http://www.alliancegenome.org/wp-content/uploads/2017/01/twitter.png';
    
     return (
       <div className={`col-xs-12 ${style.topBar} ${style.solidBg}`}>
@@ -26,17 +20,7 @@ class TopBar extends Component {
             </span>
           </div>
           <div className={`${LARGE_COL_CLASS} ${style.rightWidgets} ${style.miniBidgets}`}>
-              <p>
-                <Link to={facebook_link}>
-                  <img src={facebook_logo} />
-                </Link>
-                <Link to={twitter_link}>
-                  <img src={twitter_logo} />
-                </Link>
-                <Link to={github_link}>
-                  <img src={github_logo} />
-                </Link>
-              </p>
+            <SocialMedia />
           </div>
         </div>
       </div>
@@ -46,9 +30,6 @@ class TopBar extends Component {
 
 TopBar.propTypes = {
   email_link: React.PropTypes.string,
-  facebook_link: React.PropTypes.string,
-  github_link: React.PropTypes.string,
-  twitter_link: React.PropTypes.string,
 };
 
 export default TopBar;
