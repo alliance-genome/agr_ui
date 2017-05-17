5345ed59ebd3faa418095adf10496f33d8a8a113
LucyHut <lucie.hutchins@yahoo.com>
2017-05-17 14:50:28 -0400

    Added routing to more WP pages

--- src/routes.js
+++ src/routes.js
@@ -11,8 +11,15 @@ export default (
     <IndexRoute component={Wordpress} />
     <Route component={Wordpress} path='home' />
     <Route component={Wordpress} path='about' />
+    <Route component={Wordpress} path='faq' />
+    <Route component={Wordpress} path='organization' />
+    <Route component={Wordpress} path='funding' />
+    <Route component={Wordpress} path='features' />
+    <Route component={Wordpress} path='supplement' />
+    <Route component={Wordpress} path='orthology' />
+    <Route component={Wordpress} path='phenotypes' />
     <Route component={Wordpress} path='contact' />
-    <Route component={Wordpress} path='publications' />
+    <Route component={Wordpress} path='projects' />
     <Route component={Search} path='search' />
     <Route component={GenePage} path='gene/:geneId' />
   </Route>
