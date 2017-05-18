c2daaf553b6981b8e6f192e7acc03a36ab332550
LucyHut <lucie.hutchins@yahoo.com>
2017-05-18 10:15:16 -0400

    Refactor wordpress pages route

--- src/routes.js
+++ src/routes.js
@@ -9,18 +9,7 @@ import GenePage from './containers/genePage';
 export default (
   <Route component={Layout} path='/'>
     <IndexRoute component={Wordpress} />
-    <Route component={Wordpress} path='home' />
-    <Route component={Wordpress} path='about' />
-    <Route component={Wordpress} path='faq' />
-    <Route component={Wordpress} path='organization' />
-    <Route component={Wordpress} path='funding' />
-    <Route component={Wordpress} path='features' />
-    <Route component={Wordpress} path='supplement' />
-    <Route component={Wordpress} path='orthology' />
-    <Route component={Wordpress} path='phenotypes' />
-    <Route component={Wordpress} path='publications' />
-    <Route component={Wordpress} path='contact' />
-    <Route component={Wordpress} path='projects' />
+    <Route component={Wordpress} path='wordpress/:pageId' />
     <Route component={Search} path='search' />
     <Route component={GenePage} path='gene/:geneId' />
   </Route>
