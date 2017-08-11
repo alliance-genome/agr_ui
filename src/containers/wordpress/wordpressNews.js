93ec72c4e9087922395604a053cae8d3b1afaa95
Lucie Hutchins <lnh@mttv-dev1.jax.org>
2017-08-11 16:33:28 -0400

    Added a new react component to render the News page

---
+++ src/containers/wordpress/wordpressNews.js
@@ -0,0 +1,47 @@
+/*eslint-disable no-unused-vars*/
+/*** This component builds the 'News & Events' page **/
+
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+import style from './style.css';
+import{ WP_POST_MAX_COUNT ,WP_POST_PATH} from '../../constants';
+
+class WordpressNews extends Component {
+  render() {
+    let container=[];
+    /* keep trck of the number of published posts */
+    let posts_count=1;
+    for(var index in this.props.data){
+      if(posts_count>WP_POST_MAX_COUNT) break;
+      let post_slug=this.props.data[index].slug;
+      let post_status=this.props.data[index].status;
+      let post_title=this.props.data[index].title.rendered;
+      let post_image=this.props.data[index].featured_media_url;
+      let post_excerpt=this.props.data[index].excerpt.rendered;
+      
+      /* only display published posts */
+      if(post_status !='publish') continue;
+      ++posts_count; 
+      container.push(<div className={`row ${style.postContainer}`} key={index}>
+          <div className='col-xs-12 col-sm-4'>
+            <Link to={`${WP_POST_PATH}/${post_slug}`}>
+              <img className='img-fluid' src={post_image} /> </Link></div>
+          <div className='col-xs-12 col-sm-8'>
+             <Link to={`${WP_POST_PATH}/${post_slug}`}>
+               <h3 dangerouslySetInnerHTML={{ __html: post_title}} /></Link>
+             <p dangerouslySetInnerHTML={{ __html: post_excerpt}} /></div>
+         </div>);
+    }
+
+    return (
+      <div className={`col-xs-12 ${style.newsContainer}`}>{container} </div>
+    );
+  }
+}
+
+WordpressNews.propTypes = {
+  data: React.PropTypes.object.isRequired,
+};
+
+export default WordpressNews;
