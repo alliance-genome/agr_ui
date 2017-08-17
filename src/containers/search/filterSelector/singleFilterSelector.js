7120c295bbbd5a8d7b805e220ef2f299f8aaaf5f
Patrick Kalita <patrick.kalita@gmail.com>
2017-08-17 11:59:38 -0700

    Back down to bootstrap alpha 4 due to incompatibilities

--- src/containers/search/filterSelector/singleFilterSelector.js
+++ src/containers/search/filterSelector/singleFilterSelector.js
@@ -121,7 +121,7 @@ class SingleFilterSelector extends Component {
 
   renderListNode() {
     return (
-      <ul className='nav nav-pills flex-column'>
+      <ul className='nav nav-pills nav-stacked'>
         {this.renderFilterValues(this.props)}
       </ul>
     );
