cf8617f56bb75cec036a84a5895dd7f6b97cd87c
Lucie Hutchins <lnh@mttv-dev1.local>
2017-08-18 13:04:49 -0400

    Added support for posts routing

--- src/containers/layout/siteMap.js
+++ src/containers/layout/siteMap.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router';
 
 import style from './style.css';
 import SubMenu from './subMenu';
-import { MENU, WP_PAGES } from '../../constants';
-
+import { MENU, WP_PAGES ,WP_PATH ,WP_POST_PATH } from '../../constants';
 
 class SiteMap extends Component {
   render() {
     let container=[];
     for(var index in MENU){
       let page=MENU[index];
+      let page_path=(page==='news')?WP_POST_PATH:WP_PATH;
       container.push(<div className={style.navContainer} key={index}>
-          <Link className={`nav-link ${style.navLink}`} to={`/wordpress/${WP_PAGES[page].path}`}>{WP_PAGES[page].label}  </Link>
+          <Link className={`nav-link ${style.navLink}`} to={`${page_path}/${WP_PAGES[page].path}`}>{WP_PAGES[page].label}  </Link>
           <SubMenu path={page} />
          </div>);
     }
