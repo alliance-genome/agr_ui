37e16cf669d5fc7371001dd265b89594c12d7c51
Olin Blodgett <nilo@nilosplace.net>
2017-08-24 20:31:29 -0400

    Added disable host check for proxy

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -24,6 +24,7 @@ let config = {
     filename: '[name].[hash].js',
     chunkFilename: '[id].[chunkhash].js'
   },
+  disableHostCheck: true,
   devtool: 'eval-source-map',
   devServer: {
     contentBase: 'dist',
