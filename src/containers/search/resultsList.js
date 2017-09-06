dbc6dfb0fcdad433e42e92b1fd49082bb1e49ca2
Bryon Czoch <bczoch@iu.edu>
2017-09-06 17:22:17 -0400

    Update resultsList.js

--- src/containers/search/resultsList.js
+++ src/containers/search/resultsList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
 import style from './style.css';
+import genePageStyle from '../genePage/style.css';
 import CategoryLabel from './categoryLabel';
 import DetailList from './detailList';
 import { NON_HIGHLIGHTED_FIELDS } from '../../constants';
@@ -50,10 +51,12 @@ class ResultsList extends Component {
   renderGeneEntry(d, i) {
     let topFields = ['name', 'synonyms'];
     let bottomFields = ['species', 'biotype'];
+    const speciesClass = genePageStyle[(d.species || '').replace(' ', '-')];
     let link = <Link to={`/gene/${d.id}`}><span dangerouslySetInnerHTML={{ __html: d.display_name }} /></Link>;
     return (
       <div className={style.resultContainer} key={`sr${i}`}>
         {this.renderHeader(d.category, link)}
+          {speciesClass && <div className={`${genePageStyle.speciesIcon} ${speciesClass} ${style.resultSpeciesIcon}`} />}
           {this.renderDetailFromFields(d, topFields)}
           <div className={style.detailContainer}>
             <span className={style.detailLabel}><strong>Source:</strong> </span>
