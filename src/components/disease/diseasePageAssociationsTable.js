43d43d5fdd455dbb453db7d0e18df953272cf5e3
Bryon Czoch <bryon.czoch@gmail.com>
2017-09-12 17:26:47 -0400

    add data

--- src/components/disease/diseasePageAssociationsTable.js
+++ src/components/disease/diseasePageAssociationsTable.js
@@ -24,6 +24,14 @@ class DiseasePageAssociationsTable extends Component {
   }
 
   render() {
+    const data = [
+        {do_name: 'amyotrophic lateral sclerosis', do_id: 'Homo sapiens', associatedGene: 'TARDBP', associationType: 'is implicated in', source: 'RGD', references: 'PMID:56789'},
+        {do_name: 'amyotrophic lateral sclerosis', do_id: 'Rattus norvegicus', associatedGene: 'Tardbp', associationType: 'is implicated in', source: 'RGD', references: 'PMID:23456'},
+        {do_name: 'amyotrophic lateral sclerosis', do_id: 'Drosophila melanogaster', associatedGene: 'Sod1', associationType: 'is implicated in', source: 'FB', references: 'PMID:12345'},
+        {do_name: 'amyotrophic lateral sclerosis type 10', do_id: 'Mus musculus', associatedGene: 'Ang2', associationType: 'is implicated in', source: 'MGI', references: 'PMID:12345'},
+        {do_name: 'amyotrophic lateral sclerosis type 10', do_id: 'Danio rerio', associatedGene: 'angpt2b', associationType: 'is implicated in', source: 'ZFIN', references: 'PMID:34567'},
+        //{do_name: 'FTDALS1', do_id: 'Saccharomyces cerevisiae', associatedGene: 'asdf', associationType: 'is implicated in', source: 'SGD', references: {'PMID:45678', 'PMID:99891'}},
+      ],
     const columns = [
       {
         field: 'do_name',
@@ -63,7 +71,7 @@ class DiseasePageAssociationsTable extends Component {
     ];
     return (
       <div>
-        <RemoteDataTable columns={columns} url='http://dev.alliancegenome.org/api/disease/DOID:9452/associations' />
+        <RemoteDataTable data={data} columns={columns} url='http://dev.alliancegenome.org/api/disease/DOID:9452/associations' />
       </div>
     );
   }
