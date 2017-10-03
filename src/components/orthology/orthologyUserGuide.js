e7c1bae5b8961a879970bbce874ad9697c33ed74
Sibyl Gao <sibyl@wormbase.org>
2017-10-03 17:30:00 -0400

    update orthology user guide text

--- src/components/orthology/orthologyUserGuide.js
+++ src/components/orthology/orthologyUserGuide.js
@@ -10,10 +10,10 @@ class OrthologyUserGuide extends Component {
         backgroundVariants="info"
         title="Learn about how orthologs are identified"
       >
-        <p>Many aspects of data integration across organisms require a common set of orthology relationships among genes for the organisms represented in the AGR. This common set is needed to facilitate comparison of biological annotations and other data across the diverse organisms represented in AGR: human, mouse, rat, zebrafish, fly, worm, and yeast.</p>
-        <p>The AGR methodology for the identification of orthologs among human and AGR organisms is currently built on the DRSC Integrative Ortholog Prediction Tool (DIOPT) (
+        <p>Many aspects of data integration across organisms require a common set of orthology relationships among genes for the organisms represented in the Alliance of Genome Resources. This common set is needed to facilitate comparison of biological annotations and other data across the diverse organisms represented in the Alliance: human, mouse, rat, zebrafish, fly, worm, and yeast.</p>
+        <p> (
           <ExternalLink href="http://www.flyrnai.org/diopt" />
-        ). DIOPT integrates a number of existing approaches: Compara, HGNC, Inparanoid, OMA, Panther, Phylome, RoundUp, TreeFam, ZFIN, and assigns a score/count based on these different methods.</p>
+        ). DIOPT integrates a number of existing approaches including Compara, HGNC, InParanoid, OMA, Panther, Phylome, RoundUp, TreeFam, and ZFIN, then assigns a score/count based on different methods.</p>
       </CollapsiblePanel>
     );
   }
