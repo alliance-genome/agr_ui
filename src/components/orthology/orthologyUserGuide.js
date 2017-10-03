e266ce4abf482f026a1d0896b25d1a915385241e
Sibyl Gao <sibyl@wormbase.org>
2017-10-03 17:45:52 -0400

    fix text

--- src/components/orthology/orthologyUserGuide.js
+++ src/components/orthology/orthologyUserGuide.js
@@ -11,7 +11,7 @@ class OrthologyUserGuide extends Component {
         title="Learn about how orthologs are identified"
       >
         <p>Many aspects of data integration across organisms require a common set of orthology relationships among genes for the organisms represented in the Alliance of Genome Resources. This common set is needed to facilitate comparison of biological annotations and other data across the diverse organisms represented in the Alliance: human, mouse, rat, zebrafish, fly, worm, and yeast.</p>
-        <p> (
+        <p>The Alliance methodology for the identification of orthologs among human and Alliance organisms is currently built on the DRSC Integrative Ortholog Prediction Tool (
           <ExternalLink href="http://www.flyrnai.org/diopt" />
         ). DIOPT integrates a number of existing approaches including Compara, HGNC, InParanoid, OMA, Panther, Phylome, RoundUp, TreeFam, and ZFIN, then assigns a score/count based on different methods.</p>
       </CollapsiblePanel>
