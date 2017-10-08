e56758f8788149168ff3c9445bcc02136766c7f3
LucyHut <lucie.hutchins@yahoo.com>
2017-10-08 15:06:15 -0400

    navLinksContainer.js:Coverted likns from router Links to external links

--- src/containers/layout/navLinksContainer.js
+++ src/containers/layout/navLinksContainer.js
@@ -18,10 +18,7 @@ class NavLinksContainer extends Component {
       if (page_url === HOME_ROUTE) page_url = '/';
       let page_label=WP_PAGES[page].label;
       let link_token=[];
-      if(page==='news'){
-        link_token.push(<a className={`nav-link ${style.navLink}`} href={page_url} key={index}>{page_label}</a>);
-      }
-      else{link_token.push(<NavLink key={index} label={page_label} url={page_url} />);}
+      link_token.push(<a className={`nav-link ${style.navLink}`} href={page_url} key={index}>{page_label}</a>);
       container.push(<div className={style.navContainer} key={index}>
           {link_token}
           <SubMenu key={index} path={page} />
