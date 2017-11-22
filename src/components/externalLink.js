ec95de2bba7eb496da63eb9977411640d5b31bb6
Bryon Czoch <bryon.czoch@gmail.com>
2017-11-22 12:30:18 -0500

    dont require externalLink

--- src/components/externalLink.js
+++ src/components/externalLink.js
@@ -23,7 +23,7 @@ class ExternalLink extends Component {
 
 ExternalLink.propTypes = {
   children: PropTypes.node,
-  href: PropTypes.string.isRequired,
+  href: PropTypes.string,
   title: PropTypes.string,
 };
 
