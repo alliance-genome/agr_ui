d9f7fb94424a97a2520f81497c2ec2efa8266ece
Patrick Kalita <patrick.kalita@gmail.com>
2017-12-07 12:37:50 -0800

    Refactor wordpress components
    This change refactors the wordpress-related components to draw a clear distinction
    between the three type of wordpress data we interact with: pages, posts, and a list
    posts. This is reflected in the actions, reducers, and selectors too. Data fetching
    work has been moved out of components and into actions, inline with other data types.

--- src/actions/wp.js
+++ src/actions/wp.js
@@ -1,23 +1,96 @@
-export const FETCH_WP = 'FETCH_WP';
-export const FETCH_WP_SUCCESS = 'FETCH_WP_SUCCESS';
-export const FETCH_WP_FAILURE = 'FETCH_WP_FAILURE';
+import fetchData from '../lib/fetchData';
+import { WP_PAGE_BASE_URL, WP_POST_BASE_URL, WP_POST_URL } from '../constants';
 
-export const fetchWp = function () {
-  return {
-    type: FETCH_WP,
+export const FETCH_WORDPRESS_POST_LIST = 'FETCH_WORDPRESS_POST_LIST';
+export const FETCH_WORDPRESS_POST_LIST_SUCCESS = 'FETCH_WORDPRESS_POST_LIST_SUCCESS';
+export const FETCH_WORDPRESS_POST_LIST_FAILURE = 'FETCH_WORDPRESS_POST_LIST_FAILURE';
+
+export const FETCH_WORDPRESS_POST = 'FETCH_WORDPRESS_POST';
+export const FETCH_WORDPRESS_POST_SUCCESS = 'FETCH_WORDPRESS_POST_SUCCESS';
+export const FETCH_WORDPRESS_POST_FAILURE = 'FETCH_WORDPRESS_POST_FAILURE';
+
+export const FETCH_WORDPRESS_PAGE = 'FETCH_WORDPRESS_PAGE';
+export const FETCH_WORDPRESS_PAGE_SUCCESS = 'FETCH_WORDPRESS_PAGE_SUCCESS';
+export const FETCH_WORDPRESS_PAGE_FAILURE = 'FETCH_WORDPRESS_PAGE_FAILURE';
+
+export const fetchWordpressPostList = function () {
+  return (dispatch) => {
+    dispatch({
+      type: FETCH_WORDPRESS_POST_LIST
+    });
+    fetchData(WP_POST_BASE_URL)
+      .then(data => dispatch(fetchWordpressPostListSuccess(data)))
+      .catch(error => dispatch(fetchWordpressPostListFailure(error)));
   };
 };
 
-export const fetchWpSuccess = function (wp) {
+export const fetchWordpressPostListSuccess = function (postList) {
   return {
-    type: FETCH_WP_SUCCESS,
-    payload: wp,
+    type: FETCH_WORDPRESS_POST_LIST_SUCCESS,
+    payload: postList,
   };
 };
 
-export const fetchWpFailure = function (error) {
+export const fetchWordpressPostListFailure = function (error) {
   return {
-    type: FETCH_WP_FAILURE,
+    type: FETCH_WORDPRESS_POST_LIST_FAILURE,
+    payload: error,
+  };
+};
+
+export const fetchWordpressPost = function (slug) {
+  return (dispatch) => {
+    dispatch({
+      type: FETCH_WORDPRESS_POST
+    });
+    fetchData(WP_POST_URL + slug)
+      .then(data => dispatch(fetchWordpressPostSuccess(data)))
+      .catch(error => dispatch(fetchWordpressPostFailure(error)));
+  };
+};
+
+export const fetchWordpressPostSuccess = function (post) {
+  return {
+    type: FETCH_WORDPRESS_POST_SUCCESS,
+    payload: post,
+  };
+};
+
+export const fetchWordpressPostFailure = function (error) {
+  return {
+    type: FETCH_WORDPRESS_POST_FAILURE,
+    payload: error,
+  };
+};
+
+export const fetchWordpressPage = function (slug) {
+  return (dispatch) => {
+    dispatch({
+      type: FETCH_WORDPRESS_PAGE
+    });
+    fetchData(WP_PAGE_BASE_URL + slug)
+      .then(data => {
+        if (data && data[0]) {
+          dispatch(fetchWordpressPageSuccess(data[0]));
+        } else {
+          // throw our own error, since WP API doesn't return 404 when page is not found
+          throw new Error('Page not found');
+        }
+      })
+      .catch(error => dispatch(fetchWordpressPageFailure(error)));
+  };
+};
+
+export const fetchWordpressPageSuccess = function (page) {
+  return {
+    type: FETCH_WORDPRESS_PAGE_SUCCESS,
+    payload: page,
+  };
+};
+
+export const fetchWordpressPageFailure = function (error) {
+  return {
+    type: FETCH_WORDPRESS_PAGE_FAILURE,
     payload: error,
   };
 };
