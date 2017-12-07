016ad6fa21c315f5c92c8d93f8d6061bcb0b3697
Patrick Kalita <patrick.kalita@gmail.com>
2017-12-07 12:28:30 -0800

    Formatting

--- src/constants.js
+++ src/constants.js
@@ -4,12 +4,12 @@ export const SMALL_COL_CLASS = 'col-sm-4 col-md-4 col-xs-12';
 
 /* Wordpress REST API connection setting */
 export const WP_REST_API_BASE = 'https://public-api.wordpress.com/wp/v2/sites/alliancegenome.wordpress.com';
-export const WP_PAGE_BASE_URL = WP_REST_API_BASE+'/pages?slug=';
-export const WP_POST_BASE_URL = WP_REST_API_BASE+'/posts';
-export const WP_POST_URL = WP_POST_BASE_URL+'?slug=';
+export const WP_PAGE_BASE_URL = WP_REST_API_BASE + '/pages?slug=';
+export const WP_POST_BASE_URL = WP_REST_API_BASE + '/posts';
+export const WP_POST_URL = WP_POST_BASE_URL + '?slug=';
 /* Maximum number of posts to display on the News and Events page */
 export const WP_POST_MAX_COUNT = 10;
-export const WP_PATH= '';
+export const WP_PATH = '';
 export const WP_POST_PATH = '/posts';
 
 export const CATEGORIES = [
@@ -32,17 +32,17 @@ export const CATEGORIES = [
 ];
 
 /**********  Wordpress Pages *************
-  An entry is added to this constant each
-  time a new static page that's part of the
-  menu or sub-menu  is created/publised in WP
+ An entry is added to this constant each
+ time a new static page that's part of the
+ menu or sub-menu  is created/publised in WP
 
-  Fields Definition:
-  1) label: Used in the site  menu/sub menu
-  2) slug: WP slug for this page
-  3) path: Used for Portal router
-******************************************/
+ Fields Definition:
+ 1) label: Used in the site  menu/sub menu
+ 2) slug: WP slug for this page
+ 3) path: Used for Portal router
+ ******************************************/
 
-export const WP_PAGES ={
+export const WP_PAGES = {
   home: {
     label: 'Home',
     slug: 'home'
@@ -59,7 +59,7 @@ export const WP_PAGES ={
     label: 'Contact US',
     slug: 'contact-us'
   },
-  'frequently-asked-questions':{
+  'frequently-asked-questions': {
     label: 'Frequently Asked Questions',
     slug: 'frequently-asked-questions'
   },
@@ -71,11 +71,11 @@ export const WP_PAGES ={
     label: 'Organization and Governance',
     slug: 'organization-and-governance'
   },
-  publications:{
+  publications: {
     label: 'Publications',
     slug: 'publications'
   },
-  'administrative-supplement':{
+  'administrative-supplement': {
     label: 'Administrative Supplement',
     slug: 'administrative-supplement'
   },
@@ -91,16 +91,16 @@ export const WP_PAGES ={
     label: 'Phenotypes And Disease Models',
     slug: 'phenotypes-and-disease-models'
   },
-  news:{
+  news: {
     label: 'News & Events',
     slug: 'news'
   }
 };
 
 /********* Main Menu ************************
-  This is where you add/remove entries
-  to/from the site menu
-*********************************************/
+ This is where you add/remove entries
+ to/from the site menu
+ *********************************************/
 export const MENU = [
   'home',
   'about-us',
@@ -109,20 +109,20 @@ export const MENU = [
   'contact-us'
 ];
 /********* Secondary nav setting ************************
-  This maps the ids of primary menu items
-*********************************************/
+ This maps the ids of primary menu items
+ *********************************************/
 export const MENU_IDS = {
-  16:'home',
-  2:'about-us',
-  257:'projects-work-products-publications',
-  3:'contact-us'
+  16: 'home',
+  2: 'about-us',
+  257: 'projects-work-products-publications',
+  3: 'contact-us'
 };
 
 /********* Sub Menu ************************
-  This is where you add/remove entries
-  to/from the submenus
-*********************************************/
-export const SUB_MENU= {
+ This is where you add/remove entries
+ to/from the submenus
+ *********************************************/
+export const SUB_MENU = {
   'about-us': [
     'frequently-asked-questions',
     'funding',
