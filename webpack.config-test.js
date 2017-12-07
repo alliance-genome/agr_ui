88751389a303af500f01bbce100612f2ad543f0e
Patrick Kalita <patrick.kalita@gmail.com>
2017-12-07 12:27:31 -0800

    Config changes for working in IntelliJ

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -3,6 +3,11 @@ var nodeExternals = require('webpack-node-externals');
 module.exports = {
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  output: {
+    devtoolModuleFilenameTemplate        : '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+  },
+  devtool: '#inline-cheap-module-source-map',
   module: {
     loaders: [
       {
