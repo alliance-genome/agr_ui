867d723c9f73457336a54c3e120da523f8b2f992
Patrick Kalita <patrick.kalita@gmail.com>
2017-12-08 16:35:06 -0800

    Clean up runtime warnings

--- src/containers/wordpress/secondaryNav.js
+++ src/containers/wordpress/secondaryNav.js
@@ -1,64 +1,72 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 import style from './style.css';
-import { MENU_IDS,WORDPRESS_PAGES } from './../../constants';
+import { MENU_IDS, WORDPRESS_PAGES } from './../../constants';
 
 class SecondaryNav extends Component {
-  getStyle(menu_cat){
-    switch(menu_cat){
-    case 'home': return style.homeMenuContainer;
-    case 'about-us':return style.aboutMenuContainer;
-    case 'contact-us':return style.contactMenuContainer;
-    case 'projects-work-products-publications':return style.projectsMenuContainer;
-    case 'post':return style.postMenuContainer;
+  getStyle (menuCat) {
+    switch (menuCat) {
+    case 'home':
+      return style.homeMenuContainer;
+    case 'about-us':
+      return style.aboutMenuContainer;
+    case 'contact-us':
+      return style.contactMenuContainer;
+    case 'projects-work-products-publications':
+      return style.projectsMenuContainer;
+    case 'post':
+      return style.postMenuContainer;
     }
   }
-  render() {
-    let container=[];
-    let page_title=this.props.title;
-    let page_id=this.props.id;
-    let parent_id=this.props.parent;
 
-    let menu_cat=(this.props.type==='post')?'post':MENU_IDS[this.props.parent];
-    let menu_container=this.getStyle(menu_cat);
+  render () {
+    let container = [];
+    let pageTitle = this.props.title;
+    let pageId = this.props.id;
+    let parentId = this.props.parent;
 
-    container.push(<Link to='/home'>Home </Link>);
-    if(this.props.type==='post'){
-      container.push(<Link to='/posts'> /News</Link>);
+    let menuCat = (this.props.type === 'post') ? 'post' : MENU_IDS[this.props.parent];
+    let menuContainer = this.getStyle(menuCat);
+
+    container.push(<Link key='home' to='/home'>Home </Link>);
+    if (this.props.type === 'post') {
+      container.push(<Link key='posts' to='/posts'> /News</Link>);
     }
-    else{
-      if(menu_cat==='home'){return (<div />);}
-      let parent=MENU_IDS[parent_id];
-      let parent_label=WORDPRESS_PAGES[parent].label;
-      container.push(<Link to={`/${parent}`}> /{parent_label}/</Link>);
-      if(parent_id !== page_id){
-        container.push(<span dangerouslySetInnerHTML={{ __html: page_title}} />);
+    else {
+      if (menuCat === 'home') {
+        return (<div />);
+      }
+      let parent = MENU_IDS[parentId];
+      let parentLabel = WORDPRESS_PAGES[parent].label;
+      container.push(<Link key={parentLabel} to={`/${parent}`}> /{parentLabel}/</Link>);
+      if (parentId !== pageId) {
+        container.push(<span dangerouslySetInnerHTML={{__html: pageTitle}} key='title' />);
       }
 
     }
     return (
-       <div className={menu_container}>
-         <div className='container-fluid'>
-           <div className={style.secondaryNavEmptyRow} />
-           <div className={`row ${style.secondaryNav}`}>
-             <div className='container'>
-               <div className='col-xs-12 col-sm-4'>{container} </div>
-               <div className='col-xs-12 col-sm-8 text-xs-right'>
-                 <h1 dangerouslySetInnerHTML={{ __html: page_title}} />
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
+      <div className={menuContainer}>
+        <div className='container-fluid'>
+          <div className={style.secondaryNavEmptyRow} />
+          <div className={`row ${style.secondaryNav}`}>
+            <div className='container'>
+              <div className='col-xs-12 col-sm-4'>{container} </div>
+              <div className='col-xs-12 col-sm-8 text-xs-right'>
+                <h1 dangerouslySetInnerHTML={{__html: pageTitle}} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 SecondaryNav.propTypes = {
-  id: PropTypes.string,
-  parent: PropTypes.string,
+  id: PropTypes.number,
+  parent: PropTypes.number,
   title: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
 };
