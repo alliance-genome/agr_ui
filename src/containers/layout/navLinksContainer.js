e7470445c9e59defafac9c20a6472aa5d7f6ecfd
felixgondwe <gondwefelix8@gmail.com>
2018-01-23 11:18:09 -0800

    minor header changes

--- src/containers/layout/navLinksContainer.js
+++ src/containers/layout/navLinksContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
+//import { Link } from 'react-router';
 
 import style from './style.css';
 import SubMenu from './subMenu';
