603a5d856c8300ce2787713cbe72f5c32f960a76
felixgondwe <gondwefelix8@gmail.com>
2018-03-09 15:19:28 -0800

    nav, twitter widget and other minor changes

--- src/containers/layout/navigation/middleHeader.js
+++ src/containers/layout/navigation/middleHeader.js
@@ -1,9 +1,3 @@
-/**
- * Middle header section
- * author: fgondwe@stanford.edu
- * date: 12/15/17
- */
-
  import React, {Component} from 'react';
  import MenuItems from './menuItems';
 
--- src/containers/layout/navigation/topHeader.js
+++ src/containers/layout/navigation/topHeader.js
@@ -1,14 +1,6 @@
-/**
- * Topheader: contact details, social media buttons, contact details
- * author: fgondwe@stanford.edu
- * date: 12/15/17
- */
-
- /* eslint-disable */
 import React, {Component} from 'react';
 import SocialMedia from '../socialMedia';
 import style from '../style.css';
-
 
 class Topheader extends Component{
   render(){
