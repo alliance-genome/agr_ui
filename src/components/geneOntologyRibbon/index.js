024bb0255637c9f1942c476be18bf7152ee62e85
Nathan Dunn <ndunn@me.com>
2018-04-20 15:01:55 -0700

    updated development notes

--- src/components/geneOntologyRibbon/index.js
+++ src/components/geneOntologyRibbon/index.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // import Ribbon from 'gene-ontology-ribbon/src/Ribbon';
 // import RibbonDataProvider from 'gene-ontology-ribbon/src/RibbonDataProvider';
-import Ribbon , { RibbonDataProvider } from 'gene-ontology-ribbon';
+import Ribbon , { RibbonDataProvider } from '@nathandunn/gene-ontology-ribbon';
 // import Ribbon, { RibbonDataProvider } from '@sibyl229/gene-ontology-ribbon';
-import '../../../node_modules/gene-ontology-ribbon/lib/index.css';
+import '@nathandunn/gene-ontology-ribbon/lib/index.css';
+// import '../../../node_modules/gene-ontology-ribbon/lib/index.css';
 
 
 class GeneOntologyRibbon extends Component {
