54284a5f8276b5af5019961a050582af6c2ee82b
Nathan Dunn <ndunn@me.com>
2018-04-20 18:01:28 -0700

    some minor fixes

--- src/containers/genePage/index.js
+++ src/containers/genePage/index.js
@@ -97,7 +97,7 @@ class GenePage extends Component {
         </Subsection>
 
         <Subsection title='Function – GO Annotations'>
-          <GeneOntologyRibbon id={data.primaryId} />
+          <GeneOntologyRibbon id={data.primaryId} slim='agr'/>
           {/*<GeneOntologyRibbon id={this.props.data.primaryId} slim='agr' />*/}
         </Subsection>
 
