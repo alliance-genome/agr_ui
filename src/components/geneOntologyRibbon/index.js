8903caf9c469122e9987d78ba8231e3e2c797269
Nathan Dunn <ndunn@me.com>
2018-04-23 10:00:47 -0700

    removed comments

--- src/components/geneOntologyRibbon/index.js
+++ src/components/geneOntologyRibbon/index.js
@@ -1,11 +1,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import Ribbon from 'gene-ontology-ribbon/src/Ribbon';
-// import RibbonDataProvider from 'gene-ontology-ribbon/src/RibbonDataProvider';
 import Ribbon , { RibbonDataProvider } from '@nathandunn/gene-ontology-ribbon';
-// import Ribbon, { RibbonDataProvider } from '@sibyl229/gene-ontology-ribbon';
-// import '@nathandunn/gene-ontology-ribbon/lib/index.css';
 import '../../../node_modules/@nathandunn/gene-ontology-ribbon/lib/index.css';
 
 
