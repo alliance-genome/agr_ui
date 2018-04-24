1808afa79d0d7cd65fe6974c17bf5fa2ae3f054a
Patrick Kalita <patrick.kalita@gmail.com>
2018-04-24 10:13:35 -0700

    Only differentiate wildtype and all links when both are present

--- src/containers/genePage/expressionLinks.js
+++ src/containers/genePage/expressionLinks.js
@@ -6,19 +6,22 @@ import {
   AttributeLabel,
   AttributeValue,
 } from '../../components/attribute';
-import ExternalLink from '../../components/externalLink';
 import CrossReferenceList from '../../components/crossReferenceList';
+import DataSourceLink from '../../components/dataSourceLink';
 
 const ExpressionLinks = ({wildTypeExpressionLink, allExpressionLink, otherExpressionLinks}) => {
   const wtLink = (wildTypeExpressionLink || [])[0];
   const allLink = (allExpressionLink || [])[0];
+  const wtLabel = wtLink ? (wtLink.prefix + ' (wild type)') : '';
+  const allLabel = allLink ? (allLink.prefix + (wtLink ? ' (all)' : '')) : '';
   return (
     <AttributeList>
       <AttributeLabel>Primary Sources</AttributeLabel>
       <AttributeValue placeholder='None'>
-        { (wtLink || allLink) && <div>
-          {wtLink && <div><ExternalLink href={wtLink.crossRefCompleteUrl}>{wtLink.prefix} (wild type)</ExternalLink></div>}
-          {allLink && <div><ExternalLink href={allLink.crossRefCompleteUrl}>{allLink.prefix} (all)</ExternalLink></div>}
+        { (wtLink || allLink) &&
+        <div>
+          <div><DataSourceLink reference={wtLink} text={wtLabel} /></div>
+          <div><DataSourceLink reference={allLink} text={allLabel} /></div>
         </div> }
       </AttributeValue>
 
