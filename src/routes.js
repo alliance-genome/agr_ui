bf30cdeabf7746588fd777ee9498ad6b63d387c0
Sibyl Gao <sibyl@wormbase.org>
2018-05-17 14:31:26 -0400

    fix 404 page due to react-router upgrade

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import {
   WordpressPage,
@@ -16,19 +16,21 @@ export default (
   <Route path='/' component={
     (props) => (
       <Layout {...props}>
-        <Route path='/' exact component={() => (
-          <WordpressPage slug='home' />
-        )} />
-        <Route component={Search} path='/search' />
-        <Route component={GenePage} path='/gene/:geneId' />
-        <Route component={DiseasePage} path='/disease/:diseaseId' />
-        <Route component={WordpressPostList} path='/posts' />
-        {/* <Redirect from='/posts/news' to='/posts' /> */}
-        <Route component={WordpressPost} path='/posts/:slug' />
-        {/* <Redirect from='/wordpress/:id' to='/:id' />  */}
-        {/* before links within user edited WordPress content is fixed, this path rewrite is necessary */}
-        <Route component={WordpressPage} path='/:slug' />
-        <Route component={NotFound} path="*" />
+        <Switch>
+          <Route path='/' exact component={() => (
+            <WordpressPage slug='home' />
+          )} />
+          <Route component={Search} path='/search' />
+          <Route component={GenePage} path='/gene/:geneId' />
+          <Route component={DiseasePage} path='/disease/:diseaseId' />
+          <Route component={WordpressPostList} path='/posts' />
+          {/* <Redirect from='/posts/news' to='/posts' /> */}
+          <Route component={WordpressPost} path='/posts/:slug' />
+          {/* <Redirect from='/wordpress/:id' to='/:id' />  */}
+          {/* before links within user edited WordPress content is fixed, this path rewrite is necessary */}
+          <Route component={WordpressPage} path='/:slug' />
+          <Route component={NotFound} />
+        </Switch>
       </Layout>
     )
   } />
