ca613232edcc2eb9972ee623dbde9e6b65c126a7
Bryon Czoch <bryon.czoch@gmail.com>
2018-06-11 14:36:47 -0400

    example reactstrap carousel

--- src/containers/wordpress/wordpressPage.js
+++ src/containers/wordpress/wordpressPage.js
@@ -3,62 +3,32 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import style from './style.scss';
-import HeadMetaTags from '../../components/headMetaTags';
-import LoadingPage from '../../components/loadingPage';
-import SecondaryNav from './secondaryNav';
-
-import { fetchWordpressPage } from '../../actions/wordpress';
+import Example from '../../components/carousel';
 
 import {
   selectLoading,
   selectPage
 } from '../../selectors/wordpressSelectors';
-import NotFound from '../../components/notFound';
 
 class WordpressPage extends Component {
   componentDidMount() {
     this.fetchPage();
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.slug !== prevProps.slug) {
-      this.fetchPage();
-    }
-  }
-
-  fetchPage() {
-    const { dispatch, slug } = this.props;
-    dispatch(fetchWordpressPage(slug));
-  }
 
   render() {
-    const { loading, page, slug } = this.props;
-
-    if (loading) {
-      return <LoadingPage />;
-    }
-
-    if (!page) {
-      return <NotFound />;
-    }
-
-    const title = page.title.rendered;
-    let parentId = (page.parent > 0) ? page.parent : page.id;
     return (
       <div className={style.wordPressContainer}>
-        <HeadMetaTags title={title} />
-        {slug !== 'home' && <SecondaryNav parent={parentId} title={title} type='page' />}
-        <div dangerouslySetInnerHTML={{ __html: page.content.rendered}} />
+        <Example />
       </div>
     );
   }
 }
 
 WordpressPage.propTypes = {
-  dispatch: PropTypes.func,
   loading: PropTypes.bool,
   page: PropTypes.object,
-  slug: PropTypes.string,
+
 };
 
 const mapStateToProps = (state) => {
