b8d8373fcf6ff6ce9d6030a7efff37c3bca14f9b
Nathan Dunn <ndunn@me.com>
2018-08-23 12:18:00 -0700

    we fixed highlihting

--- demo/src/JSDemo.js
+++ demo/src/JSDemo.js
@@ -1,6 +1,23 @@
 import GenomeFeatureViewer from "../../src/GenomeFeatureViewer";
 
 
+var options1 = {
+    chromosome: 5,
+    start: 28456815,
+    end: 28467256,
+    genome: "Mus musculus",
+    // highlightNames: ['Shh']
+};
 
-GenomeFeatureViewer({chromosome:5, start:28456815, end:28467256, genome:"Mus musculus"},"#viewer2");
-GenomeFeatureViewer({chromosome:5, start:75574916, end:75656722, genome:"Mus musculus"}, "#viewer");
+GenomeFeatureViewer(options1,"#viewer2");
+
+
+var options2 = {
+    chromosome: 5,
+    start: 75574916,
+    end: 75656722,
+    genome: "Mus musculus",
+    highlightNames: ['Kit']
+};
+
+GenomeFeatureViewer(options2, "#viewer");
--- src/GenomeFeatureViewer.js
+++ src/GenomeFeatureViewer.js
@@ -1,31 +1,41 @@
 import DrawViewer from './DrawViewer';
-import { ApolloService } from './services/services';
+import {ApolloService} from './services/services';
 /*
  Main entry for creating the genome viewer.
  Currently only draws isoforms at a global level
  @Param config: a set of configurations for the isoform track
     {
-        chromosome:5,
-        start:75574916,
-        end:75656722,
-        genome:"Mus musculus"
+        chromosome: 5,
+        start: 75574916,
+        end: 75656722,
+        genome: "Mus musculus",
+        highlightNames: ['Kit']
     }
 
   @Param svg_target: a DOM element id, where the viewer will be drawn.
 */
-var GenerateGenomeView = function(config, svg_target)
-{
-  // TODO: 
-  // Config should be a set of tracks and we should be drawing 
-  // based on track type to the svg
-  let externalLocationString = config.chromosome + ':' + config.start + '..' + config.end;
-  var dataUrl = "https://agr-apollo.berkeleybop.io/apollo/track/" + encodeURI(config.genome) + "/All%20Genes/" + encodeURI(externalLocationString) + ".json";
-  
-  // TODO: We can still make this more robust.
-  var apolloService = new ApolloService()
-  apolloService.GetIsoformTrack(dataUrl).then((data) =>{
+var GenerateGenomeView = function (config, svg_target) {
+    // TODO:
+    // Config should be a set of tracks and we should be drawing
+    // based on track type to the svg
+    let {chromosome, start, end, genome, server, track, highlightNames} = config;
+    server = server ? server : 'https://agr-apollo.berkeleybop.io/apollo/';
+    track = track ? track : 'All Genes';
+    let nameString = '';
+
+    for (let nameIndex in highlightNames) {
+        nameString += nameIndex == 0 ? '?' : '&';
+        nameString += 'name=' + highlightNames[nameIndex];
+    }
+
+    let externalLocationString = chromosome + ':' + start + '..' + end;
+    let dataUrl = server + '/track/' + encodeURI(genome) + '/' + track + '/' + encodeURI(externalLocationString) + '.json' + nameString;
+
+    // TODO: We can still make this more robust.
+    var apolloService = new ApolloService();
+    apolloService.GetIsoformTrack(dataUrl).then((data) => {
         DrawViewer(data, svg_target);
-  });
+    });
 };
 
 export default GenerateGenomeView;
