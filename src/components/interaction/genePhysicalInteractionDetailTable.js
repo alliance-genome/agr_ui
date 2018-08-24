47e954abbef425949ef7d2de08d978f028918e06
Sibyl <sibyl@wormbase.org>
2018-08-24 17:56:31 -0400

    wip render physical interaction detail table

--- src/components/interaction/genePhysicalInteractionDetailTable.js
+++ src/components/interaction/genePhysicalInteractionDetailTable.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  LocalDataTable,
+} from '../dataTable';
 
 export default class GenePhysicalInteractionDetailTable extends React.Component {
   render() {
+    const columns = [
+      {
+        field: 'geneB',
+        label: 'Interactor gene',
+        isKey: true, // TODO: remove this later
+        format: (gene) => (gene.symbol),
+        width: '75px',
+      },
+    ];
     return (
-      <p>A place holder for physical interaction table {
-        JSON.stringify(this.props.data)
-      }</p>
-//      <LocalDataTable data={this.props} />
+      <LocalDataTable
+        columns={columns}
+        data={this.props.data}
+        filename={this.props.filename}
+        paginated
+      />
     );
   }
 }
