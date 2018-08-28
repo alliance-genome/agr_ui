803d84d38b8e4dce5c9fac2c16d899371d85cee7
Patrick Kalita <patrick.kalita@gmail.com>
2018-08-28 12:35:07 -0700

    Create new component for ortholog stringency selection

--- src/components/expression/expressionComparisonRibbon.js
+++ src/components/expression/expressionComparisonRibbon.js
@@ -1,12 +1,23 @@
+/* eslint-disable react/no-set-state */
 import React from 'react';
 import ControlsContainer from '../controlsContainer';
+import { StringencySelector } from '../orthology';
+import { STRINGENCY_HIGH } from '../orthology/constants';
 
 class ExpressionComparisonRibbon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stringency: STRINGENCY_HIGH
+    };
+  }
+
   render() {
     return (
       <React.Fragment>
         <ControlsContainer>
           <b>Compare to ortholog genes</b>
+          <StringencySelector defaultLevel={this.state.stringency} onChange={s => this.setState({stringency: s})} />
         </ControlsContainer>
       </React.Fragment>
     );
