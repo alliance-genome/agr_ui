ba1151ba4d2cdb4783c49e661ec2ee878ab7cd86
Nathan Dunn <ndunn@me.com>
2018-08-29 12:56:29 -0500

    a proposal vis AGR-1160

--- src/components/headMetaTags.js
+++ src/components/headMetaTags.js
@@ -4,9 +4,23 @@ import Helmet from 'react-helmet';
 
 class HeadMetaTags extends Component {
 
+
   render() {
 
     let title = `${this.props.title} | Alliance of Genome Resources`;
+    let data = this.props.data;
+    let schema = undefined;
+    if (data) {
+      schema = {
+        type: 'application/ld+json',
+        innerHTML: JSON.stringify({
+          '@link': 'http://www.alliancegenome.org/gene/'+data.primaryId,
+          '@type': 'DataSet',
+          identifier: data.primaryId,
+          name: data.symbol,
+        }),
+      };
+    }
 
     return (
       <div>
@@ -16,14 +30,17 @@ class HeadMetaTags extends Component {
           ]}
           title={title}
         />
+        {schema &&
+        <Helmet script={[schema]} />
+        }
       </div>
     );
   }
 }
 
 HeadMetaTags.propTypes = {
+  data: PropTypes.any,
   title: PropTypes.string
-
 };
 
 export default HeadMetaTags;
