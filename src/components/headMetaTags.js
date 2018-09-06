f26461249dba84ca5a5152dc97e66aed8b9f5348
Nathan Dunn <ndunn@me.com>
2018-09-06 15:56:27 -0700

    made compliant and added additoinal fields

--- src/components/headMetaTags.js
+++ src/components/headMetaTags.js
@@ -10,14 +10,25 @@ class HeadMetaTags extends Component {
     let title = `${this.props.title} | Alliance of Genome Resources`;
     let data = this.props.data;
     let schema = undefined;
+    let keywords =['gene',data.name,data.dataProvider.replace('\n',' '),data.symbol,...data.synonyms,data.species,data.primaryId];
     if (data) {
       schema = {
         type: 'application/ld+json',
         innerHTML: JSON.stringify({
-          '@link': 'http://www.alliancegenome.org/gene/'+data.primaryId,
-          '@type': 'DataSet',
-          identifier: data.primaryId,
+          '@context': 'http://schema.org',
+          '@type': 'Dataset',
           name: data.symbol,
+          description: data.automatedGeneSynopsis + ' ' + (data.geneSynopsis || data.geneSynopsisUrl || ''),
+          url: 'http://www.alliancegenome.org/gene/' + data.primaryId,
+          keywords: keywords.join(' '),
+          includedInDataCatalog: 'http://www.alliancegenome.org',
+          creator:{
+            '@type':'Organization',
+            'name':'Alliance of Genome Resources'
+          },
+          version: '2.0',
+          license: 'CC BY 4.0',
+          identifier: data.primaryId,
         }),
       };
     }
