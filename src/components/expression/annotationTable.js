f8864320dc0d45eeb00ee82dd3212d09fd98d191
Patrick Kalita <patrick.kalita@gmail.com>
2018-09-26 15:06:19 -0700

    Add more columns to associations table

--- src/components/expression/annotationTable.js
+++ src/components/expression/annotationTable.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import isEqual from 'lodash.isequal';
 
 import { selectAnnotations } from '../../selectors/expressionSelectors';
 import { fetchExpressionAnnotations } from '../../actions/expression';
-import { RemoteDataTable } from '../dataTable';
-
-const columns = [
-  {
-    field: 'key',
-    isKey: true,
-    hidden: true,
-  },
-  {
-    field: 'species',
-    label: 'Species',
-  },
-  {
-    field: 'gene',
-    label: 'Gene',
-  },
-  {
-    field: 'location',
-    label: 'Location',
-  },
-  {
-    field: 'stage',
-    label: 'Stage',
-  }
-];
+import { RemoteDataTable, ReferenceCell } from '../dataTable';
 
 class AnnotationTable extends React.Component {
   constructor(props) {
@@ -58,12 +35,54 @@ class AnnotationTable extends React.Component {
       return null;
     }
 
+    const columns = [
+      {
+        field: 'key',
+        isKey: true,
+        hidden: true,
+      },
+      {
+        field: 'species',
+        label: 'Species',
+        format: s => <i>{s}</i>,
+      },
+      {
+        field: 'gene',
+        label: 'Gene',
+        format: g => <Link to={'/' + g.geneID}>{g.symbol}</Link>
+      },
+      {
+        field: 'location',
+        label: 'Location',
+      },
+      {
+        field: 'stage',
+        label: 'Stage',
+      },
+      {
+        field: 'assay',
+        label: 'Assay',
+      },
+      {
+        field: 'source',
+        label: 'Source',
+      },
+      {
+        field: 'references',
+        label: 'References',
+        format: ReferenceCell
+      }
+    ];
+
     const data = annotations && annotations.data && annotations.data.results.map(result => ({
-      key: `${result.gene.geneID}-${result.termName}-${result.stage.stageID}`,
+      key: `${result.gene.geneID}-${result.termName}-${result.stage ? result.stage.stageID : 'other'}`,
       species: result.gene.speciesName,
-      gene: result.gene.symbol,
+      gene: result.gene,
       location: result.termName,
-      stage: result.stage.name,
+      stage: result.stage && result.stage.name,
+      assay: result.assay.name,
+      source: result.dataProvider,
+      references: result.publications,
     }));
     return (
       <RemoteDataTable
