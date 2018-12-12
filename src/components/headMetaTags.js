bdb24394ce677c181f4a00cecd7bd760dd86343a
Nathan Dunn <ndunn@me.com>
2018-12-12 11:23:10 -0800

    Jsonld agr 1318 agr 1319 agr 1345 (#355)
    * updaeted to allow for multiple schemas
    
    * started on adding the minimal bioschemas
    
    * added a bioschemas start
    
    * fixed image and removed extra slash

--- src/components/headMetaTags.js
+++ src/components/headMetaTags.js
@@ -9,16 +9,51 @@ class HeadMetaTags extends Component {
 
     let title = `${this.props.title} | Alliance of Genome Resources`;
     let data = this.props.data;
-    let schema = undefined;
+    let schemas = [];
+
+
+    // general view
+    schemas.push({
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        '@context': 'http://schema.org',
+        '@type': 'Organization',
+        'url': 'https://www.alliancegenome.org',
+        'logo': 'https://i1.wp.com/alliancegenome.files.wordpress.com/2016/11/banner_1_flyin_logo.png',
+        'email': 'alliance-helpdesk@lists.stanford.edu'
+      })
+    });
+
+
+    // specify actions
+    schemas.push({
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        '@context': 'http://schema.org',
+        '@type': 'WebSite',
+        'url': 'https://www.alliancegenome.org/',
+        'potentialAction': {
+          '@type': 'SearchAction',
+          'target': 'https://www.alliancegenome.org/search?q={term}',
+          'query-input': 'required name=term'
+        }
+      }),
+    });
+
+
     if (data) {
       let keywords = ['gene', data.dataProvider.replace('\n', ' '), data.symbol, ...data.synonyms, data.species, data.primaryId];
-      schema = {
+
+      schemas.push({
         type: 'application/ld+json',
         innerHTML: JSON.stringify({
           '@context': 'http://schema.org',
           '@type': 'Dataset',
           '@id': data.primaryId,
           name: data.symbol,
+          dateCreated: new Date(data.dateProduced),
+          datePublished: new Date(data.dateProduced),
+          dateModified: new Date(data.dateProduced),
           description: data.automatedGeneSynopsis + ' ' + (data.geneSynopsis || data.geneSynopsisUrl || ''),
           url: 'http://www.alliancegenome.org/gene/' + data.primaryId,
           keywords: keywords.join(' '),
@@ -30,7 +65,35 @@ class HeadMetaTags extends Component {
           version: '2.0',
           license: 'CC BY 4.0',
         }),
-      };
+      });
+
+      // based on this: https://github.com/BioSchemas/specifications/tree/master/Gene/examples
+      // bioschemas section
+      schemas.push({
+        type: 'application/ld+json',
+        innerHTML: JSON.stringify({
+          '@context': [
+            {
+              'bs': 'http://bioschemas.org/'
+            },
+            'http://schema.org',
+            {
+              '@base': 'http://schema.org'
+            }
+          ],
+          '@type': [
+            'bs:Gene'
+          ],
+          identifier: data.primaryId,
+          name: data.symbol,
+          url: `https://www.alliancegenome.org/gene/${data.primaryId}`,
+          dateCreated: new Date(data.dateProduced),
+          datePublished: new Date(data.dateProduced),
+          dateModified: new Date(data.dateProduced),
+          description: data.automatedGeneSynopsis + ' ' + (data.geneSynopsis || data.geneSynopsisUrl || ''),
+          // 'sameAs': `https://zfin.org/ZDB-GENE-001103-2`, // TODO: add resolver here
+        }),
+      });
     }
 
     return (
@@ -39,7 +102,7 @@ class HeadMetaTags extends Component {
           meta={[
             {property: 'og:title', content: {title}}
           ]}
-          script={(schema ? [schema] : [])}
+          script={(schemas)}
           title={title}
         />
       </div>
