7d0fe10ef84ac05dec120aea9cc6f2f67212aff4
Patrick Kalita <pkalita@zfin.org>
2019-02-06 16:01:49 -0800

    Add gene descriptions files to downloads page
    Also converts page to use data page infrastructure for layout.

--- src/containers/downloadsPage/index.js
+++ src/containers/downloadsPage/index.js
@@ -1,43 +1,136 @@
 import React from 'react';
-import { AttributeList, AttributeLabel, AttributeValue } from '../../components/attribute';
-import { UncontrolledCollapse } from 'reactstrap';
+import { Table, UncontrolledCollapse } from 'reactstrap';
 import ExternalLink from '../../components/externalLink';
-import HeadMetaTags from '../../components/headMetaTags';
+import Subsection from '../../components/subsection';
+import { DataPage, PageData, PageHeader, PageNav } from '../../components/dataPage';
 
 class DownloadsPage extends React.Component {
   render() {
+    const style = {
+      backgroundColor: '#2598C5',
+      color: '#ffffff',
+      padding: '0.1rem 0.3rem',
+      borderRadius: '5px',
+      marginRight: '0.4rem',
+      fontSize: '0.8rem',
+    };
+    const INTERACTIONS = 'Interactions';
+    const GENE_DESCRIPTIONS = 'Gene Descriptions';
+    const SECTIONS = [{name: GENE_DESCRIPTIONS}, {name: INTERACTIONS}];
+    const TITLE = 'Downloads';
     return (
-      <div className='container'>
-        <HeadMetaTags title='Downloads' />
-        <h1>
-          Downloads
-          <hr />
-        </h1>
-        <AttributeList>
-          <AttributeLabel>Interactions</AttributeLabel>
-          <AttributeValue>
-            <a href='https://download.alliancegenome.org/INT/Alliance_molecular_interactions.tar.gz'>
-              Alliance_molecular_interactions.tar.gz
-            </a>
-            <i
-              className='fa fa-fw fa-question-circle text-primary'
-              id='interactions-help'
-              style={{cursor: 'pointer', marginLeft: '1rem'}}
-            />
-            <UncontrolledCollapse toggler='#interactions-help'>
-              <p>
-                This file provides a set of annotations of molecular interactions for
-                genes and gene products for all Alliance species (human, rat, mouse, zebrafish, fruit fly, nematode, and
-                yeast). The file is in the <ExternalLink href='https://github.com/HUPO-PSI/miTab'>PSI-MI TAB format</ExternalLink>,
-                a tab-delimited format established by the <ExternalLink href='http://www.psidev.info'>HUPO Proteomics Standards
-                Initiative</ExternalLink> Molecular Interactions (PSI-MI) working group. The interaction data is sourced from
-                Alliance members WormBase and FlyBase, as well as the <ExternalLink href='http://www.imexconsortium.org'>IMEx
-                consortium</ExternalLink> and the <ExternalLink href='https://thebiogrid.org'>BioGRID database</ExternalLink>.
-              </p>
-            </UncontrolledCollapse>
-          </AttributeValue>
-        </AttributeList>
-      </div>
+      <DataPage title={TITLE}>
+        <PageNav entityName={TITLE} sections={SECTIONS} />
+        <PageData>
+          <PageHeader entityName={TITLE} />
+          <Subsection title={GENE_DESCRIPTIONS}>
+            <Table size='sm'>
+              <thead>
+                <tr>
+                  <th>Description</th>
+                  <th>Download</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><i>Caenorhabditis elegans</i> genes</td>
+                  <td>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/WB_gene_desc_latest.json' style={style}>JSON</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/WB_gene_desc_latest.tsv' style={style}>TSV</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/WB_gene_desc_latest.txt' style={style}>TXT</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><i>Danio rerio</i> genes</td>
+                  <td>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/ZFIN_gene_desc_latest.json' style={style}>JSON</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/ZFIN_gene_desc_latest.tsv' style={style}>TSV</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/ZFIN_gene_desc_latest.txt' style={style}>TXT</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><i>Drosophila melanogaster</i> genes</td>
+                  <td>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/FB_gene_desc_latest.json' style={style}>JSON</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/FB_gene_desc_latest.tsv' style={style}>TSV</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/FB_gene_desc_latest.txt' style={style}>TXT</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><i>Homo sapiens</i> genes</td>
+                  <td>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/HUMAN_gene_desc_latest.json' style={style}>JSON</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/HUMAN_gene_desc_latest.tsv' style={style}>TSV</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/HUMAN_gene_desc_latest.txt' style={style}>TXT</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><i>Mus musculus</i> genes</td>
+                  <td>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/MGI_gene_desc_latest.json' style={style}>JSON</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/MGI_gene_desc_latest.tsv' style={style}>TSV</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/MGI_gene_desc_latest.txt' style={style}>TXT</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><i>Rattus norvegicus</i> genes</td>
+                  <td>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/RGD_gene_desc_latest.json' style={style}>JSON</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/RGD_gene_desc_latest.tsv' style={style}>TSV</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/RGD_gene_desc_latest.txt' style={style}>TXT</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><i>Saccharomyces cerevisiae</i> genes</td>
+                  <td>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/SGD_gene_desc_latest.json' style={style}>JSON</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/SGD_gene_desc_latest.tsv' style={style}>TSV</a>
+                    <a href='http://reports.alliancegenome.org/gene-descriptions/SGD_gene_desc_latest.txt' style={style}>TXT</a>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+
+          </Subsection>
+
+          <Subsection title={INTERACTIONS}>
+            <Table size='sm'>
+              <thead>
+                <tr>
+                  <th>Description</th>
+                  <th>Download</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    Molecular interactions
+                    <i
+                      className='fa fa-fw fa-question-circle text-primary'
+                      id='interactions-help'
+                      style={{cursor: 'pointer', marginLeft: '1rem'}}
+                    />
+                    <UncontrolledCollapse toggler='#interactions-help'>
+                      <p>
+                        This file provides a set of annotations of molecular interactions for
+                        genes and gene products for all Alliance species (human, rat, mouse, zebrafish, fruit fly, nematode, and
+                        yeast). The file is in the <ExternalLink href='https://github.com/HUPO-PSI/miTab'>PSI-MI TAB format</ExternalLink>,
+                        a tab-delimited format established by the <ExternalLink href='http://www.psidev.info'>HUPO Proteomics Standards
+                        Initiative</ExternalLink> Molecular Interactions (PSI-MI) working group. The interaction data is sourced from
+                        Alliance members WormBase and FlyBase, as well as the <ExternalLink href='http://www.imexconsortium.org'>IMEx
+                        consortium</ExternalLink> and the <ExternalLink href='https://thebiogrid.org'>BioGRID database</ExternalLink>.
+                      </p>
+                    </UncontrolledCollapse>
+                  </td>
+                  <td>
+                    <a href='https://download.alliancegenome.org/INT/Alliance_molecular_interactions.tar.gz' style={style}>TXT</a>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Subsection>
+        </PageData>
+      </DataPage>
     );
   }
 }
