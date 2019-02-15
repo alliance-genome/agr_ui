6765029f1a9e58808b95e77825ba9db7f3de19af
Felix Gondwe <zungwala@gmail.com>
2019-02-14 16:25:03 -0800

    Replace jquery with fetch-api and other minor changes
    May not work in older browsers as polyfill is not included yet

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -1,13 +1,44 @@
-import $ from 'jquery';
+function fetchWrapper(url, options){
+  return fetch(url, options).then(handleResponse, handleNetworkError);
+}
 
-const TIMEOUT = 30000;
+function handleResponse(response){
+  if(response.ok){
+    return response.json();
+  }
+  else{
+    return response.json().then(function(error){
+      throw error;
+    });
+  }
+}
 
-export default function fetchData(_url, options={}) {
+function handleNetworkError(error){
+  throw {
+    msg: error.message
+  };
+}
+
+export default function fetchData(_url, options = {}) {
+  const VERBS = ['POST', 'PUT', 'DELETE'];
   let _type = options.type || 'GET';
-  return $.ajax({
-    url : _url,
-    type : _type,
-    dataType:'json',
-    timeout: TIMEOUT,
-  });
+  let headers ={
+    'Content-type': 'application/json'
+  };
+
+  if(VERBS.indexOf(_type) > -1){
+    return fetchWrapper(_url, {
+      method: _type,
+      mode: 'cors',
+      body: JSON.stringify(options.data),
+      headers: headers
+    });
+  }
+  else{
+    return fetchWrapper(_url, {
+      method: _type,
+      mode: 'cors',
+      headers: headers,
+    });
+  }
 }
