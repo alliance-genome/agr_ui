3dc85bb61c7f49f70d34899041f39da210c896d4
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-03-05 06:05:33 -0800

    Using the new Ribbon (1.9.2): appearances, multi entities, correcting summaryRibbon

--- src/components/geneOntologyRibbon/index.js
+++ src/components/geneOntologyRibbon/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Ribbon, {RibbonDataProvider} from '@geneontology/ribbon';
+import { POSITION } from '@geneontology/ribbon/lib/enums';
 import HorizontalScroll from '../horizontalScroll';
 import NoData from '../noData';
 import LoadingSpinner from '../loadingSpinner';
@@ -8,9 +9,9 @@ import LoadingSpinner from '../loadingSpinner';
 
 class GeneOntologyRibbon extends Component {
 
-  static hasBlockData(blocks){
-    for (let b of blocks) {
-      if (b.uniqueIDs.length > 0) {
+  static hasBlockData(entities){
+    for (let entity of entities) {
+      if (entity.blocks.uniqueIDs.length > 0) {
         return true;
       }
     }
@@ -22,22 +23,26 @@ class GeneOntologyRibbon extends Component {
     return (
       <RibbonDataProvider subject={id}>
         {
-          ({blocks, config, dataError, dataReceived, eco_list, title}) => {
+          ({entities, config, dataError, dataReceived}) => {
             if (!dataReceived) {
               return <LoadingSpinner />;
             }
-            if (dataError || !GeneOntologyRibbon.hasBlockData(blocks)) {
+            // if (dataError || !GeneOntologyRibbon.hasBlockData(entities)) {
+            //   return <NoData />;
+            // }
+            if (dataError) {
               return <NoData />;
             }
             return (
               <HorizontalScroll width={800}>
                 <Ribbon 
-                  blocks={blocks}
+                  borderBottom
                   config={config}
-                  eco_list={eco_list}
+                  entities={entities}
+                  entityLabel={POSITION.NONE}
+                  oddEvenColor={false}
                   showing={false}
                   subject={id}
-                  title={title}
                 />
               </HorizontalScroll>
             );
