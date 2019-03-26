035591137c891ef1655b946ca0c0d95973c26b3a
Patrick Kalita <pkalita@zfin.org>
2019-03-26 14:43:29 -0700

    Convert disease associations table to new format

--- src/actions/disease.js
+++ src/actions/disease.js
@@ -43,10 +43,10 @@ export const fetchAssociations = function (id, opts) {
     dispatch(fetchAssociationsRequest());
 
     // compensate for the fact that we replace underscore in this field for display
-    let associationTypeFilter = opts.filters.find(filter => filter.name === 'associationType');
-    if (associationTypeFilter) {
-      associationTypeFilter.value = associationTypeFilter.value.replace(/ /g, '_');
-    }
+    // let associationTypeFilter = opts.filters.find(filter => filter.name === 'associationType');
+    // if (associationTypeFilter) {
+    //   associationTypeFilter.value = associationTypeFilter.value.replace(/ /g, '_');
+    // }
 
     return fetchData(`/api/disease/${id}/associations?${buildTableQueryString(opts)}`)
       .then((data) => dispatch(fetchAssociationsSuccess(data)))
