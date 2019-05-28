9aef4922c278623730b68d9f19c5be5bd34eb0be
Patrick Kalita <pkalita@zfin.org>
2019-05-28 09:38:37 -0700

    Add sort/filter query parameters to download url

--- src/components/dataTable/remoteDataTable.js
+++ src/components/dataTable/remoteDataTable.js
@@ -24,6 +24,7 @@ import ColumnHeader from './columnHeader';
 import DropdownTextFilter from './dropdownTextFilter';
 import DropdownCheckboxFilter from './dropdownCheckboxFilter';
 import HorizontalScroll from '../horizontalScroll';
+import { buildTableQueryString } from '../../lib/utils';
 
 class RemoteDataTable extends Component {
   constructor(props) {
@@ -167,7 +168,7 @@ class RemoteDataTable extends Component {
             )
           }
         </PaginationProvider>
-        <DownloadButton downloadUrl={downloadUrl} />
+        <DownloadButton downloadUrl={`${downloadUrl}?${buildTableQueryString({sort, filters})}`} />
       </div>
     );
   }
