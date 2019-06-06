0c84717ca6f4abe40955e7d39931a3f7d5828a87
Patrick Kalita <pkalita@zfin.org>
2019-06-06 12:24:19 -0700

    Remove unused import

--- src/components/expression/annotationTable.js
+++ src/components/expression/annotationTable.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import isEqual from 'lodash.isequal';
 
