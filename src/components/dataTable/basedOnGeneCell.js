9d4c9e71117708aaa3298974fbd0e01d8fc07150
Felix Gondwe <zungwala@gmail.com>
2019-06-19 15:54:10 -0700

    - Fix column and filter - BasedOn column, geneticEntityType filter - No known side effects

--- src/components/dataTable/basedOnGeneCell.js
+++ src/components/dataTable/basedOnGeneCell.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
 import { shortSpeciesName} from '../../lib/utils';
+import { CollapsibleList } from '../../components/collapsibleList';
 
-const BasedOnGeneCell = (gene) => {
-  if(gene){
-    let cell = (<Link key={gene.id} to={`/gene/${gene.id}`}>{gene.symbol}({shortSpeciesName(gene.species.taxonId)})</Link>);
+const BasedOnGeneCell = (genes) => {
+  if(genes){
+    let cell = (
+      <CollapsibleList collapsedSize={genes.length}>
+        {genes.map(gene => (
+          <Link
+            key={gene.id}
+            to={`/gene/${gene.id}`}
+          >{gene.symbol}({shortSpeciesName(gene.species.taxonId)})
+          </Link>
+        ))}
+
+      </CollapsibleList>);
     return cell;
   }
   else{
