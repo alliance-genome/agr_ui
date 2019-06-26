2f47b3fd5503fc328b601157b4a873c720c322b0
Felix Gondwe <zungwala@gmail.com>
2019-06-26 11:41:09 -0700

    - Fix AGR-1665, AGR-1608 - Update based on column disease annotation col, source expression table col - No known side effects

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -7,7 +7,6 @@ const CONTROLLER = new AbortController();
 
 //resolve promise
 function fetchWrapper(url, options){
-  /* eslint-disable no-debugger */
   return fetch(url, options).then(handleResponse, handleNetworkError);
 }
 
