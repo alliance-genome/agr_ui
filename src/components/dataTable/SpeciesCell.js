29ec029daf566042bf1c9a59de0c3175217e825e
Patrick Kalita <pkalita@zfin.org>
2019-09-25 14:49:34 -0700

    Fix linter warnings

--- src/components/dataTable/SpeciesCell.js
+++ src/components/dataTable/SpeciesCell.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SpeciesCell = ({species}) => <i>{species.name}</i>;
+
+SpeciesCell.propTypes = {
+  species: PropTypes.object,
+};
 
 export default SpeciesCell;
