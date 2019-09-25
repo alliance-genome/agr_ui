efa35638f1c0da7e6d3d6fcf303958cd0eaa0bc1
Patrick Kalita <pkalita@zfin.org>
2019-09-25 12:27:48 -0700

    Add models table to disease page

--- src/actions/diseaseActions.js
+++ src/actions/diseaseActions.js
@@ -4,6 +4,7 @@ import {createFetchAction} from '../lib/createActions';
 export const FETCH_DISEASE = 'FETCH_DISEASE';
 export const FETCH_GENE_ASSOCIATIONS = 'FETCH_GENE_ASSOCIATIONS';
 export const FETCH_ALLELE_ASSOCIATIONS = 'FETCH_ALLELE_ASSOCIATIONS';
+export const FETCH_MODEL_ASSOCIATIONS = 'FETCH_MODEL_ASSOCIATIONS';
 
 export const fetchDisease = createFetchAction(FETCH_DISEASE, id => `/api/disease/${id}`);
 export const fetchGeneAssociations = createFetchAction(
@@ -13,4 +14,8 @@ export const fetchGeneAssociations = createFetchAction(
 export const fetchAlleleAssociations = createFetchAction(
   FETCH_ALLELE_ASSOCIATIONS,
   (id, opts) => `/api/disease/${id}/alleles?${buildTableQueryString(opts)}`
+);
+export const fetchModelAssociations = createFetchAction(
+  FETCH_MODEL_ASSOCIATIONS,
+  (id, opts) => `/api/disease/${id}/models?${buildTableQueryString(opts)}`
 );
