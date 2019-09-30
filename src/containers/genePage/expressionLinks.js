1b5e44c255aa0fbf16f3ac97c429dfc6f9b339b7
Patrick Kalita <pkalita@zfin.org>
2019-09-30 15:54:22 -0700

    Add link back to expression images at MODs

--- src/containers/genePage/expressionLinks.js
+++ src/containers/genePage/expressionLinks.js
@@ -11,6 +11,7 @@ import CrossReferenceList from '../../components/crossReferenceList';
 const ExpressionLinks = ({
   allExpressionCrossReference,
   geneDataProvider,
+  imagesCrossReference,
   otherExpressionCrossReferences,
   spellCrossReference,
   wildtypeExpressionCrossReference
@@ -18,15 +19,28 @@ const ExpressionLinks = ({
 
   // override display name to differentiate wild type and all expression
   if (wildtypeExpressionCrossReference) {
-    wildtypeExpressionCrossReference.displayName = geneDataProvider + ' (wild type)';
+    wildtypeExpressionCrossReference = {
+      ...wildtypeExpressionCrossReference,
+      displayName: geneDataProvider + ' (wild type)'
+    };
   }
   if (allExpressionCrossReference) {
-    allExpressionCrossReference.displayName = geneDataProvider + (wildtypeExpressionCrossReference ? ' (all)' : '');
+    allExpressionCrossReference = {
+      ...allExpressionCrossReference,
+      displayName: geneDataProvider + (wildtypeExpressionCrossReference ? ' (all)' : '')
+    };
+  }
+  if (imagesCrossReference) {
+    imagesCrossReference = {
+      ...imagesCrossReference,
+      displayName: geneDataProvider + ' (images)'
+    };
   }
 
   const primarySources = [
     allExpressionCrossReference,
     wildtypeExpressionCrossReference,
+    imagesCrossReference,
     spellCrossReference
   ].filter(ref => ref !== undefined);
 
@@ -44,7 +58,7 @@ const ExpressionLinks = ({
       <AttributeLabel>Primary Sources</AttributeLabel>
       <AttributeValue placeholder='None'>
         {primarySources && primarySources.length &&
-          <CrossReferenceList collapsible={false} crossReferences={primarySources} />
+          <CrossReferenceList collapsible={false} crossReferences={primarySources} sort={false} />
         }
       </AttributeValue>
 
@@ -62,6 +76,7 @@ const ExpressionLinks = ({
 ExpressionLinks.propTypes = {
   allExpressionCrossReference: PropTypes.object,
   geneDataProvider: PropTypes.string,
+  imagesCrossReference: PropTypes.object,
   otherExpressionCrossReferences: PropTypes.array,
   spellCrossReference: PropTypes.object,
   wildtypeExpressionCrossReference: PropTypes.object,
