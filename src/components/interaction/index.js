2f7e8cd1fdc2231fc827af6a86acfe0a47414412
Howie Motenko <howie.motenko@jax.org>
2019-10-01 11:35:56 -0400

    fixed typo

--- src/components/interaction/index.js
+++ src/components/interaction/index.js
@@ -1,3 +1,3 @@
 export { default as GenePhysicalInteractionDetailTable } from './genePhysicalInteractionDetailTable';
 export { default as GeneInteractionCrossReference } from './geneInteractionCrossReference';
-export { default as InteractionUserGuide } from './interactionUSerGuide';
+export { default as InteractionUserGuide } from './interactionUserGuide';
