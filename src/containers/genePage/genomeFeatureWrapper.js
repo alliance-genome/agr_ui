151ec0464151ae20a6cc41ca87deddef614fc505
Nathan Dunn <ndunn@me.com>
2019-10-11 09:39:13 -0700

    is working and webpack changes backed out

--- src/containers/genePage/genomeFeatureWrapper.js
+++ src/containers/genePage/genomeFeatureWrapper.js
@@ -17,33 +17,11 @@ class GenomeFeatureWrapper extends Component {
 
   constructor(props) {
     super(props);
-    let defaultTrackName = 'All Genes'; // this is the generic track name
-    let locationString = this.props.chromosome + ':' + this.props.fmin + '..' + this.props.fmax;
     let apolloServerPrefix = process.env.APOLLO_URL;
 
     // TODO: this is a hack to fix inconsistencies in JBrowse
-    let trackDataWithHighlight = apolloServerPrefix + 'track/' + encodeURI(this.props.species) + '/' + defaultTrackName + '/' + encodeURI(locationString) + '.json';
-    let names = [];
-    if (this.props.primaryId.indexOf(':') > 0) {
-      names.push(this.props.primaryId.split(':')[1]);
-    }
-    names.push(this.props.geneSymbol);
-
-
-    if (this.props.synonyms) {
-      for (let syn of this.props.synonyms) {
-        names.push(syn);
-      }
-    }
-
-    for (let name in names) {
-      trackDataWithHighlight = trackDataWithHighlight + (Number.parseInt(name) === 0 ? '?' : '&') + 'name=' + encodeURI(names[name]);
-    }
-
-
-    // TODO: Still some inconsistencies with SGD data
-    // else
-    trackDataWithHighlight += '&ignoreCache=true';
+    // let trackDataWithHighlight = apolloServerPrefix + 'track/' + encodeURI(this.props.species) + '/' + defaultTrackName + '/' + encodeURI(locationString) + '.json';
+    let trackDataWithHighlight = apolloServerPrefix + 'track/' ;
 
     let geneSymbolUrl = '&lookupSymbol=' + this.props.geneSymbol;
     let externalJBrowsePrefix = process.env.JBROWSE_URL + '/jbrowse/index.html?data=data%2F' + encodeURI(this.props.species);
@@ -62,7 +40,6 @@ class GenomeFeatureWrapper extends Component {
       loadState: 'loading'
     };
 
-    console.log('track data url',trackDataWithHighlight);
     this.trackDataUrl = trackDataWithHighlight;
     this.jbrowseUrl = externalJbrowseUrl;
   }
@@ -74,9 +51,23 @@ class GenomeFeatureWrapper extends Component {
 
 
   loadGenomeFeature() {
-    const {chromosome, fmin, fmax, geneSymbol, synonyms = []} = this.props;
-    let nameSuffix = [geneSymbol, ...synonyms];
+    const {chromosome, fmin, fmax, primaryId,geneSymbol, synonyms = []} = this.props;
+    // provide unique names
+    let nameSuffix = [geneSymbol, ...synonyms,primaryId].filter((x, i, a) => a.indexOf(x) === i);
     let nameSuffixString = nameSuffix.length ===0 ? '': nameSuffix.join('&name=');
+    if (nameSuffixString.length > 0) {
+      nameSuffixString = `?name=${nameSuffixString}`;
+    }
+    // resolved in widget as:
+    // var dataUrl = track["url"][0] + encodeURI(track["genome"]) + track["url"][1] + encodeURI(externalLocationString) + track["url"][2];
+    // https://agr-apollo.berkeleybop.io/apollo/track/Mus%20musculus/All%20Genes/2:105668900..105697364.json?name=97490&name=Pax6Mus%20musculus/All%20Genes/2:105668900..105697364.jsonPax6
+    //
+    // should be:
+    //
+    // https://agr-apollo.berkeleybop.io/apollo/track/Mus%20musculus/All%20Genes/2:105668900..105697364.json?name=97490&name=Pax6
+    // [0] should be apollo_url: https://agr-apollo.berkeleybop.io/apollo/track
+    // [1] should be track name : ALL_Genes
+    // [2] should be track name : name suffix string
     let transcriptTypes = getTranscriptTypes();
     const configGlobal = {
       'locale': 'global',
@@ -90,12 +81,14 @@ class GenomeFeatureWrapper extends Component {
           'genome': this.props.species,
           'type': 'isoform',
           'url': [
-            this.trackDataUrl,nameSuffixString
+            this.trackDataUrl,
+            '/All%20Genes/',
+            `.json${nameSuffixString}`
           ]
         },
       ]
     };
-    console.log('this pros id',this.props.id)
+    console.log('this pros id',this.props.id, configGlobal.tracks[0]);
     new GenomeFeatureViewer(configGlobal, `#${this.props.id}`, 900, undefined);
   }
 
