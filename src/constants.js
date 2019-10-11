1c46e26174a3efd5965e00962e319ebd710a3173
Patrick Kalita <pkalita@zfin.org>
2019-10-11 16:23:07 -0700

    Layout tweaks for search filters

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const SEARCH_API_ERROR_MESSAGE = 'There was a problem connecting to the server. Please refresh the page.  If you continue to see this message, please contact alliance-software@lists.stanford.edu';
-export const LARGE_COL_CLASS = 'col-sm-8 col-md-8 col-12';
-export const SMALL_COL_CLASS = 'col-sm-4 col-md-4 col-12';
+export const LARGE_COL_CLASS = 'col-md-8 col-12';
+export const SMALL_COL_CLASS = 'col-md-4 col-12';
 
 /* Wordpress REST API connection setting */
 export const WORDPRESS_REST_API_BASE = 'https://public-api.wordpress.com/wp/v2/sites/alliancegenome.wordpress.com';
