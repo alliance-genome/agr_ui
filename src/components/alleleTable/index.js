994d12ef6c3466b964070e5cf8988c62628f8cec
Nathan Dunn <nathandunn@lbl.gov>
2019-10-28 14:47:05 -0700

    fixes AGR-1768 a second time (#535)
    * fixes AGR-1768 a second time
    
    * insertion only usese the start cooridnations

--- src/components/alleleTable/index.js
+++ src/components/alleleTable/index.js
@@ -22,12 +22,13 @@ class AlleleTable extends Component {
   calculateHighlight(location, type) {
     switch(type){
     case 'insertion':
-      return `${location.chromosome}:${typeof location.start === 'number' ? location.start : location.end}..${location.end-1}`;
+      return `${location.chromosome}:${typeof location.start === 'number' ? location.start : location.start}..${location.start}`;
     case 'deletion':
+    case 'delins':
       return `${location.chromosome}:${typeof location.start === 'number' ? location.start-1 : location.end}..${location.end}`;
     default:
     case 'point_mutation':
-      return `${location.chromosome}:${typeof location.start === 'number' ? location.start-1 : location.end}..${location.end}`;
+      return `${location.chromosome}:${typeof location.start === 'number' ? location.start : location.end}..${location.end}`;
     }
   }
 
