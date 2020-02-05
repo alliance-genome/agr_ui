40f9b643f1cf254c2fc80d024503e5c1bb5355ea
Prita Mani <prita@uoregon.edu>
2020-02-05 13:34:20 -0800

    AGR-1792 removed Gene Models download button

--- src/containers/genePage/GeneModelsTable.js
+++ src/containers/genePage/GeneModelsTable.js
@@ -62,7 +62,6 @@ const GeneModelsTable = ({dispatchFetchModels, id, models}) => {
     <RemoteDataTable
       columns={columns}
       data={models.data}
-      downloadUrl={`/api/gene/${id}/models/download`}
       keyField='id'
       loading={models.loading}
       onUpdate={dispatchFetchModels}
