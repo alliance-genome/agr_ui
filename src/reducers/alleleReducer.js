7f99a2780d049358bc64960e1b16bd429e5cb7a8
Sibyl <sibyl@wormbase.org>
2020-02-07 12:13:29 -0500

    variant table

--- src/reducers/alleleReducer.js
+++ src/reducers/alleleReducer.js
@@ -7,7 +7,8 @@ import {
 import {
   FETCH_ALLELE,
   FETCH_ALLELE_PHENOTYPES,
-  FETCH_ALLELE_DISEASE
+  FETCH_ALLELE_DISEASE,
+  FETCH_ALLELE_VARIANTS
 } from '../actions/alleleActions';
 
 const DEFAULT_STATE = fromJS({
@@ -26,12 +27,19 @@ const DEFAULT_STATE = fromJS({
     error: null,
     total: 0
   },
+  variants: {
+    data: [],
+    loading: false,
+    error: null,
+    total: 0
+  },
 });
 
 const alleleReducer = handleActions(DEFAULT_STATE,
   forObjectRequestAction(FETCH_ALLELE),
   forCollectionRequestAction(FETCH_ALLELE_PHENOTYPES, 'phenotypes'),
   forCollectionRequestAction(FETCH_ALLELE_DISEASE, 'diseaseAssociations'),
+  forCollectionRequestAction(FETCH_ALLELE_VARIANTS, 'variants'),
 );
 
 export default alleleReducer;
