6bf520689309f257351143c0ed8c8d8286bc29d9
Nathan Dunn <ndunn@me.com>
2020-02-12 08:38:42 -0800

    integrated in the allele sequence view and genome feature wrapper

--- src/containers/allelePage/AlleleSequenceView.js
+++ src/containers/allelePage/AlleleSequenceView.js
@@ -5,43 +5,18 @@ import { connect } from 'react-redux';
 import GenomeFeatureWrapper from '../genePage/genomeFeatureWrapper';
 // import GenomeFeatureWrapper from '../genePage/genomeFeatureWrapper';
 
-const AlleleSequenceView = ({allele, variants = {}}) => {
+const AlleleSequenceView = ({allele}) => {
   console.log('allele',allele);
 
   // myl7
   // http://localhost:2992/gene/ZFIN:ZDB-GENE-991019-3#alleles-and-variants
   // az2
   // http://localhost:2992/allele/ZFIN:ZDB-ALT-181010-2
+  const genomeLocations = allele.gene.genomeLocations;
+  const genomeLocation = genomeLocations && genomeLocations.length > 0 ? genomeLocations[0] : null ;
 
-  const { data:variantsData = []} = variants;
-
-
-  let genomeLocation = {
-    assembly: 'GRCz11',
-    chromosome:'8',
-    start:40457107,
-    end:40464935,
-    strand:'+',
-  };
   return (
     <div>
-      Allele viewer here
-      {/*{allele}*/}
-      <pre>
-        {JSON.stringify(variantsData, null,4)}
-      </pre>
-      <ul>
-        <li>assembly: N/A fake {genomeLocation.assembly}</li>
-        <li>fmin : N/A fake {genomeLocation.start}</li>
-        <li>fmax: N/A  fake {genomeLocation.end}</li>
-        <li>strand: N/A fake {genomeLocation.end}</li>
-        <li>chromosome : N/A fake {genomeLocation.chromosome}</li>
-        <li>biotype: {allele.type}, though maybe mapped as a gene</li>
-        <li>symbol: {allele.symbol}</li>
-        <li>id: {allele.id}</li>
-        <li>species: {allele.species.name}</li>
-        <li>synonyms: {allele.synonyms}</li>
-      </ul>
       <GenomeFeatureWrapper
         assembly={genomeLocation.assembly}
         biotype='gene'
