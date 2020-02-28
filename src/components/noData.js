81a315d898a0a336159645b014164d1803384884
Patrick Kalita <pkalita@zfin.org>
2020-02-28 14:41:12 -0800

    Fix capitalization

--- src/components/noData.js
+++ src/components/noData.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const NoData = ({children}) => (
-  <i className='text-muted'>{children || 'No Data Available'}</i>
+  <i className='text-muted'>{children || 'No data available'}</i>
 );
 
 NoData.propTypes = {
