210726f03e48660208f96f40915cc9c0ef277812
Patrick Kalita <pkalita@zfin.org>
2020-03-02 12:47:08 -0800

    Add search example links

--- src/containers/homepage/index.js
+++ src/containers/homepage/index.js
@@ -3,6 +3,7 @@ import HeadMetaTags from '../../components/headMetaTags';
 
 import style from './style.scss';
 import {SearchBarComponent} from '../layout/searchBar';
+import SearchExample from './SearchExample';
 
 const Homepage = () => {
   return (
@@ -10,10 +11,19 @@ const Homepage = () => {
       <HeadMetaTags title='Home' />
 
       <section className={style.searchSection}>
-        <h1 className='display-4 mb-4'>Big Call to Action</h1>
-        <h3 className='mb-4'>A smaller subheading with more text about whatever</h3>
-        <div className={style.searchBarContainer}>
-          <SearchBarComponent autoFocus />
+        <div className={style.searchSectionInner}>
+          <h1 className='display-4 mb-4 text-center'>Big Call to Action</h1>
+          <h3 className='mb-4 text-center'>A smaller subheading with more text about whatever</h3>
+          <div className={style.searchBarContainer}>
+            <SearchBarComponent autoFocus />
+          </div>
+          <ul className={style.searchExampleList}>
+            <li>Examples</li>
+            <li><SearchExample term='RPB7' /></li>
+            <li><SearchExample term='kinase' /></li>
+            <li><SearchExample term='asthma' /></li>
+            <li><SearchExample term='liver' /></li>
+          </ul>
         </div>
       </section>
     </div>
