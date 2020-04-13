d47747426cde91bb5e7b99117632b03a7d2785fc
Patrick Kalita <pkalita@zfin.org>
2020-04-13 13:58:45 -0700

    Remove contact bar; use inverted navbar

--- src/containers/layout/navigation/menuItems.js
+++ src/containers/layout/navigation/menuItems.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import { NAV_MENU } from '../../../constants';
-
 import { NavItem } from '.';
-import style from './style.scss';
 
 class MenuItems extends Component {
   render () {
     return (
-      <nav className={`navbar navbar-expand-md ${style.topnav}`}>
-        <div className='container-fluid'>
-          <div className={`collapse navbar-collapse ${style.inner}`} id='exCollapsingNavbar2'>
-            <ul className='navbar-nav'>
-              {NAV_MENU.map(page => <NavItem currentRoute={this.props.currentRoute} key={page.label} page={page} />)}
-            </ul>
-          </div>
+      <nav className='navbar navbar-expand-md navbar-dark bg-primary p-0'>
+        <div className='collapse navbar-collapse' id='exCollapsingNavbar2'>
+          <ul className='navbar-nav m-3 m-md-1'>
+            {NAV_MENU.map(page => <NavItem currentRoute={this.props.currentRoute} key={page.label} page={page} />)}
+          </ul>
         </div>
       </nav>
     );
