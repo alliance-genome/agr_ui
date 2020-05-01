a9e7a386b920c2d3c651118780c89e81c8db372b
Nathan Dunn <ndunn@me.com>
2020-05-01 10:44:25 -0700

    added ids

--- src/services/TrackService.js
+++ src/services/TrackService.js
@@ -8,9 +8,11 @@ export function renderTrackDescription(feature){
   let returnString = '';
   returnString += `<table class="tooltip-table" style="margin-top: 30px;"><tbody>`;
   if(feature.id.indexOf('http')<0){
-    returnString += `<tr><th>ID</th><td>${feature.id}</td></tr>`;
+    returnString += `<tr><th>Name</th><td>${feature.name} (${feature.id})</td></tr>`;
   }
-  returnString += `<tr><th>Name</th><td>${feature.name}</td></tr>`;
+  else{
+    returnString += `<tr><th>Name</th><td>${feature.name}</td></tr>`;
+  }
   returnString += `<tr><th>Type</th><td>${feature.type}</td></tr>`;
   returnString += `<tr><th>Source</th><td>${feature.source}</td></tr>`;
 
