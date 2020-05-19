f45e8e6c4af49b2e5aeff15a7306ff1f1a561604
Patrick Kalita <pkalita@zfin.org>
2020-05-19 13:35:31 -0700

    Group heading and subheading more closely

--- src/containers/homepage/SearchSection.js
+++ src/containers/homepage/SearchSection.js
@@ -9,12 +9,12 @@ const SearchSection = () => {
   return (
     <section className={`${style.section} ${style.searchBackground} shadow`}>
       <div className={style.contentContainer}>
-        <h1 className='display-4 mb-4 font-weight-normal text-center'>Search Across Species</h1>
-        <h3 className='mb-4 text-center'>Explore model organism and human comparative genomics</h3>
+        <h1 className='display-4 font-weight-normal mb-1 text-center'>Search Across Species</h1>
+        <h4 className='mb-5 text-center'>Explore model organism and human comparative genomics</h4>
         <div className={style.searchBarContainer}>
           <SearchBarComponent autoFocus placeholder='Search for genes, alleles, disease models, and more' />
         </div>
-        <ul className={style.searchExampleList}>
+        <ul className={`${style.searchExampleList} mb-4`}>
           <li>Examples:</li>
           <li><SearchExample term='RPB7' /></li>
           <li><SearchExample term='kinase' /></li>
