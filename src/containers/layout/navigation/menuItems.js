5173ce96ca9cd895af645a090b5cc57fa1d0168e
Patrick Kalita <pkalita@zfin.org>
2020-07-09 11:15:58 -0700

    Fix merge issues

--- src/containers/layout/navigation/menuItems.js
+++ src/containers/layout/navigation/menuItems.js
@@ -3,18 +3,17 @@ import PropTypes from 'prop-types';
 import { Collapse } from 'reactstrap';
 
 import { NAV_MENU } from '../../../constants';
-import { NavItem } from '.';
-import style from './style.scss';
+import { MenuItem } from '.';
 
 class MenuItems extends Component {
   render () {
     const { currentRoute, menuOpen, onItemClick } = this.props;
     return (
       <nav className='navbar navbar-expand-md navbar-dark bg-primary p-0'>
-        <Collapse className={style.inner} isOpen={menuOpen} navbar>
+        <Collapse isOpen={menuOpen} navbar>
           <ul className='navbar-nav m-3 m-md-1'>
             {NAV_MENU.map(page => (
-              <NavItem
+              <MenuItem
                 currentRoute={currentRoute}
                 key={page.label}
                 onClick={onItemClick}
