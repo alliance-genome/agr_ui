da868c5875b32543ba71ad303ff04b0aeaac5a00
Patrick Kalita <pkalita@zfin.org>
2020-07-21 10:29:26 -0700

    Fix alignment of gene symbol when no icon present

--- src/components/dataPage/PageNavEntity.js
+++ src/components/dataPage/PageNavEntity.js
@@ -18,7 +18,7 @@ const PageNavEntity = ({children, entityName, icon, truncateName}) => {
     <div className={`${style.entity}`}>
       <div className='w-100'>
         <div className='d-flex align-items-center'>
-          {icon && <span className='mr-2'>{icon}</span>}
+          {icon && <span>{icon}</span>}
           <h5 className={truncateName ? 'text-truncate' : ''} id='PageNavEntityTitle' ref={titleRef}>{entityName}</h5>
           {truncateName && attachTooltip &&
             <UncontrolledTooltip innerClassName={style.titleTooltip} placement='bottom' target='PageNavEntityTitle'>
