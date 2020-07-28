24fc632ec28656fedd898e7db5b7a170223a8a57
Patrick Kalita <pkalita@zfin.org>
2020-07-28 15:44:25 -0700

    Remove unused ribbon-related redux code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,3 @@
-import diseaseRibbonReducer from './diseaseRibbonReducer';
-import expressionReducer from './expressionReducer';
 import geneReducer from './geneReducer';
 import searchReducer from './searchReducer';
 import wordpressReducer from './wordpressReducer';
@@ -9,8 +7,6 @@ import alleleReducer from './alleleReducer';
 
 export default {
   allele: alleleReducer,
-  diseaseRibbon: diseaseRibbonReducer,
-  expression: expressionReducer,
   fileManagementSystem: fileManagementSystemReducer,
   gene: geneReducer,
   search: searchReducer,
