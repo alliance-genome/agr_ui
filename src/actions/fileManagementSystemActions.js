d3d07febc0ef22fc8f3ebb696f1a96f25ee07a66
Patrick Kalita <pkalita@zfin.org>
2020-07-29 17:06:36 -0700

    Fetch download files data via hook

--- src/actions/fileManagementSystemActions.js
+++
@@ -1,8 +0,0 @@
-import {createFetchAction} from '../lib/createActions';
-
-export const FETCH_RELEASE_FILES = 'FETCH_CURRENT_RELEASE_FILES';
-
-export const fetchReleaseFiles = createFetchAction(
-  FETCH_RELEASE_FILES,
-  (release) => `https://fms.alliancegenome.org/api/datafile/by/release/${release}?latest=true`
-);
