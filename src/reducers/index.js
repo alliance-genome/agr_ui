2bb98543df824925b377026f6fd01aec63bdd973
Patrick Kalita <pkalita@zfin.org>
2020-07-29 17:06:36 -0700

    Fetch download files data via hook

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import geneReducer from './geneReducer';
 import searchReducer from './searchReducer';
 import wordpressReducer from './wordpressReducer';
-import fileManagementSystemReducer from './fileManagementSystemReducer';
 import loadingReducer from './loadingReducer';
 import alleleReducer from './alleleReducer';
 
 export default {
   allele: alleleReducer,
-  fileManagementSystem: fileManagementSystemReducer,
   gene: geneReducer,
   search: searchReducer,
   wordpress: wordpressReducer,
