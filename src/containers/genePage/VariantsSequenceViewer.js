49281ee009dec31c40798afa29f358aab78e3111
Patrick Kalita <pkalita@zfin.org>
2020-08-07 10:53:01 -0700

    Pass alleles to VariantSequenceViewer instead of pulling from redux

--- src/containers/genePage/VariantsSequenceViewer.js
+++ src/containers/genePage/VariantsSequenceViewer.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import GenomeFeatureWrapper from './genomeFeatureWrapper';
-import {selectAlleles} from '../../selectors/geneSelectors';
 import NoData from '../../components/noData';
 
-export function findFminFmax(genomeLocation,variants){
+export function findFminFmax(genomeLocation, variants) {
   let fmax = genomeLocation.end;
   let fmin = genomeLocation.start;
-  if(variants && variants.data){
-    for(let variant of variants.data){
-      if(variant.variants){
-        for(let innerVariant of variant.variants){
-          if(innerVariant.location){
-            if(innerVariant.location.start < fmin ){
-              fmin = innerVariant.location.start ;
+  if (variants && variants.data) {
+    for (let variant of variants.data) {
+      if (variant.variants) {
+        for (let innerVariant of variant.variants) {
+          if (innerVariant.location) {
+            if (innerVariant.location.start < fmin) {
+              fmin = innerVariant.location.start;
             }
-            if(innerVariant.location.end  > fmax ){
-              fmax = innerVariant.location.end ;
+            if (innerVariant.location.end > fmax) {
+              fmax = innerVariant.location.end;
             }
           }
         }
       }
     }
   }
-  return {fmin,fmax};
+  return { fmin, fmax };
 }
 
-const VariantsSequenceViewer = ({alleles,allelesSelected,allelesVisible, tableState, gene, genomeLocation}) => {
-  if (alleles.loading || alleles.error || alleles.data.length === 0 || !genomeLocation.chromosome) {
+const VariantsSequenceViewer = ({ alleles, allelesSelected, allelesVisible, tableState, gene, genomeLocation }) => {
+  if (alleles.length === 0 || !genomeLocation.chromosome) {
     return null;
   }
 
@@ -36,12 +34,12 @@ const VariantsSequenceViewer = ({alleles,allelesSelected,allelesVisible, tableSt
   console.log(tableState);  // eslint-disable-line
 
 
-  const anyVariantData = alleles.data.some(allele => allele.variants.length > 0);
+  const anyVariantData = alleles.some(allele => allele.variants.length > 0);
   if (!anyVariantData) {
     return <NoData>No mapped variant information available</NoData>;
   }
 
-  const {fmin,fmax} = findFminFmax(genomeLocation,alleles);
+  const { fmin, fmax } = findFminFmax(genomeLocation, alleles);
   return (
     <GenomeFeatureWrapper
       allelesSelected={allelesSelected}
@@ -58,14 +56,14 @@ const VariantsSequenceViewer = ({alleles,allelesSelected,allelesVisible, tableSt
       species={gene.species.taxonId}
       strand={genomeLocation.strand}
       synonyms={gene.synonyms}
-      visibleVariants={allelesVisible.map( a => a.id)}
+      visibleVariants={allelesVisible.map(a => a.id)}
       width='600px'
     />
   );
 };
 
 VariantsSequenceViewer.propTypes = {
-  alleles: PropTypes.object,
+  alleles: PropTypes.array,
   allelesSelected: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
@@ -88,8 +86,4 @@ VariantsSequenceViewer.propTypes = {
   tableState: PropTypes.object.isRequired,  // TODO: have type checking based on what this component actually uses
 };
 
-const mapStateToProps = state => ({
-  alleles: selectAlleles(state),
-});
-
-export default connect(mapStateToProps)(VariantsSequenceViewer);
+export default VariantsSequenceViewer;
