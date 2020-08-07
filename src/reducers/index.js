a4d26e1cddb0672a3b2a06861a621164838d122f
Patrick Kalita <pkalita@zfin.org>
2020-08-07 09:27:29 -0700

    Remove unused redux functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,7 @@
-import geneReducer from './geneReducer';
 import searchReducer from './searchReducer';
 import loadingReducer from './loadingReducer';
-import alleleReducer from './alleleReducer';
 
 export default {
-  allele: alleleReducer,
-  gene: geneReducer,
   search: searchReducer,
   loading: loadingReducer,
 };
