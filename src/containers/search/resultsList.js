628969d9248c1c85942247778f109d189a965324
Patrick Kalita <pkalita@zfin.org>
2020-09-16 14:49:31 -0700

    Italicize species names in search results

--- src/containers/search/resultsList.js
+++ src/containers/search/resultsList.js
@@ -12,6 +12,7 @@ import SpeciesIcon from '../../components/speciesIcon';
 import { getLinkForEntry, stringifyQuery } from '../../lib/searchHelpers';
 import {UncontrolledTooltip} from 'reactstrap';
 import hash from 'object-hash';
+import SpeciesName from '../../components/SpeciesName';
 
 const DEFAULT_FIELDS = ['symbol', 'name', 'synonyms', 'sourceHref', 'id', 'type'];
 
@@ -31,7 +32,11 @@ class ResultsList extends Component {
     return (<div className="row">
       <div className="col-sm-10">
         <h4 className={style.resultLinkLabel}>{getLinkForEntry(d)}</h4>
-        {d.species && <span className={style.resultSpeciesLabel} dangerouslySetInnerHTML={{ __html: '(' + d.species + ')' }} />}
+        {d.species && (
+          <span className='ml-3'>
+            (<SpeciesName dangerouslySetInnerHTML={{ __html: d.species }} />)
+          </span>
+        )}
       </div>
       <div className="col-sm-2">
         <span className={style.resultCatLabel}>
