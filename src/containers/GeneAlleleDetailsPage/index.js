010306965856021766adc1428b58c317806e989b
Sibyl <sibyl@wormbase.org>
2021-01-19 13:03:17 -0500

    remove data loading from DataPage

--- src/containers/GeneAlleleDetailsPage/index.js
+++ src/containers/GeneAlleleDetailsPage/index.js
@@ -2,27 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { DataPage, PageHeader } from '../../components/dataPage';
+import NotFound from '../../components/notFound';
 import usePageLoadingQuery from '../../hooks/usePageLoadingQuery';
 import ErrorBoundary from '../../components/errorBoundary';
 import GeneAlleleDetailsTable from './GeneAlleleDetailsTable';
 
 const GeneAlleleDetailsPage = ({geneId}) => {
   const pageProps = usePageLoadingQuery(`/api/gene/${geneId}`);
-  const { data } = pageProps;
+  const { data, isLoading, isError } = pageProps;
+
+  if (isLoading) {
+    return null;
+  } else if (isError) {
+    return <NotFound />;
+  }
   const { symbol, id } = data || {};
 
-  const pageTitle = `Alleles/Variants Details`
+  const pageTitle = 'Alleles or Variants Details';
 
   return (
-    <DataPage {...pageProps}>
+    <DataPage>
       <div className="w-100 pl-3">
         <nav aria-label="breadcrumb" className="d-flex">
-          <ol class="breadcrumb">
-            <li class="breadcrumb-item">Gene</li>
-            <li class="breadcrumb-item">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">Gene</li>
+            <li className="breadcrumb-item">
               <Link to={`/gene/${id}`}>{symbol}</Link>
             </li>
-            <li class="breadcrumb-item active" aria-current="page">{pageTitle}</li>
+            <li className="breadcrumb-item active" aria-current="page">{pageTitle}</li>
           </ol>
         </nav>
         <PageHeader>{pageTitle} ({symbol})</PageHeader>
