b62c3c1dff635f501f33be3d5ee58316b3965b83
Patrick Kalita <patrick.kalita@gmail.com>
2021-01-20 16:00:03 -0800

    Get download file types from API response

--- src/containers/downloadsPage/DownloadFileRow.js
+++ src/containers/downloadsPage/DownloadFileRow.js
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DownloadFileLink from './downloadFileLink';
 
-const DownloadFileRow = ({description, url, fileType}) => {
-  if (!Array.isArray(url)) {
-    url = [url];
-  }
-
-  url = url.filter(u => u);
+const DownloadFileRow = ({description, files}) => {
+  // remove any undefined entries in the array
+  files = files.filter(u => u);
 
   return (
     <tr>
       <td>{description}</td>
       <td>
-        {url.map(u => <DownloadFileLink fileType={fileType} key={u} url={u} />)}
+        {files.map(file => (
+          <DownloadFileLink
+            fileType={file.dataType.fileExtension}
+            key={file.id}
+            url={file.s3Url}
+          />
+        ))}
       </td>
     </tr>
   );
@@ -21,8 +24,13 @@ const DownloadFileRow = ({description, url, fileType}) => {
 
 DownloadFileRow.propTypes = {
   description: PropTypes.node,
-  fileType: PropTypes.string,
-  url: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+  files: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    s3Url: PropTypes.string,
+    dataType: PropTypes.shape({
+      fileExtension: PropTypes.string,
+    }),
+  })),
 };
 
 export default DownloadFileRow;
