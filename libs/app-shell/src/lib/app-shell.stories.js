3ed560fac00785aa856f5870695bc6e426e35fbb
Sibyl <sibyl@wormbase.org>
2021-02-18 19:41:41 -0500

    run storybook configuration on app-shell

---
+++ libs/app-shell/src/lib/app-shell.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppShell } from './app-shell';
+
+export default {
+  component: AppShell,
+  title: 'AppShell',
+};
+
+export const primary = () => {
+  return <AppShell />;
+};
