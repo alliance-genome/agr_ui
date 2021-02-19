a55a4ae5f8bcff6d32902d3cd6cbe47ea18b3256
Sibyl <sibyl@wormbase.org>
2021-02-18 22:54:05 -0500

    generate Menu component in app-shell

---
+++ libs/app-shell/src/lib/menu/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export function Menu(props) {
+  return (
+    <div>
+      <h1>Welcome to Menu!</h1>
+    </div>
+  );
+}
+export default Menu;
---
+++ libs/app-shell/src/lib/menu/Menu.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Menu from './Menu';
+describe('Menu', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Menu />);
+    expect(baseElement).toBeTruthy();
+  });
+});
