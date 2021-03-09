b7e216ef4016b6b26d6aa4664528303dd9c52bff
Sibyl <sibyl@wormbase.org>
2021-03-09 14:49:01 -0500

    handle module styles without the .module.scss suffix

---
+++ apps/main-app-import-test-run/webpack.js
@@ -0,0 +1,37 @@
+const getWebpackConfig = require('@nrwl/react/plugins/webpack');
+const path = require('path');
+
+module.exports = function (configOriginal) {
+  const config = getWebpackConfig(configOriginal);
+
+  Object.defineProperty(RegExp.prototype, 'toJSON', {
+    value: RegExp.prototype.toString,
+  });
+  // console.log(JSON.stringify(config.module.rules, null, 2));
+
+  /* by default the styles are treated as modules */
+  const [styleRule] = config.module.rules.filter(({test}) => (
+    test.toString().search(/\.scss/) > -1
+  ));
+
+  if (styleRule && styleRule.oneOf) {
+    const scssModuleRuleIndex = styleRule.oneOf.findIndex(({test}) => (
+      test.test('somepath/style.module.scss')
+    ));
+    if (scssModuleRuleIndex > -1) {
+      styleRule.oneOf.splice(
+        scssModuleRuleIndex + 1,
+        0,
+        {
+          ...styleRule.oneOf[scssModuleRuleIndex],
+          test: /\.(scss|sass)$/,
+          include: path.resolve(__dirname, 'src'),
+          exclude: path.resolve(__dirname, 'src/style.scss'),
+        },
+      );
+    }
+  }
+
+  // console.log(JSON.stringify(config.module.rules, null, 2));
+  return config;
+}
