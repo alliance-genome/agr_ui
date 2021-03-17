24955f8c7707a55b3d3c756dbe3f20b618021ca4
Sibyl <sibyl@wormbase.org>
2021-03-17 19:03:51 -0400

    mock some functions

--- apps/main-app/src/tests/application.test.js
+++ apps/main-app/src/tests/application.test.js
@@ -5,6 +5,13 @@ import { MemoryRouter } from 'react-router-dom';
 
 import ReactApp from '../reactApplication';
 
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useLayoutEffect: jest.requireActual('react').useEffect,
+}));
+
+global.scrollTo = jest.fn();
+
 describe('ReactApp', () => {
   it('should be able to render to an HTML string', () => {
     let htmlString = renderToString(
