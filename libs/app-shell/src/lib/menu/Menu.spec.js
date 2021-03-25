a71429129a39a27c5cefa625ea3f1f8f80275206
Sibyl <sibyl@wormbase.org>
2021-03-25 16:25:12 -0400

    fix some tests

--- libs/app-shell/src/lib/menu/Menu.spec.js
+++ libs/app-shell/src/lib/menu/Menu.spec.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Menu from './Menu';
+import LinkRenderProvider from './link-render-provider/LinkRenderProvider';
+import LinkRenderDefault from './link-render-provider/LinkRenderDefault';
+
 describe('Menu', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Menu />);
+    const { baseElement } = render(
+      <LinkRenderProvider renderLink={LinkRenderDefault}>
+        <Menu />
+      </LinkRenderProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
