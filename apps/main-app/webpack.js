6e272593d574622270235c9611460b047049acab
Sibyl <sibyl@wormbase.org>
2021-03-26 19:30:41 -0400

    API_URL environment variable for devServer

--- apps/main-app/webpack.js
+++ apps/main-app/webpack.js
@@ -4,6 +4,15 @@ const path = require('path');
 module.exports = function (configOriginal) {
   const config = getWebpackConfig(configOriginal);
 
+  if (config.devServer) {
+    config.devServer.proxy = {
+      "/api": {
+        target: process.env.API_URL || "http://build.alliancegenome.org",
+        secure: false
+      }
+    }
+  }
+
   Object.defineProperty(RegExp.prototype, 'toJSON', {
     value: RegExp.prototype.toString,
   });
