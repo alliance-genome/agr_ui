db92d89cd2c2fcc8d0dd7b584a4650d3fed0f742
Sibyl <sibyl@wormbase.org>
2021-04-15 14:25:44 -0400

    configure twin.macro with emotion

--- apps/mod-landing-pages/pages/_app.js
+++ apps/mod-landing-pages/pages/_app.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
-function CustomApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to mod-landing-pages!</title>
-      </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to mod-landing-pages!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </>
-  );
-}
-export default CustomApp;
+import { GlobalStyles } from 'twin.macro';
+
+const App = ({ Component, pageProps }) => (
+  <div>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </div>
+);
+
+export default App;
