e54b3eb155172c1027bead20d113247c0978f10b
Sibyl <sibyl@wormbase.org>
2021-04-16 17:31:43 -0400

    add basePath /members

--- apps/mod-landing-pages/next.config.js
+++ apps/mod-landing-pages/next.config.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
 
-module.exports = withNx({});
+module.exports = withNx({
+  basePath: '/members',
+});
