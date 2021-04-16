10aafc1c453760e2714707e063a56ad5185b5ba9
Sibyl <sibyl@wormbase.org>
2021-04-16 00:03:12 -0400

    mod specific primary color

--- libs/app-shell/src/lib/app-shell.js
+++ libs/app-shell/src/lib/app-shell.js
@@ -1,16 +1,20 @@
 import React, { useCallback, useMemo } from 'react';
+import { Global } from '@emotion/react';
 import tw from 'twin.macro';
 import Menu from './menu/Menu';
 import LinkRenderProvider from './link-render-provider/LinkRenderProvider';
 import LinkRenderDefault from './link-render-provider/LinkRenderDefault';
+import MODThemeProvider from './modtheme-provider/MODThemeProvider';
 
-export function AppShell({ children, renderLink = LinkRenderDefault }) {
+export function AppShell({ children, renderLink = LinkRenderDefault, mod }) {
   return (
     <LinkRenderProvider renderLink={renderLink}>
-      <header tw="flex bg-primary">
-        <Menu />
-      </header>
-      {children}
+      <MODThemeProvider mod={mod}>
+        <header tw="flex bg-primary">
+          <Menu />
+        </header>
+        {children}
+      </MODThemeProvider>
     </LinkRenderProvider>
   );
 }
