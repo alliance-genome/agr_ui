6ed306fd4360ac5c72a863ac51c82490fd27de4d
Sibyl <sibyl@wormbase.org>
2021-04-20 19:55:56 -0400

    add agr logo

--- libs/shared-assets/src/lib/logo/Logo.js
+++ libs/shared-assets/src/lib/logo/Logo.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import organizations, { getOrganizationById } from '../organizations';
+import { allianceLogoBase64 } from '../assets';
 
 export function Logo({ id, ...props }) {
   const organization = getOrganizationById(id);
-  return organization ? <img src={organization.logo} {...props} /> : null;
+  return (
+    <img
+      src={organization ? organization.logo : allianceLogoBase64}
+      {...props}
+    />
+  );
 }
 export default Logo;
