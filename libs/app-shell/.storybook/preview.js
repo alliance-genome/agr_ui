6f47646f66d9fe92bf1151cfda1607b69e548f82
Sibyl <sibyl@wormbase.org>
2021-04-20 18:04:51 -0400

    remove padding from storybook for app-shell

--- libs/app-shell/.storybook/preview.js
+++ libs/app-shell/.storybook/preview.js
@@ -2,3 +2,7 @@ import { addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 
 addDecorator(withKnobs);
+
+export const parameters = {
+  layout: 'fullscreen',
+};
