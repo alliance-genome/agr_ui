aea06d2395592533d04c571482b6b4f830021a53
Sibyl <sibyl@wormbase.org>
2021-04-20 20:38:54 -0400

    fix loader for images in next.js app

--- apps/mod-landing-pages/next.config.js
+++ apps/mod-landing-pages/next.config.js
@@ -3,4 +3,63 @@ const withNx = require('@nrwl/next/plugins/with-nx');
 
 module.exports = withNx({
   basePath: '/members',
+  webpack: (config) => {
+    Object.defineProperty(RegExp.prototype, 'toJSON', {
+      value: RegExp.prototype.toString,
+    });
+
+    config.module.rules.push(
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: Number.MAX_VALUE,
+        },
+      },
+      {
+        test: /\.svg$/,
+        oneOf: [
+          // If coming from JS/TS file, then transform into React component using SVGR.
+          {
+            issuer: {
+              test: /\.[jt]sx?$/,
+            },
+            use: [
+              {
+                loader: require.resolve('@svgr/webpack'),
+                options: {
+                  svgo: false,
+                  titleProp: true,
+                  ref: true,
+                },
+              },
+              {
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: 10000, // 10kB
+                  name: '[name].[hash:7].[ext]',
+                  esModule: false,
+                },
+              },
+            ],
+          },
+          // Fallback to plain URL loader.
+          {
+            use: [
+              {
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: 10000, // 10kB
+                  name: '[name].[hash:7].[ext]',
+                },
+              },
+            ],
+          },
+        ],
+      }
+    );
+    console.log(JSON.stringify(config.module.rules, null, 2));
+
+    return config;
+  },
 });
