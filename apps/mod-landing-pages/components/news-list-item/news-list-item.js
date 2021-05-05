7a5cfeedfb81db6842a26e333d204324068d77a1
Sibyl <sibyl@wormbase.org>
2021-05-05 13:43:35 -0400

    fix grid gap of news

--- apps/mod-landing-pages/components/news-list-item/news-list-item.js
+++ apps/mod-landing-pages/components/news-list-item/news-list-item.js
@@ -9,14 +9,14 @@ export function NewsListItem({
   URL = '',
 }) {
   return (
-    <a tw="shadow grid grid-cols-2 p-4 mb-4" href={URL}>
+    <a tw="shadow grid grid-cols-2 gap-2 p-4 mb-4" href={URL}>
       <h3 tw="col-span-full text-xl text-justify text-primary">{title}</h3>
       <span tw="text-gray-500">{author.name}</span>
       <span tw="justify-self-end text-gray-500">
         {new Date(date).toDateString()}
       </span>
       <div
-        tw="col-span-full text-justify mt-2"
+        tw="col-span-full text-justify"
         dangerouslySetInnerHTML={{ __html: excerpt }}
       />
     </a>
