881e5d0467558fa9ddea9425c603dd06005a9d46
Sibyl <sibyl@wormbase.org>
2021-06-08 15:36:41 -0400

    generate PageFooter component

--- libs/app-shell/src/index.js
+++ libs/app-shell/src/index.js
@@ -1,3 +1,4 @@
+export * from './lib/page-footer/PageFooter';
 export * from './lib/menu/Menu';
 export * from './lib/link-render-provider/LinkRenderProvider';
 export * from './lib/app-shell';
---
+++ libs/app-shell/src/lib/page-footer/PageFooter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export function PageFooter(props) {
+  return (
+    <div>
+      <h1>Welcome to page-footer!</h1>
+    </div>
+  );
+}
+export default PageFooter;
