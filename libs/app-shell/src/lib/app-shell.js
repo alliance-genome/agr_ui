5f6128e2971983f1f9cbe4a4a995f1714ea2f1a4
Sibyl <sibyl@wormbase.org>
2021-06-08 23:14:13 -0400

    add footer

--- libs/app-shell/src/lib/app-shell.js
+++ libs/app-shell/src/lib/app-shell.js
@@ -6,32 +6,36 @@ import Menu from './menu/Menu';
 import LinkRenderProvider from './link-render-provider/LinkRenderProvider';
 import LinkRenderDefault from './link-render-provider/LinkRenderDefault';
 import MODThemeProvider from './modtheme-provider/MODThemeProvider';
+import PageFooter from './page-footer/PageFooter';
 
 export function AppShell({ children, renderLink = LinkRenderDefault, mod }) {
   const organization = getOrganizationById(mod);
   return (
     <LinkRenderProvider renderLink={renderLink}>
       <MODThemeProvider mod={mod}>
-        <header>
-          <div tw="flex items-center h-10 my-2 divide-x ">
-            <Logo tw="object-contain h-full px-4" />
-            {organization ? (
-              <div tw="flex items-center h-full px-4">
-                <Logo id={mod} tw="object-contain h-full" />
-                <span tw="p-2 text-2xl font-medium hidden md:inline-block">
-                  {organization.name}
-                </span>
-                <span tw="p-2 text-2xl font-medium md:hidden">
-                  {organization.abbreviation}
-                </span>
-              </div>
-            ) : null}
-          </div>
-          <nav tw="flex bg-primary">
-            <Menu />
-          </nav>
-        </header>
-        {children}
+        <div tw="flex flex-col min-h-screen">
+          <header>
+            <div tw="flex items-center h-10 my-2 divide-x ">
+              <Logo tw="object-contain h-full px-4" />
+              {organization ? (
+                <div tw="flex items-center h-full px-4">
+                  <Logo id={mod} tw="object-contain h-full" />
+                  <span tw="p-2 text-2xl font-medium hidden md:inline-block">
+                    {organization.name}
+                  </span>
+                  <span tw="p-2 text-2xl font-medium md:hidden">
+                    {organization.abbreviation}
+                  </span>
+                </div>
+              ) : null}
+            </div>
+            <nav tw="flex bg-primary">
+              <Menu />
+            </nav>
+          </header>
+          <main tw="flex-grow">{children}</main>
+          <PageFooter />
+        </div>
       </MODThemeProvider>
     </LinkRenderProvider>
   );
--- libs/app-shell/src/lib/page-footer/PageFooter.js
+++ libs/app-shell/src/lib/page-footer/PageFooter.js
@@ -1,9 +1,56 @@
+import tw from 'twin.macro';
 import React from 'react';
+import { allianceLogoWhiteBase64 } from '@wormbase/agr-shared-assets';
+import { useRenderLink } from '../link-render-provider/LinkRenderProvider';
+import PageFooterLinkGroup from './PageFooterLinkGroup';
+import PageFooterLinkGroupLabel from './PageFooterLinkGroupLabel';
+import sitemap from '../sitemap';
+import PageFooterLink from './PageFooterLink';
+import { ReactComponent as FacebookIcon } from './assets/facebook_black_24dp.svg';
+
 export function PageFooter(props) {
+  const Link = useRenderLink();
   return (
-    <div>
-      <h1>Welcome to page-footer!</h1>
-    </div>
+    <footer tw="flex flex-col items-stretch bg-primary p-6 text-white">
+      <div tw="flex flex-col flex-wrap lg:max-h-60 lg:justify-evenly p-2">
+        {sitemap.map(({ sub, label, route }) =>
+          route === '/' ? null : (
+            <PageFooterLinkGroup key={label}>
+              <PageFooterLinkGroupLabel>
+                {route ? <Link to={route}>{label}</Link> : label}
+              </PageFooterLinkGroupLabel>
+              {sub &&
+                sub.map(({ label, route }) => (
+                  <PageFooterLink key={label} to={route}>
+                    {label}
+                  </PageFooterLink>
+                ))}
+            </PageFooterLinkGroup>
+          )
+        )}
+        <PageFooterLinkGroup>
+          <PageFooterLinkGroupLabel>Connect With Us</PageFooterLinkGroupLabel>
+          <PageFooterLink to={'https://www.facebook.com/alliancegenome'}>
+            <FacebookIcon tw="fill-current inline" /> Facebook
+          </PageFooterLink>
+          <PageFooterLink to={'https://twitter.com/alliancegenome'}>
+            Twitter
+          </PageFooterLink>
+          <PageFooterLink to={'https://github.com/alliance-genome'}>
+            GitHub
+          </PageFooterLink>
+        </PageFooterLinkGroup>
+      </div>
+      <div tw="self-center p-2">
+        <Link to="/">
+          <img
+            alt="Alliance of Genome Resources logo"
+            src={allianceLogoWhiteBase64}
+            tw="object-contain h-12"
+          />
+        </Link>
+      </div>
+    </footer>
   );
 }
 export default PageFooter;
