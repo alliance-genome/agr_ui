b25f4ff66bc080e889092dc6b13ebaceccf68ebe
Sibyl <sibyl@wormbase.org>
2021-07-26 12:45:36 -0400

    import React

--- libs/app-shell/src/lib/page-footer/PageFooterLinkGroupLabel.js
+++ libs/app-shell/src/lib/page-footer/PageFooterLinkGroupLabel.js
@@ -1,5 +1,5 @@
 import tw from 'twin.macro';
-import { Children } from 'react';
+import React from 'react';
 
 export default function PageFooterLinkGroupLabel({ children }) {
   return <span tw="block text-sm font-bold">{children}</span>;
