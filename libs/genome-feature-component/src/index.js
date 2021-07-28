51547f8ee689c1a4857d905ad9e8d667f2606891
Sibyl <sibyl@wormbase.org>
2021-07-28 14:54:28 -0400

    export tracks/

--- libs/genome-feature-component/src/index.js
+++ libs/genome-feature-component/src/index.js
@@ -1,1 +1,10 @@
-export { default as GenomeFeatureViewer } from './GenomeFeatureViewer.js';
+export { default as GenomeFeatureViewer } from './GenomeFeatureViewer';
+
+export { default as IsoformAndVariantTrack } from './tracks/IsoformAndVariantTrack';
+export { default as IsoformEmbeddedVariantTrack } from './tracks/IsoformEmbeddedVariantTrack';
+export { default as IsoformTrack } from './tracks/IsoformTrack';
+export { default as ReferenceTrack } from './tracks/ReferenceTrack';
+export { TRACK_TYPE } from './tracks/TrackTypeEnum';
+export { default as VariantTrack } from './tracks/VariantTrack';
+export { default as VariantTrackGlobal } from './tracks/VariantTrackGlobal';
+
