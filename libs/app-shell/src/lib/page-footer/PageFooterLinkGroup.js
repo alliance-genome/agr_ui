21f3df5574686fa0dafaed89d688a186fa1f15b9
Sibyl <sibyl@wormbase.org>
2021-07-28 00:29:45 -0400

    explictly make PageFooterLinkGroup flex-col layout

--- libs/app-shell/src/lib/page-footer/PageFooterLinkGroup.js
+++ libs/app-shell/src/lib/page-footer/PageFooterLinkGroup.js
@@ -2,5 +2,5 @@ import tw from 'twin.macro';
 import React from 'react';
 
 export default function PageFooterLinkGroup({ children }) {
-  return <div tw="py-2">{children}</div>;
+  return <div tw="py-2 flex flex-col">{children}</div>;
 }
