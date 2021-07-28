0633dab412b6d05d9cb44c4f467dc4ef7bdd941f
Sibyl <sibyl@wormbase.org>
2021-07-27 22:05:41 -0400

    header bottom border

--- libs/app-shell/src/lib/app-shell.js
+++ libs/app-shell/src/lib/app-shell.js
@@ -24,7 +24,7 @@ export function AppShell({
     <LinkRenderProvider renderLink={renderLink}>
       <MODThemeProvider mod={mod}>
         <div tw="flex flex-col min-h-screen">
-          <header tw="z-40">
+          <header tw="z-40 border-b">
             <div tw="flex flex-wrap items-center gap-x-4 gap-y-2 mx-4 my-2">
               <button
                 tw="lg:hidden text-gray-700 h-6 w-6"
