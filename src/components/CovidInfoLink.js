0eee6035f9af35608751d889bbe224a244fffa8e
Manuel Luypaert <luypaert@ebi.ac.uk>
2021-10-28 11:55:31 +0100

    Fixes for "NIH community call" revert
    This reverts commit 13ab38d8a1b88a7be29a7d18c3c555fd20896d91.

--- src/components/CovidInfoLink.js
+++ src/components/CovidInfoLink.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-
 const CovidInfoLink = () => {
   return (
     <Link className='text-decoration-none' to='/coronavirus-resources'>
