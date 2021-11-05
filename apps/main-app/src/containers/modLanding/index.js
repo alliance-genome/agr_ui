900add438e9e38980f6f4df4181a320d0d978e46
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-11-04 17:39:59 -0700

    feat(about): about page with static content - one page for each mod
    first implementation of the about page for WormBase based on components without using template pages
    
    https://agr-jira.atlassian.net/browse/SCRUM-688

--- apps/main-app/src/containers/modLanding/index.js
+++ apps/main-app/src/containers/modLanding/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import HeadMetaTags from '../../components/headMetaTags';
-import AboutSection from './AboutSection';
 import style from './style.scss';
+import PropTypes from 'prop-types';
+import MainWB from "./MainWB";
 
-const MODLanding = () => {
+const MODLanding = ({modId}) => {
   return (
     <div>
       <HeadMetaTags title='Landing page' />
 
       <section className={style.section}>
         <div className={`${style.contentContainer}`}>
+          {modId === 'wb' ? <MainWB/> : null}
         </div>
       </section>
-
-      <AboutSection />
     </div>
   );
 };
 
 export default MODLanding;
+
+MODLanding.propTypes = {
+  modId: PropTypes.string.isRequired
+}
