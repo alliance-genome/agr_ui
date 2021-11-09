3d75cb1b36045d69fe51d9b4198c17211b6b0d09
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-11-08 16:20:46 -0800

    feat(modLanding): new file structure based on separate folders with different components for each mod

---
+++ apps/main-app/src/containers/modLanding/wb/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        WormBase is an international consortium of biologists and computer scientists providing the research community
+        with accurate, current, accessible information concerning the genetics, genomics and biology of
+        <i>C. elegans</i> and related nematodes. Founded in 2000, the WormBase Consortium is led by Paul Sternberg
+        (CalTech), Matt Berriman (The Wellcome Trust Sanger Institute), Kevin Howe (EBI), and Lincoln Stein (The
+        Ontario Institute for Cancer Research).
+      </p>
+      <p>
+        WormBase is a founding member of the Alliance of Genome Resources Project.
+      </p>
+    </div>
+  );
+}
+
+export default About;
