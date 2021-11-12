a579dfe62f1a9666c6c8566bcc6a29ac3f9821b4
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-11-12 09:46:35 -0800

    feat(modLanding): applied alliance style to mod landing pages

--- apps/main-app/src/containers/modLanding/sgd/Main.js
+++ apps/main-app/src/containers/modLanding/sgd/Main.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import About from './About';
 import style from '../style.scss';
+import Resources from "./Resources";
+import News from "./News";
+import Meetings from "./Meetings";
 
 const Main = () => {
   return (
     <div>
-      <h1>SGD</h1>
-      <section className={style.section}>
-        <div>
-          <About/>
+      <div className={style.bannerWB}>
+        <div className={style.secondaryNavEmptyRow} />
+        <div className={`row ${style.secondaryNavSGD}`}>
+          <div className='container'>
+            <h1>Saccharomyces Genome Database</h1>
+          </div>
         </div>
-      </section>
+      </div>
+      <div className='container'>
+        <About/>
+      </div>
+      <div className='container'>
+        <div className="row">
+          <div className="col-sm-4">
+            <section className={style.section}>
+              <div className={`${style.contentContainer}`}>
+                <Resources/>
+              </div>
+            </section>
+          </div>
+          <div className="col-sm-4">
+            <section className={style.section}>
+              <div className={`${style.contentContainer}`}>
+                <News/>
+              </div>
+            </section>
+          </div>
+          <div className="col-sm-4">
+            <section className={style.section}>
+              <div className={`${style.contentContainer}`}>
+                <Meetings/>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
