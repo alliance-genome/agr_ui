8f4a3ee8c2b8a9be866b41c148ad0f2db43c63c6
Paulo Nuin <6273+nuin@users.noreply.github.com>
2021-11-12 10:44:43 -0700

    styling WB landing page

--- apps/main-app/src/containers/modLanding/wb/About.js
+++ apps/main-app/src/containers/modLanding/wb/About.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import style from '../style.scss';
 
 const About = () => {
   return (
     <div>
-      <h2>About</h2>
-      <p>
-        WormBase is an international consortium of biologists and computer scientists providing the research community
-        with accurate, current, accessible information concerning the genetics, genomics and biology of
-        <i>C. elegans</i> and related nematodes. Founded in 2000, the WormBase Consortium is led by Paul Sternberg
-        (CalTech), Matt Berriman (The Wellcome Trust Sanger Institute), Kevin Howe (EBI), and Lincoln Stein (The
-        Ontario Institute for Cancer Research).
-      </p>
-      <p>
-        WormBase is a founding member of the Alliance of Genome Resources Project.
-      </p>
-      <h3>
-        To visit Wormbase's main website click <a href="https://wormbase.org"> here</a>.
-      </h3>
+      <div>
+          <div className='container-fluid'>
+            <div className={style.secondaryNavEmptyRow} />
+            <div className={`row ${style.secondaryNav}`}>
+              <div className='container'>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div className={style.secondaryNavEmptyRow} >
+        <div className={style.aboutMenuContainer}>
+          <h2>WormBase</h2>
+        </div>
+      </div>
+      <div className={style.contentContainer}>
+        <div className='row'>
+          <div className='col-md-12'>
+            <p className='lead'>
+              WormBase is an international consortium of biologists and computer scientists providing the research community
+              with accurate, current, accessible information concerning the genetics, genomics and biology of
+              <i>C. elegans</i> and related nematodes. Founded in 2000, the WormBase Consortium is led by Paul Sternberg
+              (CalTech), Matt Berriman (The Wellcome Trust Sanger Institute), Kevin Howe (EBI), and Lincoln Stein (The
+              Ontario Institute for Cancer Research).
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
