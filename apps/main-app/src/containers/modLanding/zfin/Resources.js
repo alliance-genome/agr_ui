bc368a0a181b0238a927bdd0681214cdf716a26c
Paulo Nuin <6273+nuin@users.noreply.github.com>
2021-11-12 20:59:51 -0700

    ZFIN landing page added, including banner image, colours, logo, etc

---
+++ apps/main-app/src/containers/modLanding/zfin/Resources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../style.scss';
+
+const Resources = () => {
+  return (
+    <div>
+      <h2 className={style.sectionTitle}>Resources</h2>
+    </div>
+  );
+}
+
+export default Resources;
