d416060a39f8dcf2261891b1697c5c5a040b2372
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-11-22 11:47:43 -0800

    refactor(modLanding): created template page for all MODs
    - parametrized inner components and moved MOD specific content to content.js file
    - created a Main mod landing page that feeds MOD specific content to the inner components
    - refactored about and added new LinkToMOD component
    - removed files from old structure
    - renamed images in asset folder

---
+++ apps/main-app/src/containers/modLanding/Main.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import HeadMetaTags from '../../components/headMetaTags';
+import PropTypes from 'prop-types';
+import style from './style.scss';
+import {MODContent} from './content';
+import About from "./About";
+import LinkToMOD from "./LinkToMOD";
+import Title from "./Title";
+
+const MODLanding = ({modId}) => {
+
+  const content = MODContent[modId];
+
+  return (
+    <div>
+      <HeadMetaTags title='Members' />
+      <div>
+        <Title bannerStyle={content.bannerStyle} titleBarStyle={content.titleBarStyle} logoImgSrc={content.logoImgSrc}
+               modFullName={content.modFullName}/>
+        <About htmlContent={content.about} modShortName={content.modShortName} linkToMod={content.link} />
+        <div className='container'>
+          <div className="row">
+            <div className="col-sm-4">
+              <section className={style.section}>
+                <div className={`${style.contentContainer}`}>
+
+                </div>
+              </section>
+            </div>
+            <div className="col-sm-4">
+              <section className={style.section}>
+                <div className={`${style.contentContainer}`}>
+                </div>
+              </section>
+            </div>
+            <div className="col-sm-4">
+              <section className={style.section}>
+                <div className={`${style.contentContainer}`}>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MODLanding;
+
+MODLanding.propTypes = {
+  modId: PropTypes.string.isRequired
+}
--- apps/main-app/src/containers/modLanding/Resources.js
+++ apps/main-app/src/containers/modLanding/Resources.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from '../style.scss';
+import style from './style.scss';
 
 const Resources = () => {
   return (
