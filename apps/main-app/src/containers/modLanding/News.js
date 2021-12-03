6828a275a36528664c3ab0deb392680a718e6c68
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-12-03 14:44:10 -0800

    feat(modLanding): added 'more news' link

--- apps/main-app/src/containers/modLanding/News.js
+++ apps/main-app/src/containers/modLanding/News.js
@@ -3,21 +3,21 @@ import style from './style.scss';
 import PropTypes from "prop-types";
 import WordpressNews from "./wordpressNews"
 
-const News = ({urlNewsMod, fetchNewsCount, modShortName, linkToMod}) => {
+const News = ({urlNewsMod, fetchNewsCount, linkToNewsPage}) => {
   return (
     <div className='container'>
       <div className={style.section}>
         <h2 className={style.sectionTitle}>News</h2>
-        <WordpressNews urlNewsMod={urlNewsMod} fetchNewsCount={fetchNewsCount} />
+        <WordpressNews urlNewsMod={urlNewsMod} fetchNewsCount={fetchNewsCount} linkToNewsPage={linkToNewsPage} />
       </div>
     </div>
   );
 }
 
 News.propTypes = {
-  //htmlContent: PropTypes.string.isRequired,
-  modShortName: PropTypes.string.isRequired,
-  linkToMod: PropTypes.string.isRequired
+  urlNewsMod: PropTypes.string.isRequired,
+  fetchNewsCount: PropTypes.number.isRequired,
+  linkToNewsPage: PropTypes.string.isRequired
 }
 
 export default News;
