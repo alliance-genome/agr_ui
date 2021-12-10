b5ddc43fd83e1d15f4a96201acfb4c5e136d11aa
gildossantos <gilbertodossantos74@gmail.com>
2021-12-10 07:24:18 -0500

    point icons in homepage Members section to MOD landing pages

--- apps/main-app/src/containers/homepage/MembersSection.js
+++ apps/main-app/src/containers/homepage/MembersSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import style from './style.scss';
 
@@ -8,31 +9,32 @@ const MembersSection = () => {
       <div className={style.contentContainer}>
         <h1>Members</h1>
         <div className='d-flex flex-wrap'>
-          <a className={style.member} href='https://flybase.org/'>
+          {/* <a className={style.member} href='https://flybase.org/'> */}
+          <a className={style.member} href='/members/fb'>
             <img src='https://alliancegenome.files.wordpress.com/2016/11/logo_flybase.png' />
             FlyBase
           </a>
-          <a className={style.member} href='http://www.informatics.jax.org/'>
+          <a className={style.member} href='/members/rgd'>
             <img src='https://alliancegenome.files.wordpress.com/2016/11/logo_rgd.png' />
             MGD
           </a>
-          <a className={style.member} href='https://rgd.mcw.edu/'>
+          <a className={style.member} href='/members/mgi'>
             <img src='https://alliancegenome.files.wordpress.com/2016/11/logo_mgd.png' />
             RGD
           </a>
-          <a className={style.member} href='https://www.yeastgenome.org/'>
+          <a className={style.member} href='/members/sgd'>
             <img src='https://alliancegenome.files.wordpress.com/2016/11/logo_sgd.png' />
             SGD
           </a>
-          <a className={style.member} href='https://www.wormbase.org/'>
+          <a className={style.member} href='/members/wb'>
             <img src='https://alliancegenome.files.wordpress.com/2016/11/logo_wormbase.png' />
             WormBase
           </a>
-          <a className={style.member} href='https://zfin.org/'>
+          <a className={style.member} href='/members/zfin'>
             <img src='https://alliancegenome.files.wordpress.com/2016/11/logo_zfin.png' />
             ZFIN
           </a>
-          <a className={style.member} href='http://www.geneontology.org/'>
+          <a className={style.member} href='/members/goc'>
             <img src='https://alliancegenome.files.wordpress.com/2016/11/logo_goc.png' />
             GOC
           </a>
