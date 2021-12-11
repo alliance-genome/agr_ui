200b70e85531a39cc79c5d18ce438ea19e9cd085
Juancarlos <azurebrd@pop-os.localdomain>
2021-12-10 21:12:59 -0800

    SCRUM-957 more changes to css and layout.  Importing fonts.  About link to MOD is a button now with same color stylting as the titlebar.  Tests probably fail, will fix later.  resource section paragraphs have less margin-bottom.  h4 news have more padding-top.  more news link lowercased and more vertical padding.  section text uses Open Sans with more padding and default font size instead of reduced.

--- apps/main-app/src/containers/modLanding/LinkToMOD.js
+++ apps/main-app/src/containers/modLanding/LinkToMOD.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import style from './style.scss';
 
-const LinkToMOD = ({modName, linkAddress}) => {
+const LinkToMOD = ({modName, linkAddress, titleBarStyle}) => {
   return (
     <p>
-      <b>To visit {modName} click <a href={linkAddress}> here</a>.</b>
+      <a href={linkAddress} >
+        <button className={`${style.buttonVisitMod} ${titleBarStyle}`} >Visit {modName}</button>
+      </a>
     </p>
   );
 }
