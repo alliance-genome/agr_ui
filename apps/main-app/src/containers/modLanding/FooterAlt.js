dda278d88faa7fe26c500ee2ba2886c686fbf99e
Juancarlos <azurebrd@pop-os.localdomain>
2021-12-19 18:11:53 -0800

    SCRUM-831 Refactored the Footer code to be more concise in the same branch.  Also added the hex icon and modShortName to the footer. Modified style.scss to add a modFooter__MGD a color, so the footer links look right. Using css variables for the mod primary and secondary colors, so they can be changed at the css root instead of in each class.  Ticket comment has more thoughts.

--- apps/main-app/src/containers/modLanding/FooterAlt.js
+++ apps/main-app/src/containers/modLanding/FooterAlt.js
@@ -1,43 +1,43 @@
 import style from './style.scss';
 import PropTypes from "prop-types";
 
-const FooterAlt = ({links, note, footerStyle, logoImgSrc, modShortName}) => {
-
-  let generateHtml = (links, modShortName, note) => {
-    if (!links){
-        return "";
-    }
-    let htmlString = '<div class="container-fluid">' +
-                     '   <div class="row">' +
-                     '     <div class="col-med-2 col-md-2 col-lg-2 col-xl-2">' +
-                     '     </div>';
-    htmlString += '<div class="col-med-10 col-md-10 col-lg-10 col-xl-10">' +
-                   '<div class="row">';
-    for (let i=0; i< links.length; i++){
-        htmlString += '<div class="col-med-2 col-md-2 col-lg-2 col-xl-2">';
-        htmlString += `      <a href="${links[i][1]}">${links[i][0]}</a>`;
-        htmlString += '</div>';
-    }
-    htmlString += "</div></div></div>"; // end container row container
-    if (note){
-        htmlString += `<div><p>${note}</p></div>`;
-    }
-    return htmlString;
-  }
-
+const FooterAlt = ({links, note, footerStyle, logoImgSrc, titleBarStyle, modShortName}) => {
   return (
       <div className={`${footerStyle} ${style.modFooter}`}>
         <div className={`container ${style.modFooterContainer}`}>
-          <div className={style.modFooterText} dangerouslySetInnerHTML={{__html: generateHtml(links, modShortName, note)}}/>
+          <div className={`{style.modFooterText} ${titleBarStyle}`}>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-med-2 col-md-2 col-lg-2 col-xl-2">
+                  <img src={logoImgSrc} height="50px" /> <span className={style.titleBarText} dangerouslySetInnerHTML={{__html: modShortName}}/>
+                </div>
+                <div className="col-med-10 col-md-10 col-lg-10 col-xl-10">
+                  <div className="row">
+                  { links && links.map(link => {
+                      return (
+                        <div className="col-med-2 col-md-2 col-lg-2 col-xl-2">
+                          <a href={link[1]}>{link[0]}</a>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                { note && <div><p>{note}</p></div> }
+              </div>
+            </div>
+          </div>
         </div>
       </div>
   );
-}
+};
 
 FooterAlt.propTypes = {
   links: PropTypes.array.isRequired,
   note: PropTypes.string,
   footerStyle: PropTypes.string.isRequired,
+  logoImgSrc: PropTypes.string.isRequired,
+  titleBarStyle: PropTypes.string.isRequired,
+  modShortName: PropTypes.string.isRequired,
 }
 
 export default FooterAlt;
