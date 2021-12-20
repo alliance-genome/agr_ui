755bf32b27e4cbe2c0a82a4ebd752950143197fa
Paulo Nuin <nuin@genedrift.org>
2021-12-19 20:55:00 -0700

    MOD log on the bottom is now a link to the MOD site changes varianles in the SCSS file for a simpler syntax

--- apps/main-app/src/containers/modLanding/FooterAlt.js
+++ apps/main-app/src/containers/modLanding/FooterAlt.js
@@ -1,7 +1,7 @@
 import style from './style.scss';
 import PropTypes from "prop-types";
 
-const FooterAlt = ({links, note, footerStyle, logoImgSrc, titleBarStyle, modShortName}) => {
+const FooterAlt = ({link, links, note, footerStyle, logoImgSrc, titleBarStyle, modShortName}) => {
   return (
       <div className={`${footerStyle} ${style.modFooter}`}>
         <div className={`container ${style.modFooterContainer}`}>
@@ -9,7 +9,7 @@ const FooterAlt = ({links, note, footerStyle, logoImgSrc, titleBarStyle, modShor
             <div className="container-fluid">
               <div className="row">
                 <div className="col-med-2 col-md-2 col-lg-2 col-xl-2">
-                  <img src={logoImgSrc} height="50px" /> <span className={style.titleBarText} dangerouslySetInnerHTML={{__html: modShortName}}/>
+                  <a href={link}><img src={logoImgSrc} height="50px" /></a> <span className={style.titleBarText} dangerouslySetInnerHTML={{__html: modShortName}}/>
                 </div>
                 <div className="col-med-10 col-md-10 col-lg-10 col-xl-10">
                   <div className="row">
