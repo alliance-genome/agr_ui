1a778f8ac1f7876f0e76a6eb8c132349326a2a0d
Juancarlos <azurebrd@pop-os.localdomain>
2021-12-21 19:17:04 -0800

    Some tests did not have new parameters for components, Footer elements did not have a unique key, using link name since there is no counter.  Some tests still do not pass.

--- apps/main-app/src/containers/modLanding/FooterAlt.js
+++ apps/main-app/src/containers/modLanding/FooterAlt.js
@@ -15,7 +15,7 @@ const FooterAlt = ({link, links, note, footerStyle, logoImgSrc, titleBarStyle, m
                   <div className="row">
                   { links && links.map(link => {
                       return (
-                        <div className="col-med-2 col-md-2 col-lg-2 col-xl-2">
+                        <div key={link[0]} className="col-med-2 col-md-2 col-lg-2 col-xl-2">
                           <a href={link[1]}><span dangerouslySetInnerHTML={{__html: link[0]}}/></a>
                         </div>
                       );
