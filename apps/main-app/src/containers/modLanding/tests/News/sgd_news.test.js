8066e63230fca3123ad10c655c76d64333203b54
ianlongden <ianlongden@morgan.harvard.edu>
2022-01-12 17:05:58 -0500

    put tests in sub directories

--- apps/main-app/src/containers/modLanding/tests/News/sgd_news.test.js
+++ apps/main-app/src/containers/modLanding/tests/News/sgd_news.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import NewsFlybase from "../NewsFlybase";
+import WordpressNews from "../../wordpressNews";
 import thunk from 'redux-thunk';
 import configureMockStore from "redux-mock-store";
-import {MODContent} from '../content';
+import {MODContent} from '../../content';
 import { Provider } from 'react-redux';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
- const content = MODContent['flybase'];
+ const content = MODContent['sgd'];
 
 
  const initialState = {isLoading:false, payload: []}
@@ -25,15 +25,16 @@ import '@testing-library/jest-dom'
 
  describe('App', () => {
      beforeEach(() => {
-       render(<Provider store={store}> <NewsFlybase
-              urlNewsMod={content.flybaseNewsAPI}
-              linkToNewsPage={content.linkToNewsPage}
-              fetchNewsCount={content.fetchNewsCount}/></Provider>)
+        render(<Provider store={store}> <WordpressNews           
+            urlNewsMod={content.wordpressNewsBaseURL}
+            fetchNewsCount={content.fetchNewsCount}
+            linkToNewsPage={content.linkToNewsPage}
+            sectionStyle={content.sectionStyle} /></Provider>)
      });
 
      it('should render successfully', async () => {
        const news_div = await waitFor(() => screen.findByTestId("div_news_2"), { timeout: 8000 });
-       expect(screen.getByTestId("div_news_2")).toHaveTextContent("Update on fly title 1");
+       expect(screen.getByTestId("div_news_2")).toHaveTextContent("Update on sgd title 1");
      }, 10000);
 
      it('should contain a link', async () => {
@@ -58,23 +59,37 @@ import '@testing-library/jest-dom'
       it('news href should have a header and be equal to', async () => {
          const news_div = await waitFor(() => screen.findByTestId("div_news_2"), { timeout: 8000 });
          const head = screen.getByTestId("header_news_2");
-         expect(head).toHaveTextContent("Update on fly title 1");
+         expect(head).toHaveTextContent("Update on sgd title 1");
       }, 10000);
 
       it('news excerpt', async () => {
          const news_div = await waitFor(() => screen.findByTestId("div_news_2"), { timeout: 8000 });
          const news_text = screen.getByTestId("text_news_2");
-         expect(news_text).toHaveTextContent("Excerpt 1");
+         expect(news_text).toHaveTextContent("excerpt 1");
          expect(screen.getByTestId("div_news_2")).toContainElement(news_text);
       }, 10000);
 
-      // Flybase has no link to more news. If it ever does uncomment the below.
+     
       it('more news link checks', async () => {
          const news_div = await waitFor(() => screen.findByTestId("div_news_2"), { timeout: 8000 });
          const more_div = screen.getByTestId("more_news_div");
          const more_link = screen.getByTestId("more_news_link");
          expect(more_div).toContainElement(more_link);
-         expect(more_link).toHaveAttribute('href', 'https://flybase.org/commentaries');
+         expect(more_link).toHaveAttribute('href', 'https://yeastgenomeblog.wordpress.com/');
       }, 10000);
 
+
+      it('Check news with status of "private" is NOT displayed', async () => {
+        const news_div = await waitFor(() => screen.findByTestId("div_news_3"), { timeout: 8000 });
+        // check previous is okay
+        const ex_2 = screen.getByTestId("text_news_3")
+        expect(ex_2).toHaveTextContent("excerpt 2");
+
+        // check after is okay  
+        // NOTE: counter is still the next one BUT excerpt has jumnped by 2
+        //       So one has been missed.
+        const ex_4 = screen.getByTestId("text_news_4")
+        expect(ex_4).toHaveTextContent("excerpt 4");
+
+       }, 10000);
      });
