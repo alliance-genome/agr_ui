e60a8e7641e1e94075c85740e1336b01fdf0290b
ianlongden <ianlongden@morgan.harvard.edu>
2022-01-13 16:22:22 -0500

    New footer tests for each member

--- apps/main-app/src/containers/modLanding/FooterAlt.js
+++ apps/main-app/src/containers/modLanding/FooterAlt.js
@@ -2,21 +2,31 @@ import style from './style.scss';
 import PropTypes from "prop-types";
 
 const FooterAlt = ({link, links, note, footerStyle, logoImgSrc, titleBarStyle, modShortName}) => {
+  let count = 0;
   return (
       <div className={`${footerStyle} ${style.modFooter}`}>
         <div className={`container ${style.modFooterContainer}`}>
           <div className={`{style.modFooterText} ${titleBarStyle}`}>
             <div className="container-fluid">
               <div className="row">
-                <div className="col-med-2 col-md-2 col-lg-2 col-xl-2">
-                  <a href={link}><img src={logoImgSrc} height="50px" /></a> <span className={style.modFooterLabel} dangerouslySetInnerHTML={{__html: modShortName}}/>
+                <div data-testid={'footer_div'} className="col-med-2 col-md-2 col-lg-2 col-xl-2">
+                  <a data-testid={'footer_link'} href={link}>
+                      <img data-testid={'footer_hex'} src={logoImgSrc} height="50px" />
+                  </a> 
+                  <span className={style.modFooterLabel} 
+                        dangerouslySetInnerHTML={{__html: modShortName}}
+                        data-testid={'modname_footer'}/>
                 </div>
                 <div className="col-med-10 col-md-10 col-lg-10 col-xl-10">
                   <div className="row">
                   { links && links.map((link, index) => {
+                      count++;
                       return (
                         <div key={index} className="col-med-4 col-md-4 col-lg-4 col-xl-4">
-                          <a href={link[1]}><span dangerouslySetInnerHTML={{__html: link[0]}}/></a>
+                          <a href={link[1]} data-testid={'href_footer_' + count} >
+                            <span dangerouslySetInnerHTML={{__html: link[0]}} 
+                                  data-testid={'footer_label_' + count}/>
+                          </a>
                         </div>
                       );
                     })}
