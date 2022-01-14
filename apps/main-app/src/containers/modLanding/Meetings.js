3dc06967c60f2d7490b2c6feeec24f5b9de1a0ae
Juancarlos <azurebrd@pop-os.localdomain>
2022-01-13 18:22:25 -0800

    SCRUM-627 Meetings for mods with links only.  WormBase and ZFIN

--- apps/main-app/src/containers/modLanding/Meetings.js
+++ apps/main-app/src/containers/modLanding/Meetings.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import style from './style.scss';
+import PropTypes from "prop-types";
+import GoogleapisMeetings from "./googleapisMeetings"
 
-const Meetings = () => {
+const Meetings = ({content}) => {
   return (
-    <div>
-      <h2 className={style.sectionTitle}>Meetings</h2>
+    <div className={`container ${style.containerExtra}`}>
+      <div className={`${style.section} ${content.sectionStyle}`}>
+        <h2 className={style.sectionTitle}>Meetings</h2>
+        {(() => {
+          if (content.meetingsURL) { 
+            return (<h5 className={style.externalNews} ><a href={content.meetingsURL}>Click here for the latest meetings from {content.modShortName}</a></h5>); }
+          return (<div>No Meetings</div>);
+        })()}
+      </div>
     </div>
   );
+}
+
+Meetings.propTypes = {
+  content: PropTypes.object.isRequired
 }
 
 export default Meetings;
