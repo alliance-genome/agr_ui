c317661828d03f96826e6241308e333e83013f68
ianlongden <ianlongden@morgan.harvard.edu>
2022-01-14 10:43:06 -0500

    Resource container tests

--- apps/main-app/src/containers/modLanding/Resources.js
+++ apps/main-app/src/containers/modLanding/Resources.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import style from './style.scss';
 
-const Resources = ({htmlContent, sectionStyle}) => {
+const Resources = ({links, sectionStyle}) => {
   return (
     <div>
       <div className={`container ${style.containerExtra}`}>
-        <div className={`${style.section} ${sectionStyle}`}>
-        <h2 className={style.sectionTitle}>Resources</h2>
-          <div className={style.resourceDiv} dangerouslySetInnerHTML={{__html: htmlContent }} />
+        <div data-testid={'resources_topdiv'} className={`${style.section} ${sectionStyle}`}>
+        <h2 data-testid={'resources_header'} className={style.sectionTitle}>Resources</h2>
+          <div data-testid={'resources_link_div'} className={style.resourceDiv} >
+          { links && links.map((link, index) => {
+                      return (
+                        <div key={index}>
+                          <p>
+                            <a href={link[1]} data-testid={'href_resources_' + index} >
+                            <span dangerouslySetInnerHTML={{__html: link[0]}} 
+                                  data-testid={'resources_label_' + index}/>
+                          </a></p>
+                        </div>
+                      );
+                    })}
+          </div>
         </div>
       </div>
     </div>
