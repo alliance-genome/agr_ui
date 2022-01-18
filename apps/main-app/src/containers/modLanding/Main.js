efa30f6372722bbb48900990c02bc2468f65ee30
ianlongden <ianlongden@morgan.harvard.edu>
2022-01-18 11:41:04 -0500

    Basic whole page tests. Contianers done separately so this is just making sure they play nicely together

--- apps/main-app/src/containers/modLanding/Main.js
+++ apps/main-app/src/containers/modLanding/Main.js
@@ -15,32 +15,34 @@ const MODLanding = ({modId}) => {
   const content = MODContent[modId];
 
   return (
-    <div style={{backgroundColor: '#f5f5f5'}}>
+    <div data-testid={'modlanding_div'} style={{backgroundColor: '#f5f5f5'}}>
       <HeadMetaTags title={content.modShortName} />
       <Title bannerStyle={content.bannerStyle} titleBarStyle={content.titleBarStyle} logoImgSrc={content.logoImgSrc}
              modFullName={content.modFullName}/>
       <div className={`container`}>
         <div className={`container ${style.containerExtra}`}>
           <div className="row">
-            <div className={`col-xl-8 col-lg-8 col-md-8 col-sm-12 ${style.noPadding}`}>
+            <div data-testid={'modlanding_middle'} className={`col-xl-8 col-lg-8 col-md-8 col-sm-12 ${style.noPadding}`}>
               <About htmlContent={content.about} modVisitButtonText={content.modVisitButtonText} linkToMod={content.link}
                                                  sectionStyle={content.sectionStyle} titleBarStyle={content.titleBarStyle} />
               {content.hasNews && <News content={content} />}
               {content.hasMeetings && <Meetings content={content} />}
             </div>
-            <div className={`col-xl-4 col-lg-4 col-md-4 col-sm-12 ${style.noPadding}`}>
+            <div data-testid={'modlanding_resources'} className={`col-xl-4 col-lg-4 col-md-4 col-sm-12 ${style.noPadding}`}>
               <Resources links={content.resources} sectionStyle={content.sectionStyle} />
             </div>
           </div>
         </div>
       </div>
-      <FooterAlt  link={content.link}
-                  links={content.footer}
-                  note={content.footerNote}
-                  footerStyle={content.footerStyle}
-                  logoImgSrc={content.logoImgSrc}
-                  titleBarStyle={content.titleBarStyle}
-                  modShortName={content.modShortName}/>
+      <div data-testid={'modlanding_footer'} >
+        <FooterAlt  link={content.link}
+                    links={content.footer}
+                    note={content.footerNote}
+                    footerStyle={content.footerStyle}
+                    logoImgSrc={content.logoImgSrc}
+                    titleBarStyle={content.titleBarStyle}
+                    modShortName={content.modShortName}/>
+      </div>
     </div>
   );
 };
