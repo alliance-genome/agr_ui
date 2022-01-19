f5ae75e967db5099137bb064df7fa8044e290e04
ianlongden <ianlongden@morgan.harvard.edu>
2022-01-19 10:49:59 -0500

    Removed old code and its refernces

--- apps/mod-landing-pages/components/news-list-item/news-list-item.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import NewsListItem from './news-list-item';
-describe('NewsListItem', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<NewsListItem />);
-    expect(baseElement).toBeTruthy();
-  });
-});
