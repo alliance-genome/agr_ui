94b1a912b8b3def6fe9875999ccb47a394778bd8
Juancarlos <azurebrd@its.caltech.edu>
2022-04-23 01:10:35 +0000

    can inject a slug, tested with about-us slug, modeled after code in wordpress/ instead of modLanding/wordpressNews

---
+++ apps/main-app/src/containers/wordpress/wordpressInject.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './style.scss';
+import ReplaceLinks from './ReplaceLinks';
+import usePageLoadingQuery from '../../hooks/usePageLoadingQuery';
+import { WORDPRESS_PAGE_BASE_URL } from '../../constants';
+import fetchWordpress from '../../lib/fetchWordpress';
+import LoadingSpinner from '../../components/loadingSpinner';
+
+const WordpressInject = ({slug}) => {
+  const {
+    data: post,
+    isLoading,
+    isError
+  } = usePageLoadingQuery(WORDPRESS_PAGE_BASE_URL + slug, fetchWordpress);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (isError || !post) {
+    return null;
+  }
+
+  const title = post.title.rendered;
+  return (
+    <div className={style.wordPressContainer}>
+      <div className='container'>
+        <div className='row'>
+          <ReplaceLinks html={post.content.rendered} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+WordpressInject.propTypes = {
+  slug: PropTypes.string.isRequired,
+};
+
+export default WordpressInject;
