3df62557ef37e514b36b05fad43d122999a55115
Adam Gibson <agibson@mcw.edu>
2022-12-07 16:40:02 -0600

    SCRUM-2021 added code to remove duplicate refs and providers

--- apps/main-app/src/components/dataTable/referencesCellCuration.js
+++ apps/main-app/src/components/dataTable/referencesCellCuration.js
@@ -4,12 +4,23 @@ import ExternalLink from '../ExternalLink';
 import { CollapsibleList } from '../collapsibleList';
 import { getMultipleReferencesCuriesAndUrls } from "./utils";
 
+const removeDuplicates = (refs) => {
+  const newArray = refs.map((ref) => [ref.curie, ref]);
+  const newMap = new Map(newArray);
+  const iterator = newMap.values();
+  const uniqueRefs = [...iterator];
+
+  return uniqueRefs;
+}
+
 const ReferencesCellCuration = (refs) => {
   const refStringsAndUrls = getMultipleReferencesCuriesAndUrls(refs);
+	const uniqueRefs = removeDuplicates(refStringsAndUrls);
+	console.log(uniqueRefs);
   return refs &&
     <CollapsibleList>
       {
-        refStringsAndUrls.map(({ curie, url }) => {
+        uniqueRefs.map(({ curie, url }) => {
           return <ExternalLink href={url} key={curie} title={curie}>{curie}</ExternalLink>;
         })
       }
