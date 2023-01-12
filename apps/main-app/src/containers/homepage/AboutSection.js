90236fe3019e4e95208a85d76d1e526d72ba9617
Ketaki Thorat <72769853+kthorat-prog@users.noreply.github.com>
2023-01-12 10:56:49 -0600

    Added wordpress slug for About us section

--- apps/main-app/src/containers/homepage/AboutSection.js
+++ apps/main-app/src/containers/homepage/AboutSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WordpressInject from '../wordpress/wordpressInject';
 
 import style from './style.scss';
 
@@ -8,22 +9,7 @@ const AboutSection = () => {
       <div className={style.contentContainer}>
         <div className='row'>
           <div className='col-lg-12'>
-            <h1>What is the Alliance of Genome Resources?</h1>
-            <p className='lead'>
-              A consortium of 7 model organism databases (MODs) and the Gene
-              Ontology (GO) Consortium whose goal is to provide an integrated view
-              of their data to all biologists, clinicians and other interested
-              parties.
-            </p>
-
-            <p className='lead'>
-              The primary mission of the Alliance of Genome Resources (the Alliance)
-              is to develop and maintain sustainable genome information resources
-              that facilitate the use of diverse model organisms in understanding
-              the genetic and genomic basis of human biology, health and disease.
-              This understanding is fundamental for advancing genome biology
-              research and for translating human genome data into clinical utility.
-            </p>
+            <WordpressInject  slug='agr-summary-homepage' />
           </div>
           <div className='col-md-4'>
             <a
