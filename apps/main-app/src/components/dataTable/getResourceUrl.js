15384801dbd98bb1cf7c3f4136ea844b37991c69
Adam Gibson <agibson@mcw.edu>
2023-03-08 13:18:23 -0600

    SCRUM-2597 fixed subtype bug and added subtypes to annotationDetails

--- apps/main-app/src/components/dataTable/getResourceUrl.js
+++ apps/main-app/src/components/dataTable/getResourceUrl.js
@@ -1,26 +1,33 @@
 import { resourceDescriptors } from "../../../../../dist/resourceDescriptors";
 
-export const getResourceUrl = (curie, type) => {
+export const getResourceUrl = (curie, type, subtype ) => {
   const [prefix, id] = curie.split(':');
 
-  if(type === "AffectedGenomicModel" ){
-		if(prefix === "MGI") type = "genotype";
-		if(prefix === "ZFIN") type = "fish";
+  if(type === "AffectedGenomicModel" && !subtype){
+    subtype = {};
+		if(prefix === "MGI") subtype.name = "genotype";
+		if(prefix === "ZFIN") subtype.name = "fish";
 		if(prefix === "WB") {
-			if(curie.toLowerCase().includes("genotype")) type = "genotype";
-			if(curie.toLowerCase().includes("strain")) type = "strain";
+			if(curie.toLowerCase().includes("genotype")) subtype.name = "genotype";
+			if(curie.toLowerCase().includes("strain")) subtype.name = "strain";
 		}
-		if(prefix === "RGD") type = "strain";
+		if(prefix === "RGD") subtype.name = "strain";
   }
+
   let resource;
-  if(!type){
+  if(subtype){
     [resource] = resourceDescriptors
-        .filter(resource => resource.db_prefix === prefix)[0].default_url
+        .filter(resource => resource.db_prefix === prefix)[0].pages
+        .filter(page => page.name === subtype.name.toLowerCase())[0].url
         .split('[');
-  } else {
+  } else if(type){
     [resource] = resourceDescriptors
         .filter(resource => resource.db_prefix === prefix)[0].pages
         .filter(page => page.name === type.toLowerCase())[0].url
+        .split('[');
+  } else {
+    [resource] = resourceDescriptors
+        .filter(resource => resource.db_prefix === prefix)[0].default_url
         .split('[');
   }
   return resource + id;
