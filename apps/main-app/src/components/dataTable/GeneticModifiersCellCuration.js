5b1d13b7ef098bdaaa16cabec64bdae8e2aae24a
Andres Becerra <abecerra@ebi.ac.uk>
2023-06-23 13:50:02 +0100

    SCRUM-2833: make gene and allele links internal

--- apps/main-app/src/components/dataTable/GeneticModifiersCellCuration.js
+++ apps/main-app/src/components/dataTable/GeneticModifiersCellCuration.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { CollapsibleList } from '../collapsibleList';
 import ExternalLink from '../ExternalLink';
 import { getResourceUrl } from './getResourceUrl';
+import { Link } from 'react-router-dom';
 
 
-function GeneticModifier(modifier) {
-    let url = getResourceUrl(modifier.curie, modifier.type, modifier.subtype);
-    return <ExternalLink href={url}>{modifier.curie}</ExternalLink>;
+function GeneticModifierLink(modifier) {
+    switch(modifier?.type) {
+        case 'Gene':
+            return <Link to={`/gene/${modifier.curie}`} target='_blank'>{modifier.geneSymbol.displayText}</Link>;
+            break;
+        case 'Allele':
+            return <Link to={`/allele/${modifier.curie}`}  target='_blank'>{modifier.alleleSymbol.displayText}</Link>;
+            break;
+        case 'AffectedGenomicModel':
+            let url = getResourceUrl(modifier.curie, modifier.type, modifier.subtype);
+            return <ExternalLink href={url}>{modifier.curie}</ExternalLink>;
+            break;
+        default:
+            return <></>;
+            break;
+    }
+    return <></>;
 }
 
 function GeneticModifiersCellCuration ({relation, modifiers}) {
@@ -16,7 +31,7 @@ function GeneticModifiersCellCuration ({relation, modifiers}) {
                 <dt>{relation.name?.replace(/_/, ' ')}:</dt>
                 <dd>
                     <CollapsibleList collapsedSize={modifiers.length}>
-                        {modifiers.map(GeneticModifier)}
+                        {modifiers.map(GeneticModifierLink)}
                     </CollapsibleList>
                 </dd>
             </React.Fragment>
