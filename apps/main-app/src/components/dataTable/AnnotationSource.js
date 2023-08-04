cb7ec4af3756eca22aeb9adf1e51f9e670234643
Adam Gibson <agibson@mcw.edu>
2023-08-04 12:38:32 -0500

    SCRUM-2372 refactored utils methods

--- apps/main-app/src/components/dataTable/AnnotationSource.js
+++
@@ -1,9 +0,0 @@
-function AnnotationSource({dataProvider}) {
-    console.log(dataProvider);
-    if(dataProvider) {
-        return <>{dataProvider.abbreviation}</>;
-    }
-    return <></>;
-}
-
-export default AnnotationSource;
