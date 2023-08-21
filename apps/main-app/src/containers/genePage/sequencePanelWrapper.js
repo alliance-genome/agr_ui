efc033ed9482610882e1f99b39000b45856a491c
Scott Cain <scott+git@scottcain.net>
2023-08-21 12:52:35 -0700

    This is part of what's needed for https://agr-jira.atlassian.net/browse/KANBAN-366 still needed is dealing with Name/Symbol confusion for yeast

--- apps/main-app/src/containers/genePage/sequencePanelWrapper.js
+++ apps/main-app/src/containers/genePage/sequencePanelWrapper.js
@@ -18,11 +18,21 @@ class SequencePanel extends Component {
   }
 
   render() {
-    console.log(this.props.refseq);
+    var refseq = this.props.refseq;
+    if((this.props.species === 'NCBITaxon:559292' || this.props.species === 'NCBITaxon:8355') && !this.props.refseq.startsWith('chr') && !this.props.refseq.toLowerCase().startsWith('scaffold')){
+      refseq = 'chr' + refseq;
+    }
+    console.log(this.props);
+    console.log(this.props.gene);
     console.log(this.jBrowseurltemplate);
+    console.log(refseq);
+    console.log(this.props.start);
+    console.log(this.props.end);
+    console.log(this.jBrowsenclistbaseurl);
+    console.log(this.jBrowsefastaurl);
     return (
       <GenericGeneSeqPanel
-            refseq={this.props.refseq}
+            refseq={refseq}
             start={this.props.start}
             end={this.props.end}
             gene={this.props.gene}
