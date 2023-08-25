a1e3afdcb10a23624956e1df916baa4b08ca8855
Scott Cain <scott+git@scottcain.net>
2023-08-25 08:25:08 -0700

    removing logging messages

--- apps/main-app/src/containers/genePage/sequencePanelWrapper.js
+++ apps/main-app/src/containers/genePage/sequencePanelWrapper.js
@@ -22,14 +22,6 @@ class SequencePanel extends Component {
     if((this.props.species === 'NCBITaxon:559292' || this.props.species === 'NCBITaxon:8355') && !this.props.refseq.startsWith('chr') && !this.props.refseq.toLowerCase().startsWith('scaffold')){
       refseq = 'chr' + refseq;
     }
-    console.log(this.props);
-    console.log(this.props.gene);
-    console.log(this.jBrowseurltemplate);
-    console.log(refseq);
-    console.log(this.props.start);
-    console.log(this.props.end);
-    console.log(this.jBrowsenclistbaseurl);
-    console.log(this.jBrowsefastaurl);
     return (
       <GenericGeneSeqPanel
             refseq={refseq}
