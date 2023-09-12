062e79a0d55fb99908b8674019f566b020bc7c47
Olin Blodgett <nilo@nilosplace.net>
2023-09-12 11:33:19 -0400

    Renamed files

--- cdk/amplify-alb-stack.ts
+++ cdk/amplify-alb-stack.ts
@@ -6,15 +6,20 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as targets from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
 import * as cdk from 'aws-cdk-lib';
 
-export class StageALBStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+interface MultistackProps extends cdk.StackProps {
+  dnsName?: string,
+  targetInstanceId?: string
+}
+
+export class AmplifyALBStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: MultistackProps) {
     super(scope, id, props);
 
     const vpc = ec2.Vpc.fromLookup(this, 'Docker', { vpcName: 'Docker' });
     const sg1 = ec2.SecurityGroup.fromLookupById(this, 'default', 'sg-21ac675b');
     const sg2 = ec2.SecurityGroup.fromLookupById(this, 'HTTP/HTTPS', 'sg-0415cab61ab6b45c5');
     const cert = elbv2.ListenerCertificate.fromArn('arn:aws:acm:us-east-1:100225593120:certificate/047a56a2-09dd-4857-9f28-32d23650d4da');
-    const stage = new targets.InstanceIdTarget('i-0d7ea7b7cc11a2e8f')
+    const targetInstance = new targets.InstanceIdTarget(props.targetInstanceId)
     const public_zone = route53.HostedZone.fromHostedZoneAttributes(this, 'Public Zone', { zoneName: 'alliancegenome.org', hostedZoneId: 'Z3IZ3D6V94JEC2' });
     const private_zone = route53.HostedZone.fromHostedZoneAttributes(this, 'Private Zone', { zoneName: 'alliancegenome.org', hostedZoneId: 'Z007692222A6W93AZVSPD' });
 
@@ -28,13 +33,13 @@ export class StageALBStack extends cdk.Stack {
 
     new route53.CnameRecord(this, "Public DNS", {
       zone: public_zone,
-      recordName: "stage-alb",
+      recordName: props.dnsName + '-alb',
       domainName: alb.loadBalancerDnsName,
       ttl: cdk.Duration.minutes(5)
     });
     new route53.CnameRecord(this, "Private DNS", {
       zone: private_zone,
-      recordName: "stage-alb",
+      recordName: props.dnsName + '-alb',
       domainName: alb.loadBalancerDnsName,
       ttl: cdk.Duration.minutes(5)
     });
@@ -45,7 +50,7 @@ export class StageALBStack extends cdk.Stack {
       certificates: [cert]
     });
 
-    listener_https.addTargets("Backend Stage", {
+    listener_https.addTargets("Backend Server", {
       healthCheck: {
         path: '/robots.txt',
         unhealthyThresholdCount: 3,
@@ -53,19 +58,19 @@ export class StageALBStack extends cdk.Stack {
         interval: cdk.Duration.seconds(60),
       },
       port: 80,
-      targets: [stage],
+      targets: [targetInstance],
     });
 
 /*
-    listener_https.addAction("Redirect to Stage", {
+    listener_https.addAction("Redirect to Server", {
       priority: 10,
       conditions: [
-        elbv2.ListenerCondition.hostHeaders(['stage-alb.alliancegenome.org']),
+        elbv2.ListenerCondition.hostHeaders([props.dnsName + '-alb.alliancegenome.org']),
       ],
       action: elbv2.ListenerAction.redirect({
         protocol: 'HTTPS',
         port: '443',
-        host: 'stage.alliancegenome.org',
+        host: '#{host}',
         path: '/#{path}',
         query: '#{query}',
         permanent: true,
@@ -79,7 +84,7 @@ export class StageALBStack extends cdk.Stack {
       defaultAction: elbv2.ListenerAction.redirect({
         protocol: 'HTTPS',
         port: '443',
-        host: 'stage.alliancegenome.org',
+        host: '#{host}',
         path: '/#{path}',
         query: '#{query}',
         permanent: true,
