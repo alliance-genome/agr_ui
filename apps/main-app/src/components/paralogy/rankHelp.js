2f2cb966bebdd3972b5f8e23e6933a44b48d079d
Chris Tabone <christabone@gmail.com>
2023-09-26 09:27:50 -0300

    Update rankHelp.js

--- apps/main-app/src/components/paralogy/rankHelp.js
+++ apps/main-app/src/components/paralogy/rankHelp.js
@@ -6,11 +6,11 @@ export default function RankHelp() {
         Score = w₁ x (Alignment Length x Similarity %) + w₂ x (Alignment Length x Identity %) + w₃ x (Method Count) + w₄ x (Alignment Length).
       </p>
       <p>
-        Where the weights are as follows:
-        - w₁ = 1.000: Weight for the absolute number of similar amino acids.
-        - w₂ = 1.000: Weight for the absolute number of identical amino acids.
-        - w₃ = 1.500: Weight for the method count.
-        - w₄ = 1.500: Weight for the alignment length.
+        Where the weights are as follows:<br>
+        w₁ = 1.000: Weight for the absolute number of similar amino acids.<br>
+        w₂ = 1.000: Weight for the absolute number of identical amino acids.<br>
+        w₃ = 1.500: Weight for the method count.<br>
+        w₄ = 1.500: Weight for the alignment length.<br>
       </p>
       <p>
         Multiple entries can share the same rank.
