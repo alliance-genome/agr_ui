e1984f38fa6cc773d6aef0a123613de2375cd7bb
Olin Blodgett <nilo@nilosplace.net>
2023-11-02 15:45:58 -0400

    Removed GA - UI

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,35 +1,22 @@
-import ReactGA from 'react-ga';
 import { default as ReactGA4 } from 'react-ga4';
 
 import { GA_PROPERTY_ID_UA, GA_PROPERTY_ID_GA4, GA_EVENT_CATEGORY, GA_EVENT_ACTION } from '../constants';
 
 export const initialize = () => {
   if (process.env.NODE_ENV === 'production') {
-    ReactGA.initialize(GA_PROPERTY_ID_UA);
     ReactGA4.initialize(GA_PROPERTY_ID_GA4);
   }
 };
 
 export const autocompleteSearchEvent = (query) => {
-  ReactGA.event({
-    category: GA_EVENT_CATEGORY.AUTOCOMPLETE,
-    action: GA_EVENT_ACTION.GO_TO_SEARCH_RESULTS,
-    label: query
-  });
   ReactGA4.event({
     category: GA_EVENT_CATEGORY.AUTOCOMPLETE,
     action: GA_EVENT_ACTION.GO_TO_SEARCH_RESULTS,
     label: query
   });
-
 };
 
 export const autocompleteGoToPageEvent = (id) => {
-  ReactGA.event({
-    category: GA_EVENT_CATEGORY.AUTOCOMPLETE,
-    action: GA_EVENT_ACTION.GO_TO_PAGE,
-    label: id
-  });
   ReactGA4.event({
     category: GA_EVENT_CATEGORY.AUTOCOMPLETE,
     action: GA_EVENT_ACTION.GO_TO_PAGE,
@@ -39,16 +26,10 @@ export const autocompleteGoToPageEvent = (id) => {
 
 export const logPageView = (location) => {
   const page = location.pathname + location.search;
-  ReactGA.send({ hitType: "pageview", page: page });
   ReactGA4.send({ hitType: "pageview", page: page });
 };
 
 export const logTablePageEvent = (page) => {
-  ReactGA.event({
-    category: GA_EVENT_CATEGORY.TABLE,
-    action: GA_EVENT_ACTION.GO_TO_PAGE,
-    label: page
-  });
   ReactGA4.event({
     category: GA_EVENT_CATEGORY.TABLE,
     action: GA_EVENT_ACTION.GO_TO_PAGE,
@@ -57,11 +38,6 @@ export const logTablePageEvent = (page) => {
 };
 
 export const logTableSizeEvent = (size) => {
-  ReactGA.event({
-    category: GA_EVENT_CATEGORY.TABLE,
-    action: GA_EVENT_ACTION.SET_PAGE_SIZE,
-    label: size.toString()
-  });
   ReactGA4.event({
     category: GA_EVENT_CATEGORY.TABLE,
     action: GA_EVENT_ACTION.SET_PAGE_SIZE,
