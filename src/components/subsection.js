d2972e5b7d28c7553a20a5985bbe9a46d1b3fa0d
Adam Gibson <agibson@mcw.edu>
2023-12-22 13:23:04 -0600

    warnings quick fix

--- src/components/subsection.js
+++ src/components/subsection.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
--- src/containers/wordpress/wordpressPost.js
+++ src/containers/wordpress/wordpressPost.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './style.module.scss';
