7758dfa627e4d65d1eeab1b053666bfa55c17920
Adam Gibson <agibson@mcw.edu>
2023-12-22 13:23:04 -0600

    warnings quick fix

--- src/containers/search/filterSelector/singleFilterSelector.js
+++ src/containers/search/filterSelector/singleFilterSelector.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
