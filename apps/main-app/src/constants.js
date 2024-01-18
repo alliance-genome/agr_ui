104ab156f077de37f144176271d1a9a1a90f2e57
Howie Motenko <howie.motenko@jax.org>
2024-01-18 10:20:30 -0500

    Revert "KANBAN-457: Add 'Data Preservation to About menu"
    This reverts commit 0ed5b25889267f319d0848721e0a007acd626eca.

--- apps/main-app/src/constants.js
+++ apps/main-app/src/constants.js
@@ -4,8 +4,7 @@ export const LARGE_COL_CLASS = 'col-md-8 col-12';
 export const SMALL_COL_CLASS = 'col-md-4 col-12';
 
 /* Wordpress REST API connection setting */
-export const WORDPRESS_REST_API_BASE =
-  'https://public-api.wordpress.com/wp/v2/sites/alliancegenome.wordpress.com';
+export const WORDPRESS_REST_API_BASE = 'https://public-api.wordpress.com/wp/v2/sites/alliancegenome.wordpress.com';
 export const WORDPRESS_PAGE_BASE_URL = WORDPRESS_REST_API_BASE + '/pages?slug=';
 export const WORDPRESS_POST_BASE_URL = WORDPRESS_REST_API_BASE + '/posts';
 export const WORDPRESS_POST_URL = WORDPRESS_POST_BASE_URL + '?slug=';
@@ -15,7 +14,7 @@ export const WARNING_BANNER_SLUG = 'warning-banner';
 export const CATEGORIES = [
   {
     name: 'all',
-    displayName: 'All',
+    displayName: 'All'
   },
   {
     name: 'gene',
@@ -25,7 +24,7 @@ export const CATEGORIES = [
   {
     name: 'go',
     displayName: 'Gene Ontology',
-    displayFields: ['primaryKey', 'collapsible_synonyms', 'branch'],
+    displayFields: ['primaryKey', 'collapsible_synonyms', 'branch']
   },
   {
     name: 'disease',
@@ -35,25 +34,17 @@ export const CATEGORIES = [
   {
     name: 'allele',
     displayName: 'Allele/Variant',
-    displayFields: [
-      'primaryKey',
-      'genes',
-      'synonyms',
-      'variantType',
-      'molecularConsequence',
-      'diseases',
-      'variantName',
-    ],
+    displayFields: ['primaryKey','genes', 'synonyms','variantType','molecularConsequence', 'diseases', 'variantName'],
   },
   {
     name: 'model',
     displayName: 'Model',
-    displayFields: ['primaryKey', 'synonyms'],
+    displayFields: ['primaryKey','synonyms'],
   },
   {
     name: 'dataset',
     displayName: 'HTP Dataset Index',
-    displayFields: ['dataProviderNote', 'idCollection', 'tags', 'summary'],
+    displayFields: ['dataProviderNote', 'idCollection', 'tags', 'summary']
   },
 ];
 
@@ -66,11 +57,14 @@ export const NON_HIGHLIGHTED_FIELDS = [
   'primaryKey',
   'orthologs',
   'homologs.symbol',
-  'homologs.panther_family',
+  'homologs.panther_family'
 ];
 
 //show these fields in both the highlight section and their normal spot
-export const DUPLICATE_HIGHLIGHTED_FIELDS = ['crossReferences', 'synonyms'];
+export const DUPLICATE_HIGHLIGHTED_FIELDS = [
+  'crossReferences',
+  'synonyms'
+];
 
 export const NAV_MENU = [
   {
@@ -106,8 +100,8 @@ export const NAV_MENU = [
         label: 'JBrowse',
         route: '/jbrowse/?data=data%2FHomo%20sapiens',
         external: true,
-      },
-    ],
+      }
+    ]
   },
   {
     label: 'Members',
@@ -115,57 +109,57 @@ export const NAV_MENU = [
       {
         label: 'FlyBase',
         shortLabel: 'FlyBase',
-        route: '/members/flybase',
+        route: '/members/flybase'
       },
       {
         label: 'Mouse Genome Database',
         shortLabel: 'MGD',
-        route: '/members/mgd',
+        route: '/members/mgd'
       },
       {
         label: 'Rat Genome Database',
         shortLabel: 'RGD',
-        route: '/members/rgd',
+        route: '/members/rgd'
       },
       {
         label: 'Saccharomyces Genome Database',
         shortLabel: 'SGD',
-        route: '/members/sgd',
+        route: '/members/sgd'
       },
       {
         label: 'WormBase',
         shortLabel: 'WormBase',
-        route: '/members/wormbase',
+        route: '/members/wormbase'
       },
       {
         label: 'Xenbase',
         shortLabel: 'Xenbase',
-        route: '/members/xenbase',
+        route: '/members/xenbase'
       },
       {
         label: 'Zebrafish Information Network',
         shortLabel: 'ZFIN',
-        route: '/members/zfin',
+        route: '/members/zfin'
       },
       {
         label: 'Gene Ontology Consortium',
         shortLabel: 'GOC',
-        route: '/members/goc',
-      },
-    ],
+        route: '/members/goc'
+      }
+    ]
   },
   {
     label: 'News',
     sub: [
       {
         label: 'News and Events',
-        route: '/news',
+        route: '/news'
       },
       {
         label: 'Release Notes',
-        route: '/release-notes',
-      },
-    ],
+        route: '/release-notes'
+      }
+    ]
   },
   {
     label: 'About',
@@ -176,21 +170,21 @@ export const NAV_MENU = [
       },
       {
         label: 'Funding',
-        route: '/funding',
+        route: '/funding'
+      },
+      {
+        label: 'Organization and Governance',
+        route: '/organization-and-governance'
+      },
+      {
+        label: 'Privacy, Warranty, and Licensing',
+        route: '/privacy-warranty-licensing'
       },
       {
         label: 'Publications',
         route: '/publications',
-      },
-      {
-        label: 'Organization and Governance',
-        route: '/organization-and-governance',
-      },
-      {
-        label: 'Privacy, Warranty, Licensing, and Data Preservation Commitment',
-        route: '/privacy-warranty-licensing',
-      },
-    ],
+      }
+    ]
   },
   {
     label: 'Working Groups',
@@ -201,21 +195,21 @@ export const NAV_MENU = [
     sub: [
       {
         label: 'FAQ / Known Issues',
-        route: '/faq',
+        route: '/faq'
       },
       {
         label: 'Glossary',
-        route: '/glossary',
+        route: '/glossary'
       },
       {
         label: 'Tutorials',
-        route: '/tutorials',
+        route: '/tutorials'
       },
       {
         label: 'User Documentation',
-        route: '/help',
+        route: '/help'
       },
-    ],
+    ]
   },
   {
     label: 'Community',
@@ -229,8 +223,8 @@ export const NAV_MENU = [
         label: 'Twitter',
         route: 'https://twitter.com/alliancegenome',
         external: true,
-      },
-    ],
+      }
+    ]
   },
   {
     label: 'Contact Us',
@@ -239,7 +233,7 @@ export const NAV_MENU = [
   {
     label: 'Cite Us',
     route: '/cite-us',
-  },
+  }
 ];
 
 export const DEFAULT_TABLE_STATE = {
@@ -257,11 +251,9 @@ export const SPECIES = [
     apolloName: 'human',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Homo sapiens',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/human/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/human/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000001405.40_GRCh38.p14_genomic.fna.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000001405.40_GRCh38.p14_genomic.fna.gz',
     vertebrate: true,
     enableSingleCellExpressionAtlasLink: true,
     enableOrthologComparison: true,
@@ -273,11 +265,9 @@ export const SPECIES = [
     apolloName: 'mouse',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Mus musculus',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/MGI/mouse/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/MGI/mouse/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000001635.27_GRCm39_genomic.fna.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000001635.27_GRCm39_genomic.fna.gz',
     vertebrate: true,
     enableSingleCellExpressionAtlasLink: true,
     enableOrthologComparison: true,
@@ -289,11 +279,9 @@ export const SPECIES = [
     apolloName: 'rat',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Rattus norvegicus',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/RGD/rat/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/RGD/rat/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_015227675.2_mRatBN7.2_genomic.fna.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_015227675.2_mRatBN7.2_genomic.fna.gz',
     vertebrate: true,
     enableSingleCellExpressionAtlasLink: true,
     enableOrthologComparison: true,
@@ -305,11 +293,9 @@ export const SPECIES = [
     apolloName: 'x_laevis',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Xenopus laevis',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/XenBase/x_laevis/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/XenBase/x_laevis/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/XENLA_9.2_genome.fa.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/XENLA_9.2_genome.fa.gz',
     vertebrate: true,
     enableSingleCellExpressionAtlasLink: false,
     enableOrthologComparison: true,
@@ -321,11 +307,9 @@ export const SPECIES = [
     apolloName: 'x_tropicalis',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Xenopus tropicalis',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/XenBase/x_tropicalis/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/XenBase/x_tropicalis/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/XENTR_9.1_genome.fa.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/XENTR_9.1_genome.fa.gz',
     vertebrate: true,
     enableSingleCellExpressionAtlasLink: false,
     enableOrthologComparison: true,
@@ -337,11 +321,9 @@ export const SPECIES = [
     apolloName: 'zebrafish',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Danio rerio',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/zfin/zebrafish-11/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/zfin/zebrafish-11/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000002035.6_GRCz11_genomic.fna.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000002035.6_GRCz11_genomic.fna.gz',
     vertebrate: true,
     enableSingleCellExpressionAtlasLink: true,
     enableOrthologComparison: true,
@@ -353,11 +335,9 @@ export const SPECIES = [
     apolloName: 'fly',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Drosophila melanogaster',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/FlyBase/fruitfly/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/FlyBase/fruitfly/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz',
     vertebrate: false,
     enableSingleCellExpressionAtlasLink: true,
     enableOrthologComparison: true,
@@ -369,11 +349,9 @@ export const SPECIES = [
     apolloName: 'worm',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Caenorhabditis elegans',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/WormBase/c_elegans_PRJNA13758/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/WormBase/c_elegans_PRJNA13758/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000002985.6_WBcel235_genomic.fna.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000002985.6_WBcel235_genomic.fna.gz',
     vertebrate: false,
     enableSingleCellExpressionAtlasLink: true,
     enableOrthologComparison: true,
@@ -385,11 +363,9 @@ export const SPECIES = [
     apolloName: 'yeast',
     apolloTrack: '/All%20Genes/',
     jBrowseName: 'Saccharomyces cerevisiae',
-    jBrowsenclistbaseurl:
-      'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/SGD/yeast/',
+    jBrowsenclistbaseurl: 'https://s3.amazonaws.com/agrjbrowse/docker/6.0.0/SGD/yeast/',
     jBrowseurltemplate: 'tracks/All_Genes/{refseq}/trackData.jsonz',
-    jBrowsefastaurl:
-      'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000146045.2_R64_genomic.fna.gz',
+    jBrowsefastaurl: 'https://s3.amazonaws.com/agrjbrowse/fasta/GCF_000146045.2_R64_genomic.fna.gz',
     vertebrate: false,
     enableSingleCellExpressionAtlasLink: true,
     enableOrthologComparison: true,
@@ -406,12 +382,12 @@ export const SPECIES = [
     enableSingleCellExpressionAtlasLink: false,
     enableOrthologComparison: false,
     geneSynopsisProvider: 'UniProt',
-  },
+  }
 ];
 
-export const TAXON_ORDER = SPECIES.map((s) => s.taxonId);
+export const TAXON_ORDER = SPECIES.map(s => s.taxonId);
 
-export const SPECIES_NAME_ORDER = SPECIES.map((s) => s.fullName);
+export const SPECIES_NAME_ORDER = SPECIES.map(s => s.fullName);
 
 export const GA_PROPERTY_ID_UA = 'UA-98765810-1';
 export const GA_PROPERTY_ID_GA4 = 'G-H3F65KGJDR';
@@ -424,5 +400,6 @@ export const GA_EVENT_CATEGORY = {
 export const GA_EVENT_ACTION = {
   GO_TO_PAGE: 'Go to page',
   GO_TO_SEARCH_RESULTS: 'Go to search results',
-  SET_PAGE_SIZE: 'Set page size',
+  SET_PAGE_SIZE: 'Set page size'
 };
+
