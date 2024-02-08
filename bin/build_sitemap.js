ae4be19d9b10bd6cc728ad7414923cc2886f5482
Howie Motenko <howie.motenko@jax.org>
2024-02-08 14:52:01 -0500

    KANBAN-437: Add 'Submit Data' page

--- bin/build_sitemap.js
+++ bin/build_sitemap.js
@@ -1,10 +1,9 @@
-
-var sm = require('sitemap'), fs = require('fs'), path = require('path');
+var sm = require('sitemap'),
+  fs = require('fs'),
+  path = require('path');
 
 var sitemapindex = sm.buildSitemapIndex({
-  urls: [
-    'https://www.alliancegenome.org/main-sitemap.xml'
-  ]
+  urls: ['https://www.alliancegenome.org/main-sitemap.xml'],
 });
 
 fs.writeFileSync('dist/apps/main-app/sitemap.xml', sitemapindex.toString());
@@ -12,20 +11,86 @@ fs.writeFileSync('dist/apps/main-app/sitemap.xml', sitemapindex.toString());
 var sitemap = sm.createSitemap({
   hostname: 'https://www.alliancegenome.org',
   urls: [
-    { url: '/' ,             changefreq: 'monthly', priority: 0.1, lastmodrealtime: true },
-    { url: '/about-us',         changefreq: 'monthly', priority: 0.8, lastmodrealtime: true },
-    { url: '/contact-us',       changefreq: 'monthly', priority: 0.8, lastmodrealtime: true },
-    { url: '/cite-us',       changefreq: 'monthly', priority: 0.8, lastmodrealtime: true },
-    { url: '/frequently-asked-questions',           changefreq: 'monthly', priority: 0.8, lastmodrealtime: true },
-    { url: '/release-notes',    changefreq: 'monthly', priority: 0.8, lastmodrealtime: true },
-    { url: '/funding',       changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
-    { url: '/organization-and-governance',  changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
-    { url: '/privacy-warranty-licensing',  changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
-    { url: '/publications',  changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
-    { url: '/genome-features',      changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
-    { url: '/administrative-supplement',    changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
-    { url: '/phenotypes-and-disease-models',    changefreq: 'monthly', priority: 0.7, lastmodrealtime: true }
-  ]
+    { url: '/', changefreq: 'monthly', priority: 0.1, lastmodrealtime: true },
+    {
+      url: '/about-us',
+      changefreq: 'monthly',
+      priority: 0.8,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/contact-us',
+      changefreq: 'monthly',
+      priority: 0.8,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/cite-us',
+      changefreq: 'monthly',
+      priority: 0.8,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/frequently-asked-questions',
+      changefreq: 'monthly',
+      priority: 0.8,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/release-notes',
+      changefreq: 'monthly',
+      priority: 0.8,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/funding',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/organization-and-governance',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/privacy-warranty-licensing',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/publications',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/genome-features',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/administrative-supplement',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/phenotypes-and-disease-models',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+    {
+      url: '/submit-data',
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmodrealtime: true,
+    },
+  ],
 });
 
 fs.writeFileSync('dist/apps/main-app/main-sitemap.xml', sitemap.toString());
