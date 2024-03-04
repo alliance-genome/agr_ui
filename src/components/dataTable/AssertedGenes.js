38655c1e8ce32ada9c915370c9c403b07a3a975e
Adam Gibson <agibson@mcw.edu>
2024-03-04 15:46:33 -0600

    SCRUM-3647 make assertedGenes link internal

--- src/components/dataTable/AssertedGenes.js
+++ src/components/dataTable/AssertedGenes.js
@@ -1,11 +1,11 @@
 import CollapsibleList from '../collapsibleList/collapsibleList';
-import ExternalLink from '../ExternalLink';
+import { Link } from 'react-router-dom';
 
 
 function makeAssertedGeneLink(curie, geneSymbol) {
     if(curie) {
       const symbol = <span dangerouslySetInnerHTML={{__html: geneSymbol}}/>;
-      return <ExternalLink href={`https://www.alliancegenome.org/gene/${curie}`}>{symbol}</ExternalLink>;
+      return <Link to={`/gene/${curie}`}>{symbol}</Link>;
     }
     return null;
 }
@@ -15,7 +15,7 @@ function AssertedGenes({assertedGenes, mainRowCurie}) {
   if(assertedGenes && assertedGenes.length > 1) {
     return (
         <CollapsibleList collapsedSize={assertedGenes.length}>
-        {filteredAssertedGenes.map(gene => makeAssertedGeneLink(gene.curie, gene.geneSymbol.displayText))}
+          {filteredAssertedGenes.map(gene => makeAssertedGeneLink(gene.curie, gene.geneSymbol.displayText))}
         </CollapsibleList>
     );
   }
