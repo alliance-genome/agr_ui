e43fd77099c9ce046436cdf6064015253cb55778
Adam Gibson <agibson@mcw.edu>
2024-03-15 12:02:14 -0500

    remove getRefString

--- src/components/dataTable/utils.js
+++ src/components/dataTable/utils.js
@@ -22,54 +22,6 @@ export const getIsViaOrthology = (annotation) => {
   return annotation.generatedRelationString.includes("orthology");
 };
 
-export function getRefString(referenceItem) {
-  if (!referenceItem)
-    return;
-
-  if (!referenceItem.cross_references && !referenceItem.crossReferences){
-    return referenceItem.curie
-  }
-  let xrefCuries = referenceItem.crossReferences.map((crossReference) => crossReference.referencedCurie);
-  let primaryXrefCurie = '';
-
-  if (indexWithPrefix(xrefCuries, 'PMID:') > -1) {
-    [primaryXrefCurie] = xrefCuries.splice(indexWithPrefix(xrefCuries, 'PMID:'), 1);
-  } else if (indexWithPrefix(xrefCuries, 'FB:') > -1) {
-    [primaryXrefCurie] = xrefCuries.splice(indexWithPrefix(xrefCuries, 'FB:'), 1);
-  } else if (indexWithPrefix(xrefCuries, 'MGI:') > -1) {
-    [primaryXrefCurie] = xrefCuries.splice(indexWithPrefix(xrefCuries, 'MGI:'), 1);
-  } else if (indexWithPrefix(xrefCuries, 'RGD:') > -1) {
-    [primaryXrefCurie] = xrefCuries.splice(indexWithPrefix(xrefCuries, 'RGD:'), 1);
-  } else if (indexWithPrefix(xrefCuries, 'SGD:') > -1) {
-    [primaryXrefCurie] = xrefCuries.splice(indexWithPrefix(xrefCuries, 'SGD:'), 1);
-  } else if (indexWithPrefix(xrefCuries, 'WB:') > -1) {
-    [primaryXrefCurie] = xrefCuries.splice(indexWithPrefix(xrefCuries, 'WB:'), 1);
-  } else if (indexWithPrefix(xrefCuries, 'ZFIN:') > -1) {
-    [primaryXrefCurie] = xrefCuries.splice(indexWithPrefix(xrefCuries, 'ZFIN:'), 1);
-  } else {
-    [primaryXrefCurie] = xrefCuries.splice(0, 1);
-  }
-
-  return primaryXrefCurie;
-}
-
-
-function indexWithPrefix(array, prefix) {
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].startsWith(prefix)) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-export const getSingleReferenceCurieAndUrl = (reference) => {
-  const curie = getRefString(reference);
-  const url = getResourceUrl(curie);
-  return {curie, url};
-}
-
 export const getSingleReferenceUrl = (pubModId) => {
   const url = getResourceUrl(pubModId);
   return {pubModId, url};
