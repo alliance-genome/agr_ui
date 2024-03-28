315bbd4ea461b09532d3a13bf6ac936fb62bc8be
Howie Motenko <howie.motenko@jax.org>
2024-03-28 15:19:48 -0400

    Add Textpresso landing page

--- bin/build_sitemap.js
+++ bin/build_sitemap.js
@@ -25,7 +25,8 @@ var sitemap = sm.createSitemap({
     { url: '/genome-features',      changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
     { url: '/administrative-supplement',    changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
     { url: '/phenotypes-and-disease-models',    changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
-    { url: '/submit-data',    changefreq: 'monthly', priority: 0.7, lastmodrealtime: true }
+    { url: '/submit-data',    changefreq: 'monthly', priority: 0.7, lastmodrealtime: true },
+    { url: '/textpresso',    changefreq: 'monthly', priority: 0.7, lastmodrealtime: true }
   ]
 });
 
