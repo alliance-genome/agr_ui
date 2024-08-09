9839231cb2d600c5a0fe3836a69821d04700253c
Howie Motenko <howie.motenko@jax.org>
2024-08-09 13:40:39 -0400

    KANBAN-531: Add community resource links for AD portal

--- src/containers/alzheimersPage/ResourcesSection.js
+++ src/containers/alzheimersPage/ResourcesSection.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import style from './style.module.scss';
+import ExternalLink from '../../components/ExternalLink';
+
 
 const ResourcesSection = () => {
+  const resourceLinks = [
+    {title: "Alzhemiers.gov", url: 'https://www.alzheimers.gov/'},
+    {title: "Alzhemier's Association", url: 'https://www.alz.org/'},
+    {title: "Alzhemier's Foundation of America", url: 'https://alzfdn.org/'},
+    {title: "National Institute on Aging", url: 'https://www.nia.nih.gov/'},
+  ];
+
   return (
     <section className={style.section}>
       <div className={style.contentContainer}>
         <div className='row'>
           <div className='col-lg-12'>
-            <p>Community Resoures</p>
+            <h2>Community Resoures</h2>
+              <div>
+                {resourceLinks.map((resourceLink) => {
+                  return (
+                    <div>
+                      <ExternalLink href={resourceLink.url}>{resourceLink.title}</ExternalLink>
+                    </div>
+                  )
+                })}
+              </div>
           </div>
         </div>
       </div>
