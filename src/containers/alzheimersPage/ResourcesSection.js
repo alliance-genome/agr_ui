841d8c79211aa8aef73963c596c8dc066c71a3b3
Howie Motenko <howie.motenko@jax.org>
2025-03-10 15:54:09 -0400

    Improve const ResourceSection

--- src/containers/alzheimersPage/ResourcesSection.js
+++ src/containers/alzheimersPage/ResourcesSection.js
@@ -2,17 +2,16 @@ import React from 'react';
 import style from './style.module.scss';
 import ExternalLink from '../../components/ExternalLink';
 
+const resourceLinks = [
+  {title: "Alzheimers.gov", url: 'https://www.alzheimers.gov/'},
+  {title: "Alzheimer's Association", url: 'https://www.alz.org/'},
+  {title: "Alzheimer's Disease Clinical Trials", url: 'https://www.clinicaltrials.gov/search?cond=Alzheimer%20Disease'},
+  {title: "Alzheimer's Foundation of America", url: 'https://alzfdn.org/'},
+  {title: "National Institute on Aging", url: 'https://www.nia.nih.gov/'},
+  {title: "OMIM", url: 'https://omim.org/entry/104300'},
+];
 
 const ResourcesSection = () => {
-  const resourceLinks = [
-    {title: "Alzheimers.gov", url: 'https://www.alzheimers.gov/'},
-    {title: "Alzheimer's Association", url: 'https://www.alz.org/'},
-    {title: "Alzheimer's Disease Clinical Trials", url: 'https://www.clinicaltrials.gov/search?cond=Alzheimer%20Disease'},
-    {title: "Alzheimer's Foundation of America", url: 'https://alzfdn.org/'},
-    {title: "National Institute on Aging", url: 'https://www.nia.nih.gov/'},
-    {title: "OMIM", url: 'https://omim.org/entry/104300'},
-  ];
-
   return (
     <section className={style.section}>
       <div className={style.contentContainer}>
