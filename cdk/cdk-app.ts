e9a13b2b4c88914beb5695e5e5c72827be73f6a0
Christopher Tabone <christabone@gmail.com>
2025-04-23 14:35:39 -0300

    Removed stage stack.

--- cdk/cdk-app.ts
+++ cdk/cdk-app.ts
@@ -7,16 +7,6 @@ import { AmplifyProductionStack } from './amplify-production-stack';
 
 const app = new cdk.App();
 
-new AmplifyALBStack(app, 'stage-alb-stack', {
-  stackName: 'stage-alb-stack',
-  dnsName: 'stage',
-  targetInstanceId: 'i-0498ae078643b61eb',
-  env: {
-    region: process.env.CDK_DEFAULT_REGION,
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-  },
-});
-
 new AmplifyALBStack(app, 'test-alb-stack', {
   stackName: 'test-alb-stack',
   dnsName: 'test',
