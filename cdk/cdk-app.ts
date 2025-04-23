5e18ccb3b90e3d915c1eb161661191155532a0d4
Chris Tabone <christabone@gmail.com>
2025-04-23 14:23:03 -0300

    Update cdk-app.ts
    Revert stage id.

--- cdk/cdk-app.ts
+++ cdk/cdk-app.ts
@@ -10,7 +10,7 @@ const app = new cdk.App();
 new AmplifyALBStack(app, 'stage-alb-stack', {
   stackName: 'stage-alb-stack',
   dnsName: 'stage',
-  targetInstanceId: "i-0ebadbf0e1a9240ba",
+  targetInstanceId: "i-0498ae078643b61eb",
   env: {
     region: process.env.CDK_DEFAULT_REGION,
     account: process.env.CDK_DEFAULT_ACCOUNT,
