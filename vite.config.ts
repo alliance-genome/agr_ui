ab85b712833ae3a9287deeed3e0c91670ba6cb2f
Seth Campbell <sercampb@iu.edu>
2025-05-08 10:45:21 -0400

    KANBAN-707: adds js support

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
+  plugins: [react()],
+})
