4501eb834e455eb2fe83ea8219db623494bf7950
Seth Campbell <sercampb@iu.edu>
2025-05-08 10:04:13 -0400

    KANBAN-707: purge files

--- src/components/dataTable/AnnotationType.js
+++
@@ -1,9 +0,0 @@
-
-function AnnotationType({annotationType}) {
-    if (annotationType) {
-        return <>{annotationType.name.replaceAll('_', ' ')}</>;
-    }
-    return <></>;
-}
-
-export default AnnotationType;
