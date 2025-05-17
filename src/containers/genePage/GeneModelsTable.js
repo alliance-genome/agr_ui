cbd2cf08db2c69591e0ab66f2a147c1dc71ea915
Christian Pich <cmpich@zfin.org>
2025-05-17 18:16:04 +0200

    SCRUM-4073 use models documents from ES to serve Model table @gene page

--- src/containers/genePage/GeneModelsTable.js
+++ src/containers/genePage/GeneModelsTable.js
@@ -8,6 +8,9 @@ import DiseaseLink from '../../components/disease/DiseaseLink';
 import useDataTableQuery from '../../hooks/useDataTableQuery';
 import AssociationType from '../../components/AssociationType';
 import ExperimentalConditionCell from '../../components/dataTable/ExperimentalConditionCell';
+import ExperimentalConditionCellCuration from "../../components/dataTable/ExperimentalConditionCellCuration";
+import DiseaseLinkCuration from "../../components/disease/DiseaseLinkCuration";
+import ModelCellCuration from "../../components/dataTable/ModelCellCuration";
 
 const GeneModelsTable = ({id}) => {
   const tableQuery = useDataTableQuery(`/api/gene/${id}/models`);
@@ -18,40 +21,38 @@ const GeneModelsTable = ({id}) => {
 
   const columns = [
     {
-      dataField: 'name',
+      dataField: 'model',
       text: 'Model name',
       helpPopupProps: {
         id: 'gene-page--model-table--model-name-help',
         children: <span>The genotype--the combination of alleles and strain background--and for fish may include Sequence Targeting Reagents.</span>,
       },
-      formatter: (name, row) => (
-        <ExternalLink href={row.url}>
-          <span dangerouslySetInnerHTML={{__html: name}} />
-        </ExternalLink>
-      ),
+      formatter: model => <ModelCellCuration model={model}/>,
       filterable: true,
       filterName: 'modelName',
       headerStyle: {width: '220px'},
     },
     {
-      dataField: 'conditions',
+      dataField: 'conditionRelations',
       text: 'Experimental condition',
-      formatter: conditions => <ExperimentalConditionCell conditions={conditions} />,
+      formatter: conditions => <ExperimentalConditionCellCuration conditions={conditions} />,
       headerStyle: {width: '220px'},
+      filterName: "experimentalCondition",
+      filterable: true,
     },
     {
-      dataField: 'diseaseModels',
+      dataField: 'diseaseTerms',
       text: 'Associated Human Diseases',
       helpPopupProps: {
         id: 'gene-page--model-table--associated-human-diseases-help',
         children: <span>The Disease Ontology (DO) term for which the organism is a model. The DO is a hierarchical, standardized ontology that integrates vocabularies from MeSH, ICD, NCI’s thesaurus, SNOMED, UMLS, Orphanet, EFO and OMIM. Its hierarchical structure permits a range of detail from high-level, broadly descriptive terms to very low-level, highly specific terms.</span>,
       },
-      formatter: diseaseModels => diseaseModels && (
-        <CollapsibleList collapsedSize={diseaseModels.length}>
-          {diseaseModels.map(diseaseModel => (
-            <div key={diseaseModel.associationType + diseaseModel.disease.id}>
+      formatter: diseaseTerms => diseaseTerms && (
+        <CollapsibleList collapsedSize={diseaseTerms.length}>
+          {diseaseTerms.map(diseaseModel => (
+            <div key={diseaseModel.associationType + diseaseModel.curie}>
               <AssociationType type={diseaseModel.associationType} showOnlyNot />{' '}
-              <DiseaseLink disease={diseaseModel.disease} />
+              <DiseaseLinkCuration disease={diseaseModel} />
             </div>
           ))}
         </CollapsibleList>
@@ -61,11 +62,11 @@ const GeneModelsTable = ({id}) => {
       headerStyle: {width: '230px'},
     },
     {
-      dataField: 'phenotypes',
+      dataField: 'associatedPhenotype',
       text: 'Associated Phenotypes',
-      formatter: phenotypes => phenotypes && (
+      formatter: associatedPhenotype => associatedPhenotype && (
         <CollapsibleList collapsedSize={2} showBullets>
-          {phenotypes.map(phenotype => (
+          {associatedPhenotype.map(phenotype => (
             <span dangerouslySetInnerHTML={{__html: phenotype}} key={phenotype} />
           ))}
         </CollapsibleList>
@@ -85,10 +86,11 @@ const GeneModelsTable = ({id}) => {
       headerStyle: {width: '220px'},
     },
     {
-      dataField: 'source',
+      dataField: 'dataProvider',
       text: 'Source',
-      formatter: source => source && source.name,
+      formatter: dataProvider => dataProvider ,
       headerStyle: {width: '100px'},
+      filterName: 'source',
       filterable: true,
     },
   ];
