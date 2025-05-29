049c8137eab9bccd1c2e8e5e47827a74b98d0ed8
Seth Campbell <sercampb@iu.edu>
2025-05-29 12:36:24 -0400

    removes original_src

--- original_src/components/GeneSymbolCuration.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const GeneSymbolCuration = ({gene}) => {
-  return <span dangerouslySetInnerHTML={{__html: gene.geneSymbol.displayText}} />;
-};
-
-
-export default GeneSymbolCuration;
--- original_src/components/attribute/index.js
+++
@@ -1,9 +0,0 @@
-import AttributeList from './AttributeList';
-import AttributeLabel from './AttributeLabel';
-import AttributeValue from './AttributeValue';
-
-export {
-  AttributeList,
-  AttributeLabel,
-  AttributeValue,
-};
