6a095412f4ea7b040cc58417c71e47c34c571d84
Joe Wang <dongzhuo.wang@ucalgary.ca>
2025-06-25 12:07:50 -0600

    KANBAN-428 Display model counts, KANBAN-430 Display allele counts

--- src/containers/alzheimersPage/DiseasePortalSection.js
+++ src/containers/alzheimersPage/DiseasePortalSection.js
@@ -3,15 +3,15 @@ import style from './style.module.scss';
 // import {SearchBarComponent} from '../layout/searchBar';
 // import SearchExample from './SearchExample';
 import EntityButton from './EntityButton';
-import {useEntityButtonCounts} from "./useEntityButtonCounts";
-// import LoadingSpinner from "../../components/loadingSpinner";
+import {useEntityButtonCount, useEntityButtonCounts} from "./useEntityButtonCounts";
 // import {Link} from 'react-router-dom';
 
 const DiseasePortalSection = () => {
 
-  const url = '/api/disease/DOID:10652/genes';
-  // let number = useFetchNumber(`${url}?limit=1`);
-  const { counts, loading, error } = useEntityButtonCounts(url, 5000);
+  const url = '/api/disease/DOID:10652/';
+  const geneCount = useEntityButtonCounts(url+'genes', 5000);
+  const modelCount = useEntityButtonCounts(url+'models', 5000);
+  const alleleCount = useEntityButtonCounts(url+'alleles', 5000);
 
   return (
     <section className={`${style.section} ${style.searchBackground} shadow`}>
@@ -35,19 +35,15 @@ const DiseasePortalSection = () => {
             to='/disease/DOID:10652#associated-models'
             tooltip='View all associated models'
           >
-            213<br />Models
+          <div>{modelCount ?? '207'}</div>
+            Models
           </EntityButton>
           <EntityButton
             id='entity-genes'
             to='/disease/DOID:10652#associated-genes'
             tooltip='View all associated genes'
           >
-            {loading
-              ? <div>2969</div>
-              : error
-                ? <div>Error: {error}</div>
-                : <div>{counts}</div>
-            }
+             <div>{geneCount ?? '2969'}</div>
             Genes
           </EntityButton>
           <EntityButton
@@ -55,7 +51,8 @@ const DiseasePortalSection = () => {
             to='/disease/DOID:10652#associated-alleles'
             tooltip='View all associated alleles'
           >
-            268<br />Alleles
+            <div>{alleleCount ?? '270'}</div>
+            Alleles
           </EntityButton>
           <EntityButton
             id='entity-publications'
