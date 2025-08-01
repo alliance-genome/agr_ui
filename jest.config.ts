b663d50a6f9b8bf9cdea2b11cfa3ce941dc041a1
Seth Campbell <sercampb@iu.edu>
2025-08-01 10:16:24 -0400

    fixing github action

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,12 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom', // Same name of the lib you installed
-  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'], // The file you created to extend jest config and "implement" the jest-dom environment in the jest globals
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
   transformIgnorePatterns: ['/node_modules/d3select/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Use babel-jest for JS/JSX/TS/TSX
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': 'identity-obj-proxy', // Handle image imports
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': 'jest-transform-stub',
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Map CSS modules if using them
-    '\\.(gif|ttf|eot|svg|png)$': 'identity-obj-proxy',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
