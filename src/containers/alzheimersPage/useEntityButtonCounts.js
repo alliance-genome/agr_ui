e77f108367344a1da8d21a8da9c91d53743c6d91
Joe Wang <dongzhuo.wang@gmail.com>
2025-09-18 12:45:39 -0600

    KANBAN-755 add entity counts

--- src/containers/alzheimersPage/useEntityButtonCounts.js
+++ src/containers/alzheimersPage/useEntityButtonCounts.js
@@ -28,27 +28,5 @@ export function useEntityButtonCounts(url) {
     };
   }, [url]);
 
-  // function filterData(items) {
-  //   const set = new Set();
-  //   for (const each of items) {
-  //     const subject = each.subject;
-  //     const symbol = subject.geneSymbol?.displayText
-  //       ? subject.geneSymbol?.displayText
-  //       : subject.name
-  //         ? subject.name
-  //         : subject.alleleSymbol?.displayText;
-  //     const type = each.generatedRelationString;
-  //
-  //     if (symbol && !set.has(symbol) && !isWrongType(type)) {
-  //       set.add(symbol);
-  //     }
-  //   }
-  //
-  //   return set.size;
-  //   function isWrongType(type) {
-  //     return type === 'is_not_implicated_in' || type === 'does_not_model';
-  //   }
-  // }
-
   return counts;
 }
