d3dc45956e4f7cb7b033550bf616f0fa0bb34e2c
Joe Wang <dongzhuo.wang@gmail.com>
2025-09-18 12:40:38 -0600

    KANBAN-755 add entity counts

--- src/containers/alzheimersPage/useEntityButtonCounts.js
+++ src/containers/alzheimersPage/useEntityButtonCounts.js
@@ -1,20 +1,18 @@
 import { useEffect, useState } from 'react';
 
-export function useEntityButtonCounts(url, limit) {
+export function useEntityButtonCounts(url) {
   const [counts, setCounts] = useState();
 
   useEffect(() => {
-    if (!limit || limit <= 0) return;
-
     let cancelled = false;
 
     async function fetchCount() {
       try {
-        const response = await fetch(`${url}?limit=${limit}`);
+        const response = await fetch(`${url}`);
         if (!response.ok) throw new Error(response.status);
         const json = await response.json();
         if (!cancelled) {
-          setCounts(filterData(json.results));
+          setCounts(json);
         }
       } catch {
         if (!cancelled) {
@@ -28,29 +26,29 @@ export function useEntityButtonCounts(url, limit) {
     return () => {
       cancelled = true;
     };
-  }, [url, limit]);
+  }, [url]);
 
-  function filterData(items) {
-    const set = new Set();
-    for (const each of items) {
-      const subject = each.subject;
-      const symbol = subject.geneSymbol?.displayText
-        ? subject.geneSymbol?.displayText
-        : subject.name
-          ? subject.name
-          : subject.alleleSymbol?.displayText;
-      const type = each.generatedRelationString;
-
-      if (symbol && !set.has(symbol) && !isWrongType(type)) {
-        set.add(symbol);
-      }
-    }
-
-    return set.size;
-    function isWrongType(type) {
-      return type === 'is_not_implicated_in' || type === 'does_not_model';
-    }
-  }
+  // function filterData(items) {
+  //   const set = new Set();
+  //   for (const each of items) {
+  //     const subject = each.subject;
+  //     const symbol = subject.geneSymbol?.displayText
+  //       ? subject.geneSymbol?.displayText
+  //       : subject.name
+  //         ? subject.name
+  //         : subject.alleleSymbol?.displayText;
+  //     const type = each.generatedRelationString;
+  //
+  //     if (symbol && !set.has(symbol) && !isWrongType(type)) {
+  //       set.add(symbol);
+  //     }
+  //   }
+  //
+  //   return set.size;
+  //   function isWrongType(type) {
+  //     return type === 'is_not_implicated_in' || type === 'does_not_model';
+  //   }
+  // }
 
   return counts;
 }
