33b93c5235ec7d6a115342a003785545544075e1
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2025-09-30 09:31:01 -0700

    fix(utils): add PMCID support to reference URL generation

--- src/components/dataTable/utils.jsx
+++ src/components/dataTable/utils.jsx
@@ -32,7 +32,7 @@ export const getIdentifier = (subject) => {
 
 export const getSingleReferenceUrl = (pubModId) => {
   let url;
-  if (pubModId.includes('PMID') || pubModId.includes('ORPHA') || pubModId.includes('MIM')) {
+  if (pubModId.includes('PMID') || pubModId.includes('ORPHA') || pubModId.includes('MIM') || pubModId.includes('PMCID')) {
     url = getResourceUrl({ identifier: pubModId });
   } else {
     url = getResourceUrl({ identifier: pubModId, type: 'reference' });
