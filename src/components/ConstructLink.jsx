2285cd314ca43f0bcb5e039b8a0735aac85bfd63
Christian Pich <cmpich@zfin.org>
2025-10-08 22:23:34 +0200

    SCRUM-4072 make construct hyperlink and fix allele link

--- src/components/ConstructLink.jsx
+++ src/components/ConstructLink.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DataSourceLink from './dataSourceLink.jsx';
-import {buildUrlFromTemplate} from "../lib/utils.js";
 
 const ConstructLink = ({ construct }) => {
-    //let url = buildUrlFromTemplate(construct.dataProviderCrossReference);
-
     return (
         <span >
         <span dangerouslySetInnerHTML={{ __html: construct.constructSymbol.displayText }} >
