815c6b3d266d6ad1a3e14a1546cd214e46f89dcc
Adam Gibson <agibson@mcw.edu>
2025-10-17 10:08:01 -0500

    add falsy check for urlTemplate

--- src/components/dataSourceLinkCuration.jsx
+++ src/components/dataSourceLinkCuration.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import ExternalLink from './ExternalLink.jsx';
 import { buildUrlFromTemplate } from '../lib/utils.js';
 
-//will be used once the data provider come
 const DataSourceLinkCuration = ({ children, reference }) => {
   let url = buildUrlFromTemplate(reference);
   return reference ? <ExternalLink href={url}>{children}</ExternalLink> : null;
