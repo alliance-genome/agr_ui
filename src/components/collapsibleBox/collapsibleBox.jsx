4a2f01de873a6ae471c24c2b8eb5edcfd765793f
Jim Thurmond <jim.flybase@cox-thurmond.net>
2025-11-04 12:34:21 -0500

    KANBAN-479: changes a prop name and cleans up propTypes (there were some leftovers from copied code)

--- src/components/collapsibleBox/collapsibleBox.jsx
+++ src/components/collapsibleBox/collapsibleBox.jsx
@@ -5,12 +5,12 @@ import classNames from 'classnames';
 
 import style from './style.module.scss';
 
-const CollapsibleBox = ({ children, initiallyExpanded = false, collapsedHeight = 4.5 }) => {
+const CollapsibleBox = ({ children, initiallyExpanded = false, foldedHeight = 4.5 }) => {
   const [expanded, setExpanded] = useState(initiallyExpanded);
   let expClass = expanded ? '' : 'folded';
   let styleObj = expanded
     ? { cursor: 'pointer', transition: 'all 0.5s ease-in-out', maxHeight: '50em' }
-    : { cursor: 'pointer', transition: 'all 0.25s', maxHeight: collapsedHeight + 'em' };
+    : { cursor: 'pointer', transition: 'all 0.25s', maxHeight: foldedHeight + 'em' };
   let titleMsg = expanded ? 'click to reduce' : 'click to expand';
 
   return (
@@ -21,10 +21,9 @@ const CollapsibleBox = ({ children, initiallyExpanded = false, collapsedHeight =
 };
 
 CollapsibleBox.propTypes = {
-  backgroundVariants: PropTypes.string,
+  foldedHeight: PropTypes.number,
   children: PropTypes.element.isRequired,
   initiallyExpended: PropTypes.bool,
-  title: PropTypes.string.isRequired,
 };
 
 export default CollapsibleBox;
