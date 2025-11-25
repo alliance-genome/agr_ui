71ae4a2b1f38a88c8ae377c3df52f52b60b30470
Joe Wang <dongzhuo.wang@gmail.com>
2025-11-25 12:11:27 -0700

    Kanban 819 simplifed initalizeGOLibraryWebComponents

--- initializeGOLibraryWebComponents.js
+++ initializeGOLibraryWebComponents.js
@@ -1,29 +1,7 @@
 import { defineCustomElements } from '@geneontology/web-components/loader';
-//unlock
-const initializeGOLibraryWebComponents_DEVELOPMENT = () => {
-  defineCustomElements();
-};
-
-const initializeGOLibraryWebComponents_PRODUCTION = () => {
-  const head = document.querySelector('head');
-
-  const ribbonStripsScriptElement = document.createElement('script');
-  ribbonStripsScriptElement.src = '/assets/web-components/web-components.esm.js';
-  head.appendChild(ribbonStripsScriptElement);
-
-  const ribbonTableScriptElement = document.createElement('script');
-  ribbonTableScriptElement.src = '/assets/web-components/web-components.esm.js';
-  head.appendChild(ribbonTableScriptElement);
-
-  const goCamVizScriptElement = document.createElement('script');
-  goCamVizScriptElement.src = '/assets/web-components/web-components.esm.js';
-  head.appendChild(goCamVizScriptElement);
-};
 
 const initializeGOLibraryWebComponents = () => {
-  process.env.NODE_ENV === 'production'
-    ? initializeGOLibraryWebComponents_PRODUCTION()
-    : initializeGOLibraryWebComponents_DEVELOPMENT();
+    defineCustomElements();
 };
 
 export default initializeGOLibraryWebComponents;
