bb9edc3cb1db9452c4379cc28827782fa80b67c1
Adam Gibson <agibson@mcw.edu>
2025-12-05 09:08:41 -0600

    fix keys and add jsdoc

--- src/components/dataTable/AssertedAlleles.jsx
+++ src/components/dataTable/AssertedAlleles.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
 import CollapsibleList from '../collapsibleList/collapsibleList.jsx';
 import { Link } from 'react-router-dom';
 import { getIdentifier } from './utils.jsx';
 
+/**
+ * Creates a link to an allele page with the allele symbol as the link text.
+ * @param {string} identifier - The allele identifier (curie, primaryExternalId, or modInternalId)
+ * @param {string} alleleSymbol - The HTML string for the allele symbol to display
+ * @returns {JSX.Element|null} A Link component to the allele page, or null if no identifier
+ */
 function makeAssertedAlleleLink(identifier, alleleSymbol) {
   if (identifier) {
     const symbol = <span dangerouslySetInnerHTML={{ __html: alleleSymbol }} />;
@@ -10,6 +17,15 @@ function makeAssertedAlleleLink(identifier, alleleSymbol) {
   return null;
 }
 
+/**
+ * Renders a collapsible list of links to additional implicated alleles.
+ * Filters out the main row's allele to avoid redundant display.
+ *
+ * @param {Object} props - Component props
+ * @param {Array<Object>} props.assertedAlleles - Array of allele objects with alleleSymbol and identifier properties
+ * @param {string} [props.mainRowCurie] - The identifier of the main row's allele to exclude from the list
+ * @returns {JSX.Element} A CollapsibleList of allele links, or empty fragment if no alleles to display
+ */
 function AssertedAlleles({ assertedAlleles, mainRowCurie }) {
   if (!assertedAlleles || assertedAlleles.length === 0) return <></>;
 
@@ -19,9 +35,14 @@ function AssertedAlleles({ assertedAlleles, mainRowCurie }) {
 
   return (
     <CollapsibleList collapsedSize={filteredAssertedAlleles.length}>
-      {filteredAssertedAlleles.map((allele) =>
-        makeAssertedAlleleLink(getIdentifier(allele), allele.alleleSymbol?.displayText)
-      )}
+      {filteredAssertedAlleles.map((allele) => {
+        const identifier = getIdentifier(allele);
+        return (
+          <React.Fragment key={identifier}>
+            {makeAssertedAlleleLink(identifier, allele.alleleSymbol?.displayText)}
+          </React.Fragment>
+        );
+      })}
     </CollapsibleList>
   );
 }
