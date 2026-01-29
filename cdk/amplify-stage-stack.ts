7a1aee66ceffe623088131bc4533f4e4656e48c9
Seth Campbell <sercampb@iu.edu>
2026-01-29 11:44:40 -0500

    gitflow additions

--- cdk/amplify-stage-stack.ts
+++ cdk/amplify-stage-stack.ts
@@ -299,7 +299,7 @@ export class AmplifyStageStack extends cdk.Stack {
         oauthToken: cdk.SecretValue.secretsManager('GithubOauthDevopsToken'),
       }),
       autoBranchCreation: {
-        patterns: ['SCRUM-*', 'hotfix/*', 'hotfix-*'],
+        patterns: ['SCRUM-*', 'hotfix/*', 'hotfix-*', 'develop/*', 'feature/*'],
       },
       autoBranchDeletion: true,
       role: iam.Role.fromRoleArn(this, 'AmplifyALBRole', 'arn:aws:iam::100225593120:role/StageAmplifyRole'),
@@ -309,7 +309,7 @@ export class AmplifyStageStack extends cdk.Stack {
 
     const domain = amplifyApp.addDomain('alliancegenome.org', {
       enableAutoSubdomain: true, // in case subdomains should be auto registered for branches
-      autoSubdomainCreationPatterns: ['scrum-*', 'hotfix-*'], // regex for branches that should auto register subdomains
+      autoSubdomainCreationPatterns: ['scrum-*', 'hotfix-*', 'develop/*'], // regex for branches that should auto register subdomains
     });
 
     //domain.mapRoot(stage);
