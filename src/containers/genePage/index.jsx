80a3f6430e82b8a1f7be9b7ac72c65f31205cc1c
VarunReddy1111 <varunreddy.go@gmail.com>
2026-04-06 09:08:47 -0500

    prettier fix

--- src/containers/genePage/index.jsx
+++ src/containers/genePage/index.jsx
@@ -97,7 +97,8 @@ const SECTIONS = [
 
 const GenePageCategoryLabel = ({ gene }) => {
   const ancestorRels = gene.geneType?.ancestors?.['SO:0000704'];
-  const isGeneType = gene.geneType?.curie === 'SO:0000704' || (ancestorRels?.length === 1 && ancestorRels[0] === 'is_a');
+  const isGeneType =
+    gene.geneType?.curie === 'SO:0000704' || (ancestorRels?.length === 1 && ancestorRels[0] === 'is_a');
   const pageLabel = isGeneType ? GENE_CATEGORY : 'genome_feature';
   return <PageCategoryLabel category={pageLabel} />;
 };
