4b102793e1e14c64cea81b3aad27fe26dbc0a941
Jim Thurmond <jim.flybase@cox-thurmond.net>
2026-04-22 16:23:42 -0400

    KANBAN-1156-test: local commit of work done with Claude on KB1156 and KB1056

--- src/containers/diseasePortal/PapersSection.jsx
+++ src/containers/diseasePortal/PapersSection.jsx
@@ -3,7 +3,6 @@ import NotFound from '../../components/notFound.jsx';
 import ExternalLink from '../../components/ExternalLink.jsx';
 import SpeciesIcon from '../../components/speciesIcon/index.jsx';
 import { speciesMap } from '../referencePage/index.jsx';
-import style from './style.module.scss';
 import { Link } from 'react-router-dom';
 import usePageLoadingQuery from '../../hooks/usePageLoadingQuery';
 
@@ -50,36 +49,27 @@ const CitationLink = ({ curie }) => {
 
 const PapersSection = ({ disease }) => {
   return (
-    <section className={style.section}>
-      <div className={style.contentContainer}>
-        <div className="row">
-          <div className="col-lg-12">
-            <h2>Recent Papers</h2>
-            <div>
-              {disease.publications.map((publication, index) => {
-                if (publication.curie) {
-                  return (
-                    <p key={'publications-' + index}>
-                      <CitationLink curie={publication.curie} />
-                    </p>
-                  );
-                }
-                if (publication.pmid) {
-                  return (
-                    <p key={'publications-' + index}>
-                      <ExternalLink href={'https://www.ncbi.nlm.nih.gov/pubmed/' + publication.pmid}>
-                        {publication.title}
-                      </ExternalLink>
-                    </p>
-                  );
-                }
-                return <p key={'publications-' + index}>{publication.title}</p>;
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <div>
+      {disease.publications.map((publication, index) => {
+        if (publication.curie) {
+          return (
+            <p key={'publications-' + index}>
+              <CitationLink curie={publication.curie} />
+            </p>
+          );
+        }
+        if (publication.pmid) {
+          return (
+            <p key={'publications-' + index}>
+              <ExternalLink href={'https://www.ncbi.nlm.nih.gov/pubmed/' + publication.pmid}>
+                {publication.title}
+              </ExternalLink>
+            </p>
+          );
+        }
+        return <p key={'publications-' + index}>{publication.title}</p>;
+      })}
+    </div>
   );
 };
 
--- src/containers/diseasePortal/index.jsx
+++ src/containers/diseasePortal/index.jsx
@@ -5,37 +5,80 @@ import PortalListSection from './PortalListSection.jsx';
 import ResourcesSection from './ResourcesSection.jsx';
 import DiseasePortalSection from './DiseasePortalSection.jsx';
 import MembersSection from '../../components/MembersSection.jsx';
-import { HELP_EMAIL } from '../../constants';
-import { useParams } from 'react-router-dom';
+import NotFound from '../../components/notFound.jsx';
+import Subsection from '../../components/subsection.jsx';
+import { DataPage, PageNav, PageData } from '../../components/dataPage';
+import PageNavEntity from '../../components/dataPage/PageNavEntity.jsx';
+import usePageLoadingQuery from '../../hooks/usePageLoadingQuery';
+import { Link, useParams } from 'react-router-dom';
 import { data } from './portalData.js';
+import style from './style.module.scss';
+
+const DESCRIPTION = 'Definition';
+const COMMUNITY_RESOURCES = 'Community Resources';
+const RECENT_PAPERS = 'Recent Alliance Papers';
+const MEMBERS = 'Members';
+
+const SECTIONS = [{ name: DESCRIPTION }, { name: RECENT_PAPERS }, { name: COMMUNITY_RESOURCES }];
 
 const DiseasePortalPage = () => {
   const { name: dname } = useParams();
-  const diseaseData = data[dname] || data['human'];
+  const diseaseData = dname ? data[dname] : data['human'];
+
+  if (dname && !diseaseData) {
+    return <NotFound />;
+  }
+
+  if (!dname) {
+    return (
+      <div>
+        <HeadMetaTags title={`${diseaseData.pageName} Portal`} />
+        <DiseasePortalSection disease={diseaseData} />
+        <section className={style.section}>
+          <div className={style.contentContainer}>
+            <h2>Disease Portals</h2>
+            <PortalListSection />
+          </div>
+        </section>
+        <section className={style.section}>
+          <div className={style.contentContainer}>
+            <h2>Community Resources</h2>
+            <ResourcesSection disease={diseaseData} />
+          </div>
+        </section>
+        <MembersSection />
+      </div>
+    );
+  }
+
+  const portalTitle = diseaseData.pageName;
+  const { data: diseaseApiData } = usePageLoadingQuery(`/api/disease/${diseaseData.doid}`);
+
   return (
     <div>
       <HeadMetaTags title={`${diseaseData.pageName} Portal`} />
       <DiseasePortalSection disease={diseaseData} />
-
-      {/* resources come before papers, but after Disease Portals list */}
-      {dname ? (
-        <>
-          <ResourcesSection disease={diseaseData} />
-          <PapersSection disease={diseaseData} />
-        </>
-      ) : (
-        <>
-          <PortalListSection />
-          <ResourcesSection disease={diseaseData} />
-        </>
-      )}
-
-      <div>
-        <h4 className="mt-4 text-center">
-          Need Help? Contact Us: &nbsp;<a href={`mailto:${HELP_EMAIL}`}>{HELP_EMAIL}</a>
-        </h4>
-      </div>
-      <MembersSection />
+      <DataPage>
+        <PageNav sections={SECTIONS}>
+          <PageNavEntity entityName={portalTitle}>
+            <Link to={`/disease/${diseaseData.doid}`}>{diseaseData.doid}</Link>
+          </PageNavEntity>
+        </PageNav>
+        <PageData>
+          <Subsection title={DESCRIPTION}>
+            {diseaseApiData?.doTerm?.definition && <p>{diseaseApiData.doTerm.definition}</p>}
+          </Subsection>
+          <Subsection title={RECENT_PAPERS}>
+            <PapersSection disease={diseaseData} />
+          </Subsection>
+          <Subsection title={COMMUNITY_RESOURCES}>
+            <ResourcesSection disease={diseaseData} />
+          </Subsection>
+          <Subsection hideTitle title={MEMBERS}>
+            <MembersSection />
+          </Subsection>
+        </PageData>
+      </DataPage>
     </div>
   );
 };
