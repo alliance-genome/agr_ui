356ae1c8fab2bf7eef299d3af9195408426724ac
Seth Campbell <sercampb@iu.edu>
2026-09-17 11:27:58 -0400

    updates pavi paths in anticipation of Paulos updates on the pavi side

--- cdk/amplify-stage-stack.ts
+++ cdk/amplify-stage-stack.ts
@@ -182,13 +182,13 @@ export class AmplifyStageStack extends cdk.Stack {
       },
       {
         source: '/pavi/',
-        target: 'https://pavi.alliancegenome.org',
-        status: amplify.RedirectStatus.PERMANENT_REDIRECT,
+        target: 'https://pavi.alliancegenome.org/pavi',
+        status: amplify.RedirectStatus.REWRITE,
       },
       {
         source: '/pavi/<*>',
-        target: 'https://pavi.alliancegenome.org/<*>',
-        status: amplify.RedirectStatus.PERMANENT_REDIRECT,
+        target: 'https://pavi.alliancegenome.org/pavi/<*>',
+        status: amplify.RedirectStatus.REWRITE,
       },
       {
         source: '/alliancemine',
